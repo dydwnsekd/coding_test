609a7e6e75869ab1fde8fafbb48cb952e83e70d9
yj.han <dydwnsekd123@gmail.com>
2019-04-02 23:34:29 +0900

    백준 11654번 완료

---
+++ java/sol_11654.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.*;
+
+public class sol_11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Scanner in = new Scanner(System.in);
+	
+	char a = in.nextLine().charAt(0);
+	
+	System.out.println((int)a);
+
+	}
+
+}
