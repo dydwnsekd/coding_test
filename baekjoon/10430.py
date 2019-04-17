9fb6b0147ef6887d6b9ae0f15b87e9b6e18c2102
dydwnsekd <dydwnsekd123@gmail.com>
2019-04-17 22:56:35 +0900

    백준 10430문제 해결

---
+++ baekjoon/10430.py
@@ -0,0 +1,12 @@
+import sys
+
+a, b, c = sys.stdin.readline().split()
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+print (int((a+b)%c))
+print (int((a%c + b%c)%c))
+print (int((a*b)%c))
+print (int(a%c * b%c) % c)
