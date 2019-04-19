7c889e01f397a80b891d71b0e5f6f9c3a9e99e58
dydwnsekd <dydwnsekd123@gmail.com>
2019-04-19 17:03:34 +0900

    StringTokenizer과 split 차이점 정리 코드 수정

--- java/array_split.java
+++ java/array_split.java
@@ -1,6 +1,10 @@
 import java.util.*;
+import org.apache.commons.lang.StringUtils;
 
 //배열 형식으로 입력이 들어왔을 때 (String) 그걸 진짜 배열에 넣어서 반환하는 함수 개발
+//split와 StringTokenizer의 차이
+//split의 경우 split(",[")를 사용하는 경우 ,[ 로 문자열을 자르는 반면
+//StringTokenizer의 경우 ",[" 를 사용하는 경우 , or [ 로 구분해서 문자열을 자름
 
 public class array_split {
 
@@ -8,27 +12,35 @@ public class array_split {
 
         // 1차원 배열
         Scanner in = new Scanner(System.in);
-        // String str = in.nextLine();
-        // ArrayList<String> str_al = new ArrayList<>();
-	
-        // StringTokenizer st = new StringTokenizer(str.substring(1, str.length()-1), ", ");
-        // while(st.hasMoreElements())
-        // {
-        //     str_al.add(st.nextToken());
-        // }
-        // System.out.println(str_al);
+        String str = in.nextLine();
+        ArrayList<String> str_al = new ArrayList<>();
+    
+        str = str.replaceAll(" ", "");
+        str = str.substring(1, str.length()-1);
+
+        for (String temp: str.split(","))
+            str_al.add(temp);
+        
+        System.out.println(str_al);
 
         // 2차원 배열
         String sttr = in.nextLine();
-        ArrayList<String> sttr_al = new ArrayList<>();
-        StringTokenizer stt = new StringTokenizer(sttr.substring(1, sttr.length()-1), "],\\s[");
-        System.out.println(stt.countTokens());
-        System.out.println(sttr.substring(1, sttr.length()-1));
+        sttr = sttr.substring(1, sttr.length()-1).replaceAll(" ", "");
+        ArrayList<ArrayList<String>> sttr_al = new ArrayList<>();
+        ArrayList<String> al_temp = new ArrayList<>();
+        StringTokenizer st_temp = null;
 
-        while(stt.hasMoreElements())
+        System.out.println(sttr);
+
+        for (String temp: sttr.split("\\],"))
         {
-            System.out.println(stt.nextToken());
+            st_temp = new StringTokenizer(temp, "[,]");
+            while (st_temp.hasMoreTokens())
+            {
+                al_temp.add(st_temp.nextToken());
+            }
+            sttr_al.add(al_temp.clone());
+            al_temp.clear();
         }
-        //System.out.println(sttr_al.size());
     }
 }
