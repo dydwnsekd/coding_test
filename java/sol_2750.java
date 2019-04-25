7238166b50666410b69751de3a2fa9a061d3be5b
yj.han <dydwnsekd123@gmail.com>
2019-04-25 23:12:52 +0900

    백준 2750번 완료, 2751번은 정렬 알고리즘을 수정할 필요가 있음.

---
+++ java/sol_2750.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class sol_2751 {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> a = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        int current = 999999999;
+        int idx = 0;
+        int num = in.nextInt();
+        
+        for(int i=0; i<num; i++)
+            a.add(in.nextInt());
+        
+        //이 경우 int형 배열만 사용 가능 ArrayList를 사용하는 경우는 직접 구현해야 할 것 같음.
+        //Arrays.sort(a);
+
+        for(int i=num; i>0; i--)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(current > a.get(j))
+                {
+                    idx = j;
+                    current = a.get(j);
+                }
+            }
+            a.remove(idx);
+            result.add(current);
+            current = 999999999;
+        }
+
+        for(int i=0; i<num; i++)
+            System.out.println(result.get(i));
+    }
+
+}
