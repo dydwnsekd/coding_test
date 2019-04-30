8085a1a34a983cc9041e10e4d1f99ada06d8ae91
dydwnsekd <dydwnsekd123@gmail.com>
2019-04-30 17:43:24 +0900

    bubble_sort 파일 sort로 변경 selection_sort 내용 추가

--- java/sort.java
+++ java/sort.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class bubble_sort {
+public class sort {
 
     public static void main(String[] args)
     {
@@ -17,7 +17,7 @@ public class bubble_sort {
 
         System.out.println(a);
 
-        a = bubble(a);
+        a = selection(a);
 
         System.out.println(a);
 
@@ -30,13 +30,32 @@ public class bubble_sort {
         {
             for(int j=0; j<i; j++)
             {
-                if((int)input_arr.get(j) > (int)input_arr.get(j+1))
+                if(input_arr.get(j) > input_arr.get(j+1))
                 {
                     temp = input_arr.get(j);
                     input_arr.set(j, input_arr.get(j+1));
                     input_arr.set(j+1, temp);
                 }
             }
+        }
+        return input_arr;
+    }
+
+    public static ArrayList<Integer> selection(ArrayList<Integer> input_arr)
+    {
+        int temp;
+        int idx = 0;
+        for(int i=0; i<input_arr.size()-1; i++)
+        {
+            idx = i;
+            for(int j=i+1; j<input_arr.size(); j++)
+            {
+                if(input_arr.get(idx) > input_arr.get(j))
+                    idx = j;
+            }
+            temp = input_arr.get(i);
+            input_arr.set(i, input_arr.get(idx));
+            input_arr.set(idx, temp);
         }
         return input_arr;
     }
