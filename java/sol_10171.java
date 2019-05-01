fe869db772410425bf8119d0ab26ae8b848e83d3
yj.han <dydwnsekd123@gmail.com>
2019-05-01 23:28:22 +0900

    백준 10171번 문제 해결

---
+++ java/sol_10171.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class sol_10171 {
+
+    public static void main(String[] args)
+    {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
