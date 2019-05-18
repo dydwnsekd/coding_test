a1c75f8485592b191c02a020d5453f2c051d834c
yj.han <dydwnsekd123@gmail.com>
2019-05-18 23:15:44 +0900

    백준 17202 문제 완료

--- baekjoon/17202.py
+++ baekjoon/17202.py
@@ -1,7 +1,7 @@
 import sys
 
-num1 = sys.stdin.readline().replace("-", "")
-num2 = sys.stdin.readline().replace("-", "")
+num1 = sys.stdin.readline().replace("-", "").replace("\n", "")
+num2 = sys.stdin.readline().replace("-", "").replace("\n", "")
 
 num = ""
 temp = "0000000000"
@@ -10,19 +10,12 @@ for i in range(len(num1)):
     num = num + num1[i]
     num = num + num2[i]
 
-print (num)
-while(len(num) >= 2):
+while(len(num) >= 3):
     temp = ""
-    for i in range(len(num)-2):
-        print (num[i])
-        print (num[i+1])
-        weight = int(num[i]) + int(num[i+1])
-        if weight > 10:
-            weight -= 10
-        
+    for i in range(len(num)-1):
+        weight = (int(num[i]) + int(num[i+1])) % 10
         temp += str(weight)
-    
-    print (temp)
+
     num = temp
 
 print (num)
