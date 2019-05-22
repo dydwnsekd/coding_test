86232e4c08a1fbaefed9e6ecb214b568d959cbba
yj.han <dydwnsekd123@gmail.com>
2019-05-22 23:19:25 +0900

    백준 17260 문제 해결

--- baekjoon/17260.py
+++ baekjoon/17260.py
@@ -3,17 +3,14 @@ import sys
 sys.stdin.readline()
 problem_list = list(map(int,sys.stdin.readline().split()))
 
+dp_list = [0 for _ in range(80001)]
+dp_list[0] = 0
+
+for i in range(1, 80001):
+    dp_list[i] = dp_list[i-1]
+    if i % 3 == 0 or i % 7 == 0:
+        dp_list[i] += i
+        
+
 for i in problem_list:
-    result = 0
-
-    count3 = int(i/3)
-    count7 = int(i/7)
-
-    for j in range(1, count3+1):
-        result += j*3
-
-    for j in range(1, count7+1):
-        if (j*7) % 3 != 0:
-            result += j*7
-
-    print (result)
+    print(dp_list[i])
