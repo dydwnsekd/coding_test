b9c87af452d585b5a4fa8354859156451a1ba57c
yj.han <dydwnsekd123@gmail.com>
2019-05-31 22:01:43 +0900

    백준 1912번 문제 해결 DP이용 O(n^2) => O(n)으로 변경

--- baekjoon/1912.py
+++ baekjoon/1912.py
@@ -1,18 +1,22 @@
 import sys
 
-max_value = -999999999
-
 num_count = int(sys.stdin.readline())
 input_num = list(map(int, sys.stdin.readline().split(" ")))
 
-for i in range(num_count):
-    result_list = [0 for i in range(num_count)]
-    for j in range(i, num_count):
-        #print (j)
-        result_list[j] = result_list[j-1] + input_num[j]
+result_list = [0 for i in range(num_count)]
+result_list[0] = input_num[0]
 
-    if max(result_list) > max_value:
-        max_value = max(result_list)
+max_value = input_num[0]
+
+for i in range(1, num_count):
+
+    if result_list[i-1] + input_num[i] < input_num[i]:
+        result_list[i] = input_num[i]
+    else:
+        result_list[i] = result_list[i-1] + input_num[i]
+
+    if max_value < result_list[i]:
+        max_value = result_list[i]    
 
 print (max_value)
     
