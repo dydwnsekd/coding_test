1655bbed8f857064e33a5ca9c541286ca72fa1e1
dydwnsekd <dydwnsekd123@gmail.com>
2019-06-07 15:03:47 +0900

    백준 1932번 문제 코드 불필요 부분 제거

--- baekjoon/1932.py
+++ baekjoon/1932.py
@@ -13,6 +13,6 @@ result_list[0] = num_list[0]
 
 for i in range(1, floor):
     for j in range(0, floor-i):
-        result_list[i][j] = max(result_list[i-1][j], result_list[i-1][j+1], result_list[i][j]) + num_list[i][j]
+        result_list[i][j] = max(result_list[i-1][j], result_list[i-1][j+1]) + num_list[i][j]
 
 print(result_list[floor-1][0])
