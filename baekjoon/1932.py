3708b138f0f20561e61c32319209f3abaa0f7d16
dydwnsekd <dydwnsekd123@gmail.com>
2019-06-07 14:43:20 +0900

    백준 1932번 정수 삼각형 해결

---
+++ baekjoon/1932.py
@@ -0,0 +1,18 @@
+import sys
+
+floor = int(sys.stdin.readline())
+num_list = list()
+result_list = [ [0] * i for i in range(floor, 0, -1)]
+
+for i in range(floor):
+    num_list.append(list(map(int, sys.stdin.readline().split(" "))))
+
+num_list.reverse()
+
+result_list[0] = num_list[0]
+
+for i in range(1, floor):
+    for j in range(0, floor-i):
+        result_list[i][j] = max(result_list[i-1][j], result_list[i-1][j+1], result_list[i][j]) + num_list[i][j]
+
+print(result_list[floor-1][0])
