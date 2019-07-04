9cf031e2efeb206c34c7d4049ec832676bab4155
dydwnsekd <dydwnsekd123@gmail.com>
2019-07-04 19:38:17 +0900

    백준 9498번 문제 시험 성적 해결

---
+++ baekjoon/9498.py
@@ -0,0 +1,19 @@
+import sys
+
+a = sys.stdin.readline()
+a = int(a)
+
+if a >= 90:
+    print ('A')
+
+elif a >= 80:
+    print ('B')
+
+elif a >= 70:
+    print ('C')
+
+elif a >= 60:
+    print ('D')
+
+else:
+    print ('F')
