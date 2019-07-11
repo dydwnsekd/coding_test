9870c9813a835eba766363d66dd2cb7ba27a6f5e
yj.han <dydwnsekd123@gmail.com>
2019-07-11 21:30:50 +0900

    백준 2864번 문제 5와 6의 차이 초안 작성 수정 필요

---
+++ baekjoon/2864.py
@@ -0,0 +1,38 @@
+import sys
+
+a, b = list(sys.stdin.readline().split(" "))
+
+max_a = list()
+max_b = list()
+min_a = list()
+min_b = list()
+
+for i in range(len(a)):
+    if a[i] == '5':
+        max_a.append('6')
+        min_a.append('5')
+    elif a[i] == '6':
+        max_a.append('6')
+        min_a.append('5')
+    else:
+        max_a.append(a[i])
+        min_a.append(a[i])
+    
+for i in range(len(a)):
+    if b[i] == '5':
+        max_b.append('6')
+        min_b.append('5')
+    elif b[i] == '6':
+        max_b.append('6')
+        min_b.append('5')
+    else:
+        max_b.append(b[i])
+        min_b.append(b[i])
+
+max_a = int("".join(max_a))
+min_a = int("".join(min_a))
+max_b = int("".join(max_b))
+min_b = int("".join(min_b))
+
+print ("%d %d" % (max_a + max_b, min_a + min_b))
+
