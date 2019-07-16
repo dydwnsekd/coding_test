0b722be648277727aa9e5c262e065b7497c21cdd
yj.han <dydwnsekd123@gmail.com>
2019-07-17 00:11:28 +0900

    백준 2491번 문제 수열 문제 해결

--- baekjoon/2491.py
+++ baekjoon/2491.py
@@ -2,7 +2,8 @@ import sys
 
 n = int(sys.stdin.readline())
 length = 0
-current_length = 1
+asc_length = 1
+desc_length = 1
 equal_length = 0
 pre_value = None
 flag = None     # True = 커지고 있을 때, False = 작아지고 있을 때
@@ -17,44 +18,27 @@ if n <= 2:
 else:
     for i in num_list[1:]:
         if pre_value < i:
-            if flag != False:
-                current_length += 1
-            else:
-                if current_length > length:
-                    length = current_length
-                    current_length = 2
-                    current_length += equal_length
-                    equal_length = 0
-            flag = True
+            asc_length += 1
+
+            if desc_length > length:
+                length = desc_length
+            desc_length = 1
 
         elif pre_value == i:
-            if equal_length == 0:
-                equal_length = 1
-            else:
-                equal_length += 1
-            current_length += 1
-            
+            asc_length += 1
+            desc_length += 1
+
         else:
-            if flag != True:
-                flag = False
-                current_length += 1
-            else:
-                if current_length > length:
-                    length = current_length
-                    current_length = 2
-                    current_length += equal_length
-                    equal_length = 0
-            flag = False
-            
+            desc_length += 1
+
+            if asc_length > length:
+                length = asc_length
+            asc_length = 1
         pre_value = i
     
-        # print ('i', i)
-        # print ('current', current_length)
-        # print ('length', length)
-        # print ('equal', equal_length)
-        # print ('\n')
-
-if current_length > length:
-    length = current_length
+if asc_length > length:
+    length = asc_length
+elif desc_length > length:
+    length = desc_length
 
 print (length)
