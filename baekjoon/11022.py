ce6842307417cb1628aac17fcfe0daf121c3383f
yj.han <dydwnsekd123@gmail.com>
2019-07-30 23:12:59 +0900

    백준 11022번 문제 A+B-8 문제 해결

---
+++ baekjoon/11022.py
@@ -0,0 +1,7 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    a, b = list(map(int, sys.stdin.readline().split(" ")))
+    print ("Case #%d: %d + %d = %d" % (i+1, a, b, a+b))
