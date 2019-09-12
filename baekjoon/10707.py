ccddddb6b232af62b72db3cdf0c1ba4472569a7c
yj.han <dydwnsekd123@gmail.com>
2019-09-12 11:32:45 +0900

    백준 10707번 문제 수도꼭지 해결

--- baekjoon/10707.py
+++ baekjoon/10707.py
@@ -6,3 +6,13 @@ y_2 = int(sys.stdin.readline())
 y_3 = int(sys.stdin.readline())
 p = int(sys.stdin.readline())
 
+x_value = x * p
+if y_2 >= p:
+    y_value = y_1
+else:
+    y_value = y_1 + ((p-y_2) * y_3)
+
+if x_value < y_value:
+    print(x_value)
+else:
+    print(y_value)
