fdeb93d1bb6754c9411ae3470ff82d3ae2d77924
yj.han <dydwnsekd123@gmail.com>
2019-09-15 18:31:01 +0900

    백준 2884번 문제 알람시계 해결

--- baekjoon/2884.py
+++ baekjoon/2884.py
@@ -5,7 +5,10 @@ h, m = list(map(int, sys.stdin.readline().split(" ")))
 if m >= 45:
     m = m - 45
 else:
-    h = h - 1
+    if h == 0:
+        h = 23
+    else:
+        h = h - 1
     m = m+60-45
 
 print(h, m)
