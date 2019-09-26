dacd8c0acbe95143300e2f9790a68bb4c41e6105
yj.han <dydwnsekd123@gmail.com>
2019-09-26 23:44:41 +0900

    백준 1001번 문제 A-B java로 해결

---
+++ java/sol_1001.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class sol_1001 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        System.out.println(n-m);
+        
+    }
+}
+
