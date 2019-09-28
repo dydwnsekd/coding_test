3e74d783bf564632813f642d5cb08b764cf28aaa
yj.han <dydwnsekd123@gmail.com>
2019-09-28 22:45:22 +0900

    백준 1152번 문제 단어의 개수 java 초안 작성

---
+++ java/sol_1152.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+    public class sol_1152 {
+    
+        public static void main(String[] args) {
+    
+            Scanner in = new Scanner(System.in);
+            String n = in.nextLine();
+            int word_count = n.split(" ").length;
+    
+            System.out.println(word_count);
+            
+        }
+    }
+    
+    
