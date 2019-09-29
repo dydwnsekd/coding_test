a4d1d9ba03f2e6aee152296fd94f8cc54a7c24b3
yj.han <dydwnsekd123@gmail.com>
2019-09-29 23:06:57 +0900

    백준 1152번 문제 단어의 개수 해결 앞 뒤 공백, 공백문자에 대한 예외 처리 추가

--- java/sol_1152.java
+++ java/sol_1152.java
@@ -5,8 +5,14 @@ import java.util.*;
         public static void main(String[] args) {
     
             Scanner in = new Scanner(System.in);
-            String n = in.nextLine();
-            int word_count = n.split(" ").length;
+            String n = in.nextLine().trim();
+            int word_count=0;
+
+
+            if(n.equals(""))
+                word_count = 0;
+            else
+                word_count = n.split(" ").length;
     
             System.out.println(word_count);
             
