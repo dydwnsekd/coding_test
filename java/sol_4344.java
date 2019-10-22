9758c7aaa0890747215796d43eeecedbbadc1b39
yj.han <dydwnsekd123@gmail.com>
2019-10-22 23:54:56 +0900

    백준 4344번 문제 변수위치 수정 코드 재수정 필요

--- java/sol_4344.java
+++ java/sol_4344.java
@@ -11,16 +11,20 @@ public class sol_4344 {
         for(int i=0; i<n; i++)
         {
             int sum = 0;
+            int count = 0;
+
             m = in.nextInt();
             int[] num_list = new int[m];
+
             for(int j=0;j<m;j++)
             {   
                 int temp = in.nextInt();
                 sum += temp;
                 num_list[j]= temp;
             }
+            
             float avg = (float)sum/m;
-            int count = 0;
+            
             for(int j=0;j<m;j++)
             {
                 if(num_list[j] > avg)
