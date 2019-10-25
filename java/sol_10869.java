cc60abba121673315446cbfac7491a492a4154c4
yj.han <dydwnsekd123@gmail.com>
2019-10-26 01:13:08 +0900

    백준 10869번 문제 사칙연산 java로 해결

---
+++ java/sol_10869.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class sol_10869 {
+
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+    
+        System.out.println(n+m);
+        System.out.println(n-m);
+        System.out.println(n*m);
+        System.out.println(n/m);
+        System.out.println(n%m);
+
+	}
+
+}
