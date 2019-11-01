6b7ab08b718d0848cfa708b38b5086c31f92c5b8
dydwnsekd <dydwnsekd123@gmail.com>
2019-11-01 18:24:44 +0900

    백준 1463번 문제 1로 만들기 아이디어 작성

--- java/sol_1463.java
+++ java/sol_1463.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
+// 1~n까지의 결과를 모두 저장하는 방식을 사용
+// 결과를 저장하고 1~n까지 반복문을 이용해 돌면서 결과를 저장
+// 더 큰 숫자가 들어왔을때 3가지 연산을 모두 실행하여 그 결과를 배열에 누적 저장
+// 숫자별로 가장 작은 값을 찾을 수 있도록 함
 public class sol_1463 {
 
 	public static void main(String[] args) {
