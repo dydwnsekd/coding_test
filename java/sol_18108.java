c11162e14482599492c8638d3164d1922782caba
yj.han <dydwnsekd123@gmail.com>
2019-11-26 22:44:11 +0900

    백준 18108번 문제 1998년생인 내가 태국에서는 2541년생?! java로 해결

---
+++ java/sol_18108.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+    public class sol_18108 {
+    
+        public static void main(String[] args) {
+    
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            
+            System.out.println(n-543);
+            
+        }
+    }
+    
+    
