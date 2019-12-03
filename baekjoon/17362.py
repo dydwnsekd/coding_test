24cb1626949f73500acc25967fb54ed44411c1f2
dydwnsekd <dydwnsekd123@gmail.com>
2019-12-03 18:42:56 +0900

    백준 17362번 문제 python 해결

--- baekjoon/17362.py
+++ baekjoon/17362.py
@@ -3,11 +3,13 @@ import sys
 n = int(sys.stdin.readline())
 n = (n%8)
 
-if n == 6:
-    print (4)
-elif n == 7:
-    print (3)
-elif n == 8:
+if n == 1:
+    print (1)
+elif n == 5:
+    print (5)
+elif n == 2 or n == 0:
     print (2)
+elif n == 3 or n == 7:
+    print (3)
 else:
-    print (n)
+    print (4)
