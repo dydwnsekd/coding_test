1e30cb7f411ca88de49d9cc3352c7477a4018066
yj.han <dydwnsekd123@gmail.com>
2019-12-14 22:47:13 +0900

    프로그래머스 모의고사 python 해결

--- programmers/study_1.py
+++ programmers/study_1.py
@@ -4,6 +4,7 @@ def solution(answers):
     p_3 = [3,3,1,1,2,2,4,4,5,5]
     
     result = [0,0,0]
+    return_list = []
     count = 0
     
     for i in answers:
@@ -11,11 +12,13 @@ def solution(answers):
             result[0] = result[0] + 1
         if i == p_2[count%len(p_2)]:
             result[1] = result[1] + 1
-        if i == p_2[count%len(p_3)]:
+        if i == p_3[count%len(p_3)]:
             result[2] = result[2] + 1
         count += 1
         
-    if min(result) == max(result):
-        return [1,2,3]
+    for i in range(3):
+        if result[i] == max(result):
+            return_list.append(i+1)
+
+    return return_list
     
-    return result(index(max(result)))
