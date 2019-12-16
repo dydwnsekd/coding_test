3197ef7cea27cdd87ed8a839bc858c1ff17b8387
yj.han <dydwnsekd123@gmail.com>
2019-12-16 23:42:20 +0900

    프로그래머스 모의고사 scala

---
+++ programmers/study_1.scala
@@ -0,0 +1,16 @@
+object Solution {
+    def solution(answers: Vector[Int]): Vector[Int] = {
+        
+        val p1 = 1 to 5
+        val p2 = [2,1,2,3,2,4,2,5]
+        val p3 = [3,3,1,1,2,2,4,4,5,5]
+        
+        for ( i <- answers)
+        {
+            if 
+            println(i)
+        }
+        
+        return Vector[Int]()
+    }
+}
