c69641b6a077614d1db79b04773be9c584239ac5
yj.han <dydwnsekd123@gmail.com>
2019-12-17 22:46:59 +0900

    scala 의사코드로 작성

--- programmers/study_1.scala
+++ programmers/study_1.scala
@@ -1,16 +1,18 @@
 object Solution {
     def solution(answers: Vector[Int]): Vector[Int] = {
         
-        val p1 = 1 to 5
-        val p2 = [2,1,2,3,2,4,2,5]
-        val p3 = [3,3,1,1,2,2,4,4,5,5]
+        val p1 = List(1,2,3,4,5)
+        val p2 = List(2,1,2,3,2,4,2,5)
+        val p3 = List(3,3,1,1,2,2,4,4,5,5)
         
         for ( i <- answers)
         {
-            if 
-            println(i)
+            // vector 길이 구하는 것 필요
+            // if 이용해서 == i / p1.lenth 비교
+            // ++ 하는 부분 필요
         }
         
+        // 3개 값 중 제일 큰 값 찾고 해당 값 return
         return Vector[Int]()
     }
 }
