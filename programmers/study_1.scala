a3a696712610fbc292df5021d832612a0310c008
dydwnsekd <dydwnsekd123@gmail.com>
2019-12-18 18:35:28 +0900

    programmers 모의고사 scala 작성중 문법상의 문제가 발생하는 것으로 보임

--- programmers/study_1.scala
+++ programmers/study_1.scala
@@ -5,14 +5,23 @@ object Solution {
         val p2 = List(2,1,2,3,2,4,2,5)
         val p3 = List(3,3,1,1,2,2,4,4,5,5)
         
-        for ( i <- answers)
-        {
-            // vector 길이 구하는 것 필요
-            // if 이용해서 == i / p1.lenth 비교
-            // ++ 하는 부분 필요
-        }
+        var result = List(0, 0, 0)
         
-        // 3개 값 중 제일 큰 값 찾고 해당 값 return
+        for ((value, index) <- answers.zipWithIndex)
+        {
+            if (p1(index%p3.length) == value)
+                result.patch(0, Seq(result(index%p1.length)+1), 1)
+            if (p2(index%p3.length) == value)
+                result.patch(1, Seq(result(index%p2.length)+1), 1)
+            if (p3(index%p3.length) == value)
+                result.patch(2, Seq(result(index%p2.length)+1), 1)
+        }
+        println (result)
+        for ( i <- result)
+        {
+            print ("AAA")
+            println (i)
+        }
         return Vector[Int]()
     }
 }
