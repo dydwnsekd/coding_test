1fc47ed1d6db54d9c245f009b6de70bf6006815b
dydwnsekd <dydwnsekd123@gmail.com>
2020-03-25 16:24:51 +0900

    에라토스테네스의 해 1차 구현 완료

--- Sieve_of_Eratosthenes.py
+++ Sieve_of_Eratosthenes.py
@@ -11,3 +11,26 @@ def prime_list(n):
 
     # 소수 목록 산출
     return [i for i in range(2, n) if sieve[i] == True]
+
+def prime_list_custom(n):
+    prime_flag = [True] * (n+1)
+
+    prime_num_list = []
+
+    prime_flag[0] = False
+    prime_flag[1] = False
+
+    for i in range(2, n+1):
+        if prime_flag[i]:
+            for j in range(n):
+                if i*j > n:
+                    break
+                prime_flag[i*j] = False
+            prime_num_list.append(i)
+    
+    return prime_num_list
+            
+    
+
+
+
