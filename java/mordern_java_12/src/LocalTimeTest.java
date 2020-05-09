1f43c1bef83b481fb02b9e9506c17b85e746e5ae
dydwnsekd <dydwnsekd123@gmail.com>
2020-05-09 23:58:48 +0900

    12장예제파일

---
+++ java/mordern_java_12/src/LocalTimeTest.java
@@ -0,0 +1,14 @@
+import java.time.LocalTime;
+
+public class LocalTimeTest {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(13,45, 20);
+        int hour = time.getHour();
+        int minute = time.getMinute();
+        int second = time.getSecond();
+
+        System.out.println(hour);
+        System.out.println(minute);
+        System.out.println(second);
+    }
+}
