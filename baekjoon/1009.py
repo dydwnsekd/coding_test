85f9637dec39f6c400a3a3cfe3f750f905fd1d32
dydwnsekd <dydwnsekd123@gmail.com>
2020-05-28 23:51:48 +0900

    1009 다른방식으로 접근

--- baekjoon/1009.py
+++ baekjoon/1009.py
@@ -3,13 +3,14 @@
 import sys
 
 n = int(sys.stdin.readline())
-
+'''
 for i in range(n):
     a,b = map(int, input().rstrip().split())
     b = b % 10
     print (a**b % 10)
-
 '''
+
+
 for i in range(n):
     a,b = map(str, input().rstrip().split())
 
@@ -17,4 +18,3 @@ for i in range(n):
         temp = int(a[0]) * int(a[0])
     
     print(a[0])
-'''
