c954b895f42c70c8723e56c5b6fa7d70e9c9d6f7
dydwnsekd <dydwnsekd123@gmail.com>
2020-06-05 23:37:47 +0900

    1019번 책 페이지 숫자를 단위별로 나누는 구문 추가

--- baekjoon/1019.py
+++ baekjoon/1019.py
@@ -2,12 +2,23 @@
 import sys
 
 n = int(sys.stdin.readline())
+num_list = []
 result_list = [0] * 10
 
 for i in range(1, n+1):
     str_num = str(i)
     for j in str_num:
         result_list[int(j)] += 1
+
+while(True):
+    temp = n%10
+    n = int(n/10)
+    print (n)
+    num_list.append(temp)
+    
+    if n<10:
+        num_list.append(n)
+        break
 
 for i in result_list:
     print (i, end=' ')
