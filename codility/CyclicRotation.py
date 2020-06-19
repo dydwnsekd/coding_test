dec909b92af73cb7fe891880efe999e75804e052
dydwnsekd <dydwnsekd123@gmail.com>
2020-06-19 23:29:12 +0900

    CyclicRotation python으로 풀이 empty list 예외 처리 필요

---
+++ codility/CyclicRotation.py
@@ -0,0 +1,7 @@
+# https://app.codility.com/demo/results/training2FDYM4-ZP3/
+
+def solution(A, K):
+    # write your code in Python 3.6
+    K = K % len(A)
+    
+    return A[len(A)-K:] + A[:len(A)-K]
