d814ae20deb3b86f2e8d88d27b881cd57b41bce4
dydwnsekd <dydwnsekd123@gmail.com>
2020-06-20 23:58:30 +0900

    CyclieRotation python 해결

--- codility/CyclicRotation.py
+++ codility/CyclicRotation.py
@@ -1,7 +1,19 @@
 # https://app.codility.com/demo/results/training2FDYM4-ZP3/
 
+'''
 def solution(A, K):
     # write your code in Python 3.6
     K = K % len(A)
     
     return A[len(A)-K:] + A[:len(A)-K]
+'''
+
+# https://app.codility.com/demo/results/trainingSYVZ6G-TXB/
+def solution(A, K):
+    # write your code in Python 3.6
+    
+    if A:
+        K = K % len(A)
+        return A[len(A)-K:] + A[:len(A)-K]
+    else:
+        return []
