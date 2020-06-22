c30285cd363740bf2b05aecd55ffcc66ccd0af5b
dydwnsekd <dydwnsekd123@gmail.com>
2020-06-22 23:13:37 +0900

    중복제거 시간초과

--- codility/OddOccurrencesInArray.py
+++ codility/OddOccurrencesInArray.py
@@ -1,5 +1,5 @@
 # https://app.codility.com/demo/results/trainingMUNEKX-RNN/
-
+# 시간 초과
 def solution(A):
     # write your code in Python 3.6
     
@@ -11,3 +11,15 @@ def solution(A):
             result_list.append(i)
         
     return result_list[0]
+
+# https://app.codility.com/demo/results/trainingMTFFB9-Q8F/
+# 시간 초과
+def solution(A):
+    while(A):
+        temp = A.pop(0)
+        if temp in A:
+            A.remove(temp)
+        else:
+            return_value = temp
+            break
+    return return_value
