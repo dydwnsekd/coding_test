b59717580f7b1ad52810428c21434100aa4b88e7
dydwnsekd <dydwnsekd123@gmail.com>
2020-06-28 23:10:54 +0900

    scala 가운데 글자 가져오기 풀이중..

---
+++ programmers/scala/Level1_1_5.scala
@@ -0,0 +1,10 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12903?language=scala
+
+object Solution {
+    def solution(s: String): String = {
+        if (s.length() % 2 == 0)
+            return s.substring((s.length()/2)-1, s.length() / 2 + 1)
+        else
+            return s.substring(int(s.length() / 2), int(s.length() / 2) + 1)
+    }
+}
