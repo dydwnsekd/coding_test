843523f00df27b7603033c1aae1bd57679541e80
dydwnsekd <dydwnsekd123@gmail.com>
2020-09-19 23:31:11 +0900

    프로그래머스 모의고사 scala return 형식 변경 확인 필요

---
+++ programmers/scala/Level1_1_1.scala
@@ -0,0 +1,28 @@
+object Solution {
+    def solution(answers: Vector[Int]): Vector[Int] = {
+        
+        val p1 = List(1,2,3,4,5)
+        val p2 = List(2,1,2,3,2,4,2,5)
+        val p3 = List(3,3,1,1,2,2,4,4,5,5)
+        
+        var result = List(0, 0, 0)
+        var count = 0
+        
+        for (value <- answers)
+        {
+            if (p1(count%p1.length) == value)
+                result = result.updated(0, result(0) + 1)
+            if (p2(count%p2.length) == value)
+                result = result.updated(1, result(1) + 1)
+            if (p3(count%p3.length) == value)
+                result = result.updated(2, result(2) + 1)
+            
+            count += 1;
+        }
+        
+        val max = result.max
+        val result_index = List(result.indexOf(max))
+        
+        return result_index.toVector;
+    }
+}
