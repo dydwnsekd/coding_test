a5659292a22f6ca026ac3e02f73b9dcb317f5fb5
dydwnsekd <dydwnsekd123@gmail.com>
2020-09-24 22:02:40 +0900

    scala 체육복 Vector에 대한 값 변경 가능 여부 리서치

--- programmers/scala/Level1_1_3.scala
+++ programmers/scala/Level1_1_3.scala
@@ -2,23 +2,26 @@ import scala.collection.mutable.ListBuffer
 
 object Solution {
     def solution(n: Int, lost: Vector[Int], reserve: Vector[Int]): Int = {
-        var temp_list = ListBuffer()
     
-        for (i <- lost){
-            if (lost.contains(i)) {
-            temp_list = temp_list += lost.(i)
-            lost -= (i)
-            }
+        var temp_list:ListBuffer[Int] = ListBuffer()
+
+        for (i <- reserve) {
+          if (lost.contains(i)) {
+            temp_list = temp_list += lost(i)
+            lost.filterNot(_ == i)
+            println(lost)
+          }
         }
         for (i <- temp_list)
-            reverve.remove(i)
+          reserve.filter(_ == i)
         for (i <- reserve) {
-            if (reserve.contains(i-1)
-                lost.remove(i-1)
-            else if (reserve.contains(i+1))
-                lost.remove(i+1)
-                
+          if (reserve.contains(i-1))
+            lost.filterNot(_ == i-1)
+          else if (reserve.contains(i+1))
+            lost.filterNot(_ == i+1)
+
         }
+        print(lost)
         return (n-lost.length)
     }
 }
