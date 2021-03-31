cfa4e6418d69489c6656122ff744cd614afe22e1
dydwnsekd <dydwnsekd123@gmail.com>
2021-03-31 22:51:28 +0900

    문자열 내림차순으로 배치하기 scala 완료

---
+++ programmers/scala/DescendingString.scala
@@ -0,0 +1,8 @@
+// 문자열 내림차순으로 배치하기
+// https://programmers.co.kr/learn/courses/30/lessons/12917?language=scala
+
+object Solution {
+    def solution(s: String): String = {
+        return s.sorted.reverse
+    }
+}
