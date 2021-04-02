b08fca6c1cab0b1be3760dab915d958077ac3c2a
dydwnsekd <dydwnsekd123@gmail.com>
2021-04-02 23:54:47 +0900

    평균 구하기 완료

---
+++ programmers/scala/getAverage.scala
@@ -0,0 +1,8 @@
+// 평균 구하기
+// https://programmers.co.kr/learn/courses/30/lessons/12944?language=scala
+
+object Solution {
+    def solution(arr: Vector[Int]): Double = {
+        return arr.sum.toDouble / arr.length
+    }
+}
