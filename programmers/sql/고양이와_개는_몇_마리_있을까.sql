28cb14062f98fafd364a8cd315b5e7b4a9ad39e1
dydwnsekd <dydwnsekd123@gmail.com>
2021-08-27 23:43:02 +0900

    고양이와 개는 몇 마리 있을까, 동명 동물 수 찾기 완료

---
+++ programmers/sql/고양이와_개는_몇_마리_있을까.sql
@@ -0,0 +1,6 @@
+-- https://programmers.co.kr/learn/courses/30/lessons/59040
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) as count
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE in ('Cat', 'Dog')
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE;
