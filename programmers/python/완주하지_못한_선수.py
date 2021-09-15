a519ed46587a67ce54e8466e72af33bb9e78f7b3
dydwnsekd <dydwnsekd123@gmail.com>
2021-09-15 23:50:02 +0900

    완주하지 못한 선수 python 완료

--- programmers/python/완주하지_못한_선수.py
+++ programmers/python/완주하지_못한_선수.py
@@ -1,7 +1,10 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42576?language=python3
+from collections import Counter
+
 def solution(participant, completion):    
-    for i in participant:
-        if i not in completion:
-            return i
-        else:
-            completion.remove(i)
+    count_participant = Counter(participant)
+    count_completion = Counter(completion)
+
+    for k in count_participant.keys():
+        if count_participant[k] != count_completion[k]:
+            return k
