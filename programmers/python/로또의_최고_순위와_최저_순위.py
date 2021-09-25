a24f673322f595681f5ac8ab71a0ffa697d976d7
dydwnsekd <dydwnsekd123@gmail.com>
2021-09-25 23:50:27 +0900

    로또의 최고 순위와 최저 순위 완료

---
+++ programmers/python/로또의_최고_순위와_최저_순위.py
@@ -0,0 +1,26 @@
+# https://programmers.co.kr/learn/courses/30/lessons/77484?language=python3
+def solution(lottos, win_nums):
+    answer = []
+    
+    correct_count = 0
+    zero_count = 0
+    
+    for i in lottos:
+        if i == 0:
+            zero_count += 1
+        elif i in win_nums:
+            correct_count += 1
+    
+    if correct_count + zero_count > 1:
+        answer.append(7-(correct_count+zero_count))
+    else:
+        answer.append(6)
+        
+    if correct_count > 1:
+        answer.append(7-correct_count)
+    else:
+        answer.append(6)
+    
+    
+    
+    return answer
