9a350ac3e669a87591889482631209a71f72fb37
dydwnsekd <dydwnsekd123@gmail.com>
2021-10-05 23:42:25 +0900

    구명보트 python 완료

--- programmers/python/구명보트.py
+++ programmers/python/구명보트.py
@@ -3,6 +3,22 @@
 def solution(people, limit):
     answer = 0
     people.sort()
+    left = 0
+    right = len(people) - 1
+    
+    while left <= right:
+        if people[right] + people[left] > limit:
+            right -= 1
+        else:
+            right -= 1
+            left += 1
+        answer += 1
+        
+    return answer
+"""
+def solution(people, limit):
+    answer = 0
+    people.sort()
     
     while len(people) > 0:
         answer += 1
@@ -11,3 +27,4 @@ def solution(people, limit):
             people.pop(0)   
 
     return answer
+"""
