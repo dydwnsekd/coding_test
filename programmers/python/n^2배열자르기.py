88911f6811bab754fb20e742be608dfdbb831f72
dydwnsekd <dydwnsekd123@gmail.com>
2021-10-18 23:25:14 +0900

    start_index를 이용한 반복문 횟수 조정

--- programmers/python/n^2배열자르기.py
+++ programmers/python/n^2배열자르기.py
@@ -2,11 +2,18 @@
 def solution(n, left, right):
     answer = []
     
-    for i in range(1, n+1):
+    start_index = left // n + 1
+    end_index = right - n * (start_index - 1) + 1
+    
+    # print(start_index)
+    # print(end_index)
+    
+    for i in range(start_index, n+1):
         for j in range(1, n+1):
             if i >= j:
                 answer.append(i)
             else:
-                answer.append(j)     
-        
-    return answer[left:right+1]
+                answer.append(j)
+    
+    # print(answer)
+    return answer[left%n:end_index]
