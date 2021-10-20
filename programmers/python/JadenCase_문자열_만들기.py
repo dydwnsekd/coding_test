9c869f0be9552201c367ec3a26d212f0786667a4
dydwnsekd <dydwnsekd123@gmail.com>
2021-10-21 00:13:55 +0900

    JadenCase 문자열 만들기 python 완료

--- programmers/python/JadenCase_문자열_만들기.py
+++ programmers/python/JadenCase_문자열_만들기.py
@@ -1,8 +1,14 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12951#
 def solution(s):
     answer = ''
+    pre_string = " "
+    s = s.lower()
     
-    for i in s.split():
-            answer = answer + i[0].upper() + i[1:].lower() + ' '
+    for i in range(len(s)):
+        if pre_string == " ":
+            answer += s[i].upper()
+        else:
+            answer += s[i]
+        pre_string = s[i]
     
-    return answer[:-1]
+    return answer
