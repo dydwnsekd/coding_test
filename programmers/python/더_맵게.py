897a1b83a5d2bdd619e592368173b8f583ba1580
dydwnsekd <dydwnsekd123@gmail.com>
2021-10-24 00:05:07 +0900

    더 맵게 정렬 부분 간소화를 위한 방안 추가 quick sort로 변경 필요

--- programmers/python/더_맵게.py
+++ programmers/python/더_맵게.py
@@ -11,3 +11,27 @@ def solution(scoville, K):
             scoville.append(scoville.pop(1) * 2 + scoville.pop(0))
     
     return answer
+
+"""
+def solution(scoville, K):
+    answer = 0
+    scoville.sort()
+    
+    while True:
+        if scoville[0] >= K:
+            return answer
+        else:
+            answer += 1
+            temp = scoville.pop(1) * 2 + scoville.pop(0)
+            # print(scoville)
+            # print(temp)
+            for i in range(len(scoville)):
+                if temp <= scoville[i]:
+                    scoville.insert(i, temp)
+                    break
+                elif i == len(scoville) - 1:
+                    scoville.append(temp)
+            # print(scoville)
+    
+    return answer
+"""
