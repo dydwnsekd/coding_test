4a8b2862be239e2312833e7c2af5169bec0cdf06
dydwnsekd <dydwnsekd123@gmail.com>
2021-10-26 23:30:53 +0900

    소수 찾기 에라토스테네스의 체를 이용해 완료

--- programmers/python/소수_찾기.py
+++ programmers/python/소수_찾기.py
@@ -1,4 +1,17 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12921
+
+def solution(n):
+    sieve = [True] * (n+1)
+
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:
+            for j in range(i+i, n+1, i):
+                sieve[j] = False
+
+    return sieve[2:].count(True)
+    
+"""
 def isdecimal(n):
     if n == 2 or n == 3:
         return True
@@ -17,3 +30,4 @@ def solution(n):
             answer += 1
     
     return answer
+"""
