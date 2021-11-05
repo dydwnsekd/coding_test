80db3bd0e9c088c113112899ddf2468b1f329fd8
dydwnsekd <dydwnsekd123@gmail.com>
2021-11-06 00:25:03 +0900

    10250 일부 예외처리 완료

--- baekjoon/python/10250.py
+++ baekjoon/python/10250.py
@@ -3,6 +3,14 @@ import sys
 n = int(sys.stdin.readline())
 for i in range(n):
     h, w, n = list(map(int, sys.stdin.readline().split(" ")))
-    room_num = str((n // h) + 1)
-    floor = str(n % h)
+
+    if n <= h:
+        floor = str(n)
+        room_num = '1'
+    elif n % h == 0:
+        floor = str(n // h)
+        room_num = str(w)
+    else:
+        room_num = str((n // h) + 1)
+        floor = str(n % h)
     print(floor + room_num.zfill(2))
