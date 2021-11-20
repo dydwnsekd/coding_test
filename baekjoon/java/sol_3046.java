79280f607e10d8228e235b7c2069e9e36f33c62e
dydwnsekd <dydwnsekd123@gmail.com>
2021-11-21 00:20:01 +0900

    백준 3046 R2 java 완료

---
+++ baekjoon/java/sol_3046.java
@@ -0,0 +1,15 @@
+package com.testproject;
+
+import java.util.Scanner;
+
+public class sol_3046 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int R1 = in.nextInt();
+        int S = in.nextInt();
+
+         System.out.println(2*S - R1);
+    }
+}
