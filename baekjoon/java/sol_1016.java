06a45b494b5b99759ef095a9b8b115aef73da76c
dydwnsekd <dydwnsekd123@gmail.com>
2021-12-03 01:08:09 +0900

    백준 제곱 ㄴㄴ수 테스트 케이스 통과 채점 실패

--- baekjoon/java/sol_1016.java
+++ baekjoon/java/sol_1016.java
@@ -4,13 +4,33 @@ public class sol_1016 {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        int min = in.nextInt();
-        int max = in.nextInt();
+        long min = in.nextLong();
+        long max = in.nextLong();
 
-        int n = (int) Math.floor(Math.sqrt(max)) - (int) Math.ceil(Math.sqrt(min));
-        int m = max - min;
+        ArrayList<Long> num_list = new ArrayList<>();
 
-        System.out.println(m-n);
+        long n = (long) Math.ceil(Math.sqrt(min));
+        long m = (long) Math.floor(Math.sqrt(max));
+
+        if(n<=1)
+            n=2;
+
+        for(long i=n;i<=m;i++) {
+            long temp = i*i;
+            long count = 1;
+            while(true) {
+                if(temp*count > max)
+                    break;
+                else if(!num_list.contains(temp*count)) {
+                    num_list.add(temp * count);
+                    count++;
+                }
+                else
+                    count++;
+            }
+        }
+
+        System.out.println(max-min-num_list.size()+1);
 
     }
 }
