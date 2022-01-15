2e09e5f8c62aa82cf98470de93b2eb72045b1719
dydwnsekd <dydwnsekd123@gmail.com>
2022-01-15 23:49:21 +0900

    키패드 누르기 python 완료

--- programmers/python/키패드_누르기.py
+++ programmers/python/키패드_누르기.py
@@ -1,11 +1,11 @@
 def solution(numbers, hand):
     answer = ""
-    L = "*"
-    R = "#"
+    distance = [[1,3],[0,0],[1,0],[2,0],[0,1],[1,1],[2,1],[0,2],[1,2],[2,2],[0,3],[2,3]]
+    L = 10
+    R = 11
     
     L_location = [1,4,7]
     R_location = [3,6,9]
-    N_location = [2,5,8,0]
     
     for i in numbers:
         if i in L_location:
@@ -15,4 +15,21 @@ def solution(numbers, hand):
             answer += "R"
             R = i
         else:
-            pass
+            L_distance = abs(distance[i][0] - distance[L][0]) + abs(distance[i][1] - distance[L][1])
+            R_distance = abs(distance[i][0] - distance[R][0]) + abs(distance[i][1] - distance[R][1])
+
+            if L_distance > R_distance:
+                answer += "R"
+                R = i
+            elif L_distance < R_distance:
+                answer += "L"
+                L = i
+            else:
+                if hand == "right":
+                    answer += "R"
+                    R = i
+                else:
+                    answer += "L"
+                    L = i
+
+    return answer
