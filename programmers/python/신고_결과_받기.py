47bb1a7a41366f17a4f30d863b37c3d47afab06f
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-01 00:19:42 +0900

    신고 결과 받기 1차 수정

--- programmers/python/신고_결과_받기.py
+++ programmers/python/신고_결과_받기.py
@@ -1,21 +1,23 @@
 from collections import defaultdict
 
 def solution(id_list, reports, k):
-    answer = []
-    report_dict = defaultdict(list)
-    report_count = []
+    answer = [0] * len(id_list)
+    plaintiff_dict = defaultdict(list)
+    defendant_dict = defaultdict(int)
     
     for report in reports:
-        k, v = report.split()
-        report_dict[k].append(v)
-        
-    for k, v in report_dict.items():
-        report_count.extend(list(set(v)))
-        
-    for id in id_list:
-        answer.append(report_count.count(id))
-        
-    print(report_count)
-    print(answer)
-        
+        key, value = report.split()
+        plaintiff_dict[key].append(value)
+        defendant_dict[value] = defendant_dict[value] + 1
+    
+    for id_index in range(len(id_list)):
+        id = id_list[id_index]
+        if id in plaintiff_dict:
+            for i in plaintiff_dict[id]:
+                if defendant_dict[i] >= int(k):
+                    answer[id_index] = answer[id_index] + 1
+                    
+    print(plaintiff_dict)
+    print(defendant_dict)
+    
     return answer
