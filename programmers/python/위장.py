ee5d19639bcdaf62813c4e185f9a8ddf774c9416
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-08 23:48:55 +0900

    위장 테스트 케이스 1개 통과

---
+++ programmers/python/위장.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+def solution(clothes):
+    clothes_dict = defaultdict(list)
+    count = 0
+    temp_count = 1
+    for clothe in clothes:
+        k = clothe[1]
+        v = clothe[0]
+        clothes_dict[k].append(v)
+
+    for k in clothes_dict.keys():
+        count += len(clothes_dict[k])
+        temp_count *= len(clothes_dict[k])
+    
+    count += temp_count
+    return count
