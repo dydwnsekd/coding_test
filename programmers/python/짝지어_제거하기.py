2aa7d778b4d85fe0af8af0ef738beec758458036
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-13 23:22:31 +0900

    짝지어 제거하기 python stack 활용으로 변경 테스트 케이스 실패

--- programmers/python/짝지어_제거하기.py
+++ programmers/python/짝지어_제거하기.py
@@ -1,17 +1,13 @@
 def solution(s):
-    prev_len_s = 0
-    while prev_len_s != len(s):
-        prev_len_s = len(s)
-        for i in range(len(s)-1):
-            if s[i] == s[i+1]:
-                if len(s) > i+2:
-                    s = s[:i] + s[i+2:]
-                    break
-                else:
-                    s = s[:i]
-                    break
-                    
-    if s == "":
+    stack = []
+    
+    for i in s:
+        if stack and stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+
+    if stack:
         return 1
     else:
         return 0
