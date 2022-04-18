afbf8a4238b637cf8353489ab8378f6b24c4c741
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-19 01:00:33 +0900

    주식가격 테스트케이스 통과

--- programmers/python/주식가격.py
+++ programmers/python/주식가격.py
@@ -3,12 +3,15 @@ def solution(prices):
     
     for i in range(len(prices)):
         temp = 0
-        for j in prices[i:]:
+        for j in prices[i+1:]:
             if prices[i] <= j:
                 temp += 1
             else:
                 break
         answer.append(temp)
-        print(temp)
+    
+    for i in range(len(answer)-1):
+        if answer[i] == 0:
+            answer[i] = 1
     
     return answer
