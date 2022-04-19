7fa57433ab8e572ba5d4cc2176dc618effcc669e
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-20 01:09:11 +0900

    주식 가격 정확도 테스트 통과

--- programmers/python/주식가격.py
+++ programmers/python/주식가격.py
@@ -1,17 +1,21 @@
 def solution(prices):
     answer = []
+    last_flag = [True] * len(prices)
     
     for i in range(len(prices)):
-        temp = 0
+        temp = 1
         for j in prices[i+1:]:
             if prices[i] <= j:
                 temp += 1
             else:
+                last_flag[i] = False
                 break
         answer.append(temp)
     
-    for i in range(len(answer)-1):
-        if answer[i] == 0:
-            answer[i] = 1
+    for i in range(len(last_flag)):
+        if last_flag[i]:
+            answer[i] = answer[i] - 1
+    
+    answer[-1] = 0
     
     return answer
