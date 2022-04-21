86f3278a15966a5eab48917f8fe34c75b2f7a073
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-21 22:48:38 +0900

    주식가격 python 완료

--- programmers/python/주식가격.py
+++ programmers/python/주식가격.py
@@ -1,21 +1,10 @@
 def solution(prices):
-    answer = []
-    last_flag = [True] * len(prices)
-    
+    answer =[0] * len(prices)
     for i in range(len(prices)):
-        temp = 1
-        for j in prices[i+1:]:
-            if prices[i] <= j:
-                temp += 1
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i]+=1
             else:
-                last_flag[i] = False
+                answer[i]+=1
                 break
-        answer.append(temp)
-    
-    for i in range(len(last_flag)):
-        if last_flag[i]:
-            answer[i] = answer[i] - 1
-    
-    answer[-1] = 0
-    
     return answer
