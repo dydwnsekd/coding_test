995534ea88b113cb121bfadf74293f59a253aa32
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-23 02:08:38 +0900

    주식 가격 stack에 대한 주석

--- programmers/python/주식가격.py
+++ programmers/python/주식가격.py
@@ -11,6 +11,9 @@ def solution(prices):
 
 """
 # stack 이용 형식
+# stack를 사용할 수 있는 이유
+# 가장 최근의 값과 현재 값을 비교하는데 stack의 가장 마지막 값은 로직 상 stack의 어떤 값보다 크거나 같음을 의미
+# 따라서 최근 값과 현재 값을 비교 한 후 stack의 나머지 값을 비교함으로써 가격이 떨어지지 않은채 얼마나 유지되고 있는지를 파악할 수 있음
 def solution(prices):
     answer = [0]*len(prices)
     stack = []
