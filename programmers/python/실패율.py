571c75f88a147e7eea2b682c88ec7749dad2b5b0
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-25 01:08:46 +0900

    실패율 아이디어 바꿔 init

--- programmers/python/실패율.py
+++ programmers/python/실패율.py
@@ -1,23 +1,11 @@
 def solution(N, stages):
     answer = []
-
-    for i in range(1, N + 1):
-        fail_count = 0
-        try_count = 0
-        for j in stages:
-            if i == j:
-                fail_count += 1
-                try_count += 1
-            elif j > i:
-                try_count += 1
-
-        if try_count == 0:
-            answer.append(1)
-        else:
-            answer.append(float(fail_count) / try_count)
-        print(i)
-        print(fail_count)
-        print(try_count)
-        print("===================")
+    try_counter = [0] * (N + 1)
+    
+    for stage in stages:
+        for i in range(stage):
+            try_counter[i] += 1
+            
+    print(try_counter)
 
     return answer
