ecccc5045fd8394da11497045179fe018d5c5f54
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-26 00:06:29 +0900

    실패율 구하는 것까지 완료

--- programmers/python/실패율.py
+++ programmers/python/실패율.py
@@ -1,11 +1,18 @@
 def solution(N, stages):
     answer = []
     try_counter = [0] * (N + 1)
+    failure_rate = [0] * N
     
     for stage in stages:
         for i in range(stage):
             try_counter[i] += 1
             
+    for i in range(len(try_counter)-1):
+        if try_counter[i] == 0:
+            failure_rate[i] = 0.0
+        else:
+            failure_rate[i] = stages.count(i+1) / float(try_counter[i])
     print(try_counter)
+    print(failure_rate)
 
     return answer
