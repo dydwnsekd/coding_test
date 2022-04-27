2e126de19aca4a330e494fdc0312f6e95cc997df
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-28 00:44:30 +0900

    124나라의 숫자 init

---
+++ programmers/python/124나라의_숫자.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = ''
+    num_124 = ['1','2','4']
+    
+    while True:
+        if n > 3:
+            answer = num_124[n%3] + answer
+            n = n//3
+        else:
+            answer += num_124[n-1]
+            break
+    
+    return answer
