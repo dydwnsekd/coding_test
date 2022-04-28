f231af6d1b21cf44ee973806ef99ce1b14a557ec
dydwnsekd <dydwnsekd123@gmail.com>
2022-04-29 01:04:50 +0900

    124 나라 테스트 케이스 통과

--- programmers/python/124나라의_숫자.py
+++ programmers/python/124나라의_숫자.py
@@ -4,7 +4,7 @@ def solution(n):
     
     while True:
         if n > 3:
-            answer = num_124[n%3] + answer
+            answer = num_124[(n%3)-1] + answer
             n = n//3
         else:
             answer += num_124[n-1]
