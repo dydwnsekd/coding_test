83ce2f3417c6a689e15beeb780cb0840b641742b
dydwnsekd <dydwnsekd123@gmail.com>
2022-05-01 22:07:59 +0900

    124나라의 숫자 완료 로직 개선 필요

--- programmers/python/124나라의_숫자.py
+++ programmers/python/124나라의_숫자.py
@@ -1,29 +1,22 @@
 def solution(n):
     answer = ''
-    num_124 = ['1','2','4']
+    num_124 = ['4','1','2']
     
-    """
-    로직 변경 필요
-    10진수    답    현재 로직
-    1         1     1
-    2         2     2
-    3         4     4
-    4         11     11
-    5         12     21
-    6         14     42
-    7         21     12
-    8         22     22
-    9         24     44
-    10        41     14
-    11        42     24
-    12        44     141
-    """
     while True:
+        flag = False
         if n > 3:
-            answer = num_124[(n%3)-1] + answer
-            n = n//3
+            if n % 3 == 0:
+                flag = True
+                
+            answer += num_124[n % 3]
+            n = n // 3
+        
+            if flag:
+                n -= 1
         else:
-            answer += num_124[n-1]
+            answer += num_124[n % 3]
             break
     
-    return answer
+    return answer[::-1]
+
+print(solution(6))
