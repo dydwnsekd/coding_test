feef51d8b19ea443caba8a9f474afbf052116f86
dydwnsekd <dydwnsekd123@gmail.com>
2022-05-02 00:20:20 +0900

    124나라의 숫자 로직 변경

--- programmers/python/124나라의_숫자.py
+++ programmers/python/124나라의_숫자.py
@@ -2,19 +2,14 @@ def solution(n):
     answer = ''
     num_124 = ['4','1','2']
     
-    while True:
+    while n > 0:
         flag = False
-        if n > 3:
-            if n % 3 == 0:
-                flag = True
-                
-            answer += num_124[n % 3]
-            n = n // 3
-        
-            if flag:
-                n -= 1
-        else:
-            answer += num_124[n % 3]
-            break
+        answer += num_124[n % 3]
+        if n % 3 == 0:
+            flag = True
+        n = n // 3
     
+        if flag:
+            n -= 1
+        
     return answer[::-1]
