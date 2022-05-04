f5046ffcf714e56acfb09c3c28cf37579130a86d
dydwnsekd <dydwnsekd123@gmail.com>
2022-05-04 23:54:35 +0900

    add binary_to_decimal

--- util/binary_conversion.py
+++ util/binary_conversion.py
@@ -1,23 +1,43 @@
-class binary_conversion:
+class binary_converter:
 
-    def convert_to_binary(self, number):
+    def decimal_to_binary(self, decimal_number):
         binary_num = ""
-        while number > 0:
-            binary_num = str(number % 2) + binary_num
-            number = number // 2
+        while decimal_number > 0:
+            binary_num = str(decimal_number % 2) + binary_num
+            decimal_number = decimal_number // 2
 
         return binary_num
 
+    def binary_to_decimal(self, binary_num):
+        decimal_num = 0
+
+        binary_num = binary_num[::-1]
+        for i in range(len(binary_num)):
+            decimal_num += int(binary_num[i]) * 2 ** i
+        
+        return decimal_num
+
 
 if __name__ == "__main__":
-    converter = binary_conversion()
-    print(converter.convert_to_binary(1))
-    print(converter.convert_to_binary(2))
-    print(converter.convert_to_binary(3))
-    print(converter.convert_to_binary(4))
-    print(converter.convert_to_binary(5))
-    print(converter.convert_to_binary(6))
-    print(converter.convert_to_binary(7))
-    print(converter.convert_to_binary(8))
-    print(converter.convert_to_binary(9))
-    print(converter.convert_to_binary(10))
+    converter = binary_converter()
+    print(converter.decimal_to_binary(1))
+    print(converter.decimal_to_binary(2))
+    print(converter.decimal_to_binary(3))
+    print(converter.decimal_to_binary(4))
+    print(converter.decimal_to_binary(5))
+    print(converter.decimal_to_binary(6))
+    print(converter.decimal_to_binary(7))
+    print(converter.decimal_to_binary(8))
+    print(converter.decimal_to_binary(9))
+    print(converter.decimal_to_binary(10))
+
+    print(converter.binary_to_decimal("1"))
+    print(converter.binary_to_decimal("10"))
+    print(converter.binary_to_decimal("11"))
+    print(converter.binary_to_decimal("100"))
+    print(converter.binary_to_decimal("101"))
+    print(converter.binary_to_decimal("110"))
+    print(converter.binary_to_decimal("111"))
+    print(converter.binary_to_decimal("1000"))
+    print(converter.binary_to_decimal("1001"))
+    print(converter.binary_to_decimal("1010"))
