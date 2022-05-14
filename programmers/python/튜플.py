c3c108d6c4db8945384da5d117f47fcf40cdf284
dydwnsekd <dydwnsekd123@gmail.com>
2022-05-14 13:04:47 +0900

    문자열 list로 변경

--- programmers/python/튜플.py
+++ programmers/python/튜플.py
@@ -4,3 +4,9 @@ def solution(s):
     2. sorted(a, key=lambda a: len(a))를 이용해 문자열 길이로 정렬
     3. 정렬된 문자열을 이용하여 튜플 완성
     """
+    s = s.replace("{", "[").replace("}", "]")
+    print(s)
+
+if "__main__" == __name__:
+    s = "{{2}, {1,2}, {2,3,1}, {1,2,3,4}}"
+    solution(s)
