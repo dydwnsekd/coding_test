49c0a3c755a22e5ad98d7af6d2072e04c0e42e35
dydwnsekd <dydwnsekd123@gmail.com>
2022-05-15 13:01:14 +0900

    튜플 길이를 이용해 정렬

--- programmers/python/튜플.py
+++ programmers/python/튜플.py
@@ -1,12 +1,16 @@
 def solution(s):
     """
-    1. input이 문자열이기 때문에 문자열을 list 혹은 튜플로 변경
-    2. sorted(a, key=lambda a: len(a))를 이용해 문자열 길이로 정렬
+    1. input이 문자열이기 때문에 문자열을 list 혹은 튜플로 변경 (o)
+    2. sorted(a, key=lambda a: len(a))를 이용해 문자열 길이로 정렬 (o)
     3. 정렬된 문자열을 이용하여 튜플 완성
     """
     s = s.replace("{", "[").replace("}", "]")
-    print(s)
+    list_s = eval(s)
+    print(list_s)
+
+    list_s = sorted(list_s, key=lambda s: len(s))
+    print(list_s)
 
 if "__main__" == __name__:
-    s = "{{2}, {1,2}, {2,3,1}, {1,2,3,4}}"
+    s = "{{1,2,3,4}, {2}, {2,3,1}, {1,2}}"
     solution(s)
