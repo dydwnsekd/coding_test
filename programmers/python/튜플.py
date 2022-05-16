0c8cd28e19b2325100a79d8b73348176f09a3965
dydwnsekd <dydwnsekd123@gmail.com>
2022-05-16 23:29:39 +0900

    튜플 완성

--- programmers/python/튜플.py
+++ programmers/python/튜플.py
@@ -4,12 +4,20 @@ def solution(s):
     2. sorted(a, key=lambda a: len(a))를 이용해 문자열 길이로 정렬 (o)
     3. 정렬된 문자열을 이용하여 튜플 완성
     """
+    answer = []
+
     s = s.replace("{", "[").replace("}", "]")
     list_s = eval(s)
-    print(list_s)
-
     list_s = sorted(list_s, key=lambda s: len(s))
-    print(list_s)
+
+    for i in list_s:
+        for j in i:
+            if j not in answer:
+                answer.append(j)
+                break
+
+    return answer
+
 
 if "__main__" == __name__:
     s = "{{1,2,3,4}, {2}, {2,3,1}, {1,2}}"
