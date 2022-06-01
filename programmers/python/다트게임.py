fc0f723323403a206c1af1958760917e713585fc
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-02 00:02:59 +0900

    다트 게임 초안 작성

---
+++ programmers/python/다트게임.py
@@ -0,0 +1,10 @@
+def solution(dartResult):
+    """
+    1. 점수/보너스/옵션 형식의 input 나누기 (옵션은 존재하지 않을 수도 있음)
+    2. 점수 + 보너스, 점수 + 보너스 + 옵션 계산식 산출
+    3. 계산 점수 return
+    """
+
+    answer = 0
+
+    return answer
