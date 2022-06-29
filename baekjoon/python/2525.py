309a1ff722cccebfea7a3b738bf41d6c6ae704ba
dydwnsekd <dydwnsekd123@gmail.com>
2022-06-30 00:55:29 +0900

    백준 2525 오븐 시계 python 완료

---
+++ baekjoon/python/2525.py
@@ -0,0 +1,14 @@
+import sys
+
+h, m = list(map(int, sys.stdin.readline().split()))
+m += int(sys.stdin.readline())
+
+if m >= 60:
+    h_t = m // 60
+    m = m%60
+
+    h += h_t
+    if h >= 24:
+        h = h % 24
+
+print(f"{h} {m}")
