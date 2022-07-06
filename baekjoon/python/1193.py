d6e8eacb6630cb7c508a410043023a45ee93992f
dydwnsekd <dydwnsekd123@gmail.com>
2022-07-06 09:13:59 +0900

    백준 1193 분수찾기 python 완료

--- baekjoon/python/1193.py
+++ baekjoon/python/1193.py
@@ -3,13 +3,14 @@ fraction_num = 0
 counter = 1
 
 while True:
+    prev_counter = counter - 1
     if fraction_num == n:
-        print(f"{counter-1}/{counter-1}")
+        print(f"{prev_counter}/{prev_counter}")
         break
     if fraction_num > n:
         print(fraction_num)
         print(counter)
-        print(f"{n%(fraction_num-counter)}/{counter-1}")
+        print(f"{n%(fraction_num-prev_counter)}/{prev_counter}")
         break
     else:
         fraction_num += counter
