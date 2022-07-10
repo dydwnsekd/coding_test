7970213e7368153b7931d00aed756b1b9ee8b60b
dydwnsekd <dydwnsekd123@gmail.com>
2022-07-11 00:06:23 +0900

    백준 4948 베르트랑 공준 로직 개선

--- baekjoon/python/4948.py
+++ baekjoon/python/4948.py
@@ -1,23 +1,30 @@
 import sys
 
+num_list = []
+
 while True:
     n = int(sys.stdin.readline())
 
     if n == 0:
         break
     else:
-        count = 0
-        sosu_list = [True for i in range((n*2)+1)]
-        sosu_list[0] = False
-        sosu_list[1] = False
+        num_list.append(n)
 
-        for i in range(2, (n*2)+1):
-            if sosu_list[i]:
-                for j in range(i+i, (n*2)+1, i):
-                    sosu_list[j] = False
+max_num = max(num_list)
+sosu_list = [True for i in range((max_num*2)+1)]
+sosu_list[0] = False
+sosu_list[1] = False
 
-        for i in range(n+1, (n*2)+1):
-            if sosu_list[i]:
-                count += 1
+for i in range(2, (max_num * 2) + 1):
+    if sosu_list[i]:
+        for j in range(i + i, (max_num * 2) + 1, i):
+            sosu_list[j] = False
 
-        print(count)
+for n in num_list:
+    count = 0
+
+    for i in range(n+1, (n*2)+1):
+        if sosu_list[i]:
+            count += 1
+
+    print(count)
