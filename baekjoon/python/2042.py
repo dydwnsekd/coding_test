3e1f2109d2257d96e382770d2e7213aff9089497
dydwnsekd <dydwnsekd123@gmail.com>
2022-07-21 00:19:56 +0900

    백준 2042 구간 합 구하기 python 시간 초과

--- baekjoon/python/2042.py
+++ baekjoon/python/2042.py
@@ -4,12 +4,12 @@ num_list = []
 n, m, k = list(map(int, sys.stdin.readline().split()))
 
 for _ in range(n):
-    num_list.append(int(input))
+    num_list.append(int(input()))
 
 for _ in range(m+k):
     a, b, c = list(map(int, sys.stdin.readline().split()))
 
     if a == 1:
-        num_list[b] = c
+        num_list[b-1] = c
     elif a == 2:
-        print(sum(num_list[b:c+1]))
+        print(sum(num_list[b-1:c+1]))
