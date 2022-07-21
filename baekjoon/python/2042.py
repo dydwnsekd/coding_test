fe07ad96c3b50b96df299607594f19078fa50b8b
dydwnsekd <dydwnsekd123@gmail.com>
2022-07-22 00:04:07 +0900

    백준 2042 구간 합 구하기 세그먼트 트리 관련 링크 추가

--- baekjoon/python/2042.py
+++ baekjoon/python/2042.py
@@ -1,10 +1,12 @@
+# 세그먼트 트리를 이용하는 방식으로 풀이가 필요
+# 세그먼트 트리 https://m.blog.naver.com/ndb796/221282210534
 import sys
 
 num_list = []
 n, m, k = list(map(int, sys.stdin.readline().split()))
 
 for _ in range(n):
-    num_list.append(int(input()))
+    num_list.append(int(sys.stdin.readline()))
 
 for _ in range(m+k):
     a, b, c = list(map(int, sys.stdin.readline().split()))
