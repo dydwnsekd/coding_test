2d1365e1d4aa76fd1aa120c112dd9ec35d46b0c9
dydwnsekd <dydwnsekd123@gmail.com>
2022-07-24 23:20:38 +0900

    백준 11659 구간 합 구하기4 python 누적 합 형식으로 변경 시간초과

--- baekjoon/python/11659.py
+++ baekjoon/python/11659.py
@@ -3,8 +3,17 @@ import sys
 n, m = list(map(int, sys.stdin.readline().split()))
 
 num_list = list(map(int, sys.stdin.readline().split()))
+sum_list = [0] * n
+
+for i in range(n):
+    sum_list[i] = sum(num_list[0:i+1])
+
+print(sum_list)
 
 for _ in range(m):
     i, j = list(map(int, sys.stdin.readline().split()))
 
-    print(sum(num_list[i-1:j]))
+    if i == 1:
+        print(sum_list[j-1])
+    else:
+        print(sum_list[j-1] - sum_list[i-2])
