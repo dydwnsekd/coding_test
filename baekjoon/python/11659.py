24b40f6874f7ab56ac8d605ef9f8738715f56d91
dydwnsekd <dydwnsekd123@gmail.com>
2022-07-25 00:08:53 +0900

    백준 11659 구간 합 구하기4 python 누적 합 완료

--- baekjoon/python/11659.py
+++ baekjoon/python/11659.py
@@ -4,11 +4,10 @@ n, m = list(map(int, sys.stdin.readline().split()))
 
 num_list = list(map(int, sys.stdin.readline().split()))
 sum_list = [0] * n
+sum_list[0] = num_list[0]
 
-for i in range(n):
-    sum_list[i] = sum(num_list[0:i+1])
-
-print(sum_list)
+for i in range(1, n):
+    sum_list[i] = sum_list[i-1] + num_list[i]
 
 for _ in range(m):
     i, j = list(map(int, sys.stdin.readline().split()))
