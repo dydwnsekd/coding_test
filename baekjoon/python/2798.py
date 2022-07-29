2f494cfe43314c701074f2e939b1aa029fe37493
dydwnsekd <dydwnsekd123@gmail.com>
2022-07-29 20:32:49 +0900

    백준 2798 블랙잭 python 테스트 케이스만 통과

--- baekjoon/python/2798.py
+++ baekjoon/python/2798.py
@@ -1,9 +1,15 @@
 import sys
 from itertools import combinations
 
+answer = 9999999
+
 n, m = list(map(int, sys.stdin.readline().split()))
 num_list = list(map(int, sys.stdin.readline().split()))
 
 sum_list = list(map(sum, list(combinations(num_list, 3))))
 
-print(sum_list)
+for i in sum_list:
+    if abs(m-i) < abs(m-answer):
+        answer = i
+
+print(answer)
