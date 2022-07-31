bc0d116424ffd3ab4538b164307e0438e9ffdf8c
dydwnsekd <dydwnsekd123@gmail.com>
2022-07-31 23:31:56 +0900

    백준 2798 분해합 python init

---
+++ baekjoon/python/2231.py
@@ -0,0 +1,12 @@
+import sys
+
+n = sys.stdin.readline()
+
+for i in range(int(n)+1):
+    temp = int(i)
+    for j in str(i):
+        temp += int(j)
+
+    if temp == int(n):
+        print(i)
+        break
