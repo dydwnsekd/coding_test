43b0534ffb6d780f2d0780572a4e0794baf7aa33
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-04 00:42:18 +0900

    백준 10815 숫자 카드 python 이분 탐색으로 수정중

--- baekjoon/python/10815.py
+++ baekjoon/python/10815.py
@@ -1,13 +1,31 @@
 import sys
 
+def isInList(sorted_list, m):
+    middle_index = len(sorted_list) // 2
+    left = 0
+    right = len(sorted_list) - 1
+
+    while True:
+        if m > sorted_list[middle_index]:
+            left = middle_index + 1
+            middle_index = (left + right) // 2
+        elif m < sorted_list[middle_index]:
+            right = middle_index - 1
+            middle_index = (left + right) // 2
+        elif m == sorted_list[middle_index]:
+            return True
+        if right <= left:
+            return False
+
 n = int(sys.stdin.readline())
 n_list = list(map(int, sys.stdin.readline().split()))
+n_list.sort()
 
 m = int(sys.stdin.readline())
 m_list = list(map(int, sys.stdin.readline().split()))
 
 for i in m_list:
-    if i in n_list:
+    if isInList(n_list, i):
         print("1", end=" ")
     else:
         print("0", end=" ")
