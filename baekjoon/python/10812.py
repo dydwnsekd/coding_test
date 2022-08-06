a51ec7d37006bfc5b0730ce2ae28bb44818bf8a0
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-06 11:12:21 +0900

    백준 10812 바구니 순서 바꾸기 python init

---
+++ baekjoon/python/10812.py
@@ -0,0 +1,10 @@
+import sys
+
+n, m = list(map(int, sys.stdin.readline().split()))
+basket = [i for i in range(1, n+1)]
+
+for i in range(m):
+    start, end, mid = list(map(int, sys.stdin.readline().split()))
+    basket = basket[:start-1] + basket[mid-1:end] + basket[start-1:mid-1] + basket[end:]
+
+print(basket)
