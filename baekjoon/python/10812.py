3262f5f9f5382e7acb4e412e460c78c51f6c7be0
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-07 00:12:33 +0900

    백준 10812 바구니 순서 바꾸기 python 출력 형식 변경 완료

--- baekjoon/python/10812.py
+++ baekjoon/python/10812.py
@@ -7,4 +7,5 @@ for i in range(m):
     start, end, mid = list(map(int, sys.stdin.readline().split()))
     basket = basket[:start-1] + basket[mid-1:end] + basket[start-1:mid-1] + basket[end:]
 
-print(basket)
+for i in basket:
+    print(i, end=" ")
