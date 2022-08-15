9544fe9860233b199928c568fbbf78513a64539d
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-15 23:39:55 +0900

    백준 10814 나이순 정렬 python index 추가

--- baekjoon/python/10814.py
+++ baekjoon/python/10814.py
@@ -2,10 +2,10 @@ import sys
 
 n = int(sys.stdin.readline())
 user_list = []
-for _ in range(n):
-    user_list.append(list(sys.stdin.readline().split()))
+for i in range(n):
+    user_list.append([list(sys.stdin.readline().split()), i])
 
-user_list = sorted(user_list, key=lambda user_list: [user_list[0], user_list[1]])
+user_list = sorted(user_list, key=lambda user_list: [user_list[0][0], user_list[1]])
 
-for age, name in user_list:
-    print(age, name)
+for user_info in user_list:
+    print(user_info[0][0], user_info[0][1])
