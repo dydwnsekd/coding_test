d4ca5d08c2abf042f94a1b76c8dc77988e639601
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-16 00:11:31 +0900

    백준 10814 나이순 정렬 python 완료

--- baekjoon/python/10814.py
+++ baekjoon/python/10814.py
@@ -5,7 +5,7 @@ user_list = []
 for i in range(n):
     user_list.append([list(sys.stdin.readline().split()), i])
 
-user_list = sorted(user_list, key=lambda user_list: [user_list[0][0], user_list[1]])
+user_list = sorted(user_list, key=lambda user_list: [int(user_list[0][0]), user_list[1]])
 
 for user_info in user_list:
     print(user_info[0][0], user_info[0][1])
