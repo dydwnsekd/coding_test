0ab9213ee80e1ac44e9c1bab1b9c2cfb96f09720
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-19 20:19:18 +0900

    백준 10989 수 정렬하기3 python counter 이용 방식으로 변경 메모리 초과

--- baekjoon/python/10989.py
+++ baekjoon/python/10989.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 import sys
 
 n = int(sys.stdin.readline())
@@ -6,7 +8,11 @@ num_list = []
 for _ in range(n):
     num_list.append(int(sys.stdin.readline()))
 
-num_list.sort()
+num_dict = Counter(num_list)
+num_list = sorted(num_dict.items())
 
-for n in num_list:
-    print(n)
+for n_count in num_list:
+    num = n_count[0]
+    count = n_count[1]
+    for _ in range(count):
+        print(num)
