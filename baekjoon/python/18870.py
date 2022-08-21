a9ee47cd3699780e7e5da1e86009d0fa6977591d
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-22 01:05:33 +0900

    백준 18870 좌표 압축 python 시간 초과

--- baekjoon/python/18870.py
+++ baekjoon/python/18870.py
@@ -1,9 +1,8 @@
 import sys
-from collections import Counter
 
 n = int(sys.stdin.readline())
 num_list = list(map(int, sys.stdin.readline().split()))
-num_rank = sorted(num_list)
+num_rank = sorted(set(num_list))
 
 for num in num_list:
     print(num_rank.index(num), end=" ")
