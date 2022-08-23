6753126a3f88bebff1c0fc86b13eb5afb5b9ab09
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-23 23:17:51 +0900

    백준 18870 좌표 압축 python 완료

--- baekjoon/python/18870.py
+++ baekjoon/python/18870.py
@@ -3,6 +3,10 @@ import sys
 n = int(sys.stdin.readline())
 num_list = list(map(int, sys.stdin.readline().split()))
 num_rank = sorted(set(num_list))
+rank_dict = {}
+
+for i in range(len(num_rank)):
+    rank_dict[num_rank[i]] = i
 
 for num in num_list:
-    print(num_rank.index(num), end=" ")
+    print(rank_dict[num], end=" ")
