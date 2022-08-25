d36c26c0ccdf1ca7be3f9ae8a8574c30e246c975
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-26 00:08:00 +0900

    백준 11478 서로 다른 부분 문자열의 개수 python 순서 조정 통한 중복제거 시도

--- baekjoon/python/11478.py
+++ baekjoon/python/11478.py
@@ -2,10 +2,14 @@ from itertools import combinations
 import sys
 
 s = sys.stdin.readline().strip()
+result_list = []
 count = 0
 
-for i in range(len(s)):
-    print(set(combinations(s, i)))
-    count += len(set(combinations(s, i)))
+for i in range(len(s)+1):
+    set_list = (set(combinations(s, i)))
+    for j in set_list:
+        temp = "".join(sorted(j))
+        if temp not in result_list:
+            result_list.append(temp)
 
-print(count)
+print(len(result_list))
