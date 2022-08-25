3a3d709134da81b27fe242cecff5bf3e08fe88d0
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-25 23:14:12 +0900

    백준 11478 서로 다른 부분 문자열의 개수 python set으로 중복 제거 했지만 틀림

--- baekjoon/python/11478.py
+++ baekjoon/python/11478.py
@@ -1,11 +1,11 @@
 from itertools import combinations
 import sys
 
-s = sys.stdin.readline()
+s = sys.stdin.readline().strip()
 count = 0
 
 for i in range(len(s)):
-    print(list(combinations(s, i)))
-    count += len(list(combinations(s, i)))
+    print(set(combinations(s, i)))
+    count += len(set(combinations(s, i)))
 
 print(count)
