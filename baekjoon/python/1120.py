b1ca150e14de42cd119c95469a63c8948d81d084
dydwnsekd <dydwnsekd123@gmail.com>
2022-08-31 02:44:01 +0900

    백준 1120 문자열 python x문자열이 큰 경우 처리

--- baekjoon/python/1120.py
+++ baekjoon/python/1120.py
@@ -7,12 +7,25 @@ if len(x) == len(y):
     for i in range(len(x)):
         if x[i] != y[i]:
             count += 1
+    print(count)
+
 elif len(x) > len(y):
     min_value = 999
     for i in range(len(x)-len(y)):
         count = 0
         for j in range(len(y)):
             if x[i+j] != y[i+j]:
+                count += 1
+
+        if min_value > count:
+            min_value = count
+
+elif len(y) < len(x):
+    min_value = 999
+    for i in range(len(y)-len(x)):
+        count = 0
+        for j in range(len(x)):
+            if y[i+j] != x[i+j]:
                 count += 1
 
         if min_value > count:
