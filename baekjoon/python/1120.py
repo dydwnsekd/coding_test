1e98cfca43b62aa8728cb8a269864d7fceabcd81
dydwnsekd <dydwnsekd123@gmail.com>
2022-09-01 23:33:29 +0900

    백준 1120 문자열 python length 부분 수정 테스트 케이스 1개 실패

--- baekjoon/python/1120.py
+++ baekjoon/python/1120.py
@@ -7,25 +7,24 @@ if len(x) == len(y):
     for i in range(len(x)):
         if x[i] != y[i]:
             count += 1
-    print(count)
 
 elif len(x) > len(y):
     min_value = 999
-    for i in range(len(x)-len(y)):
+    for i in range(len(x)-len(y)+1):
         count = 0
-        for j in range(len(y)):
+        for j in range(len(y)-1):
             if x[i+j] != y[i+j]:
                 count += 1
 
         if min_value > count:
             min_value = count
 
-elif len(y) < len(x):
+elif len(y) > len(x):
     min_value = 999
-    for i in range(len(y)-len(x)):
+    for i in range(len(y)-len(x)+1):
         count = 0
-        for j in range(len(x)):
-            if y[i+j] != x[i+j]:
+        for j in range(len(x)-1):
+            if y[i+j] != x[j]:
                 count += 1
 
         if min_value > count:
