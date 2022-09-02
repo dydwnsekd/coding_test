9d9570d68609c052b6cce0b6c0d242ceea83e19d
dydwnsekd <dydwnsekd123@gmail.com>
2022-09-02 21:20:06 +0900

    백준 1120 문자열 python 완료

--- baekjoon/python/1120.py
+++ baekjoon/python/1120.py
@@ -7,28 +7,17 @@ if len(x) == len(y):
     for i in range(len(x)):
         if x[i] != y[i]:
             count += 1
+    min_value = count
 
-elif len(x) > len(y):
-    min_value = 999
-    for i in range(len(x)-len(y)+1):
-        count = 0
-        for j in range(len(y)-1):
-            if x[i+j] != y[i+j]:
-                count += 1
-
-        if min_value > count:
-            min_value = count
-
-elif len(y) > len(x):
+else:
     min_value = 999
     for i in range(len(y)-len(x)+1):
         count = 0
-        for j in range(len(x)-1):
+        for j in range(len(x)):
             if y[i+j] != x[j]:
                 count += 1
 
-        if min_value > count:
-            min_value = count
+        min_value = min([min_value, count])
 
-print(count)
+print(min_value)
 
