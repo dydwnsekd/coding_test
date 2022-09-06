85106e1097c3a64e2e2e77db694f7987193f9305
dydwnsekd <dydwnsekd123@gmail.com>
2022-09-07 01:02:45 +0900

    백준 2023 신기한 소수 python 수를 이어 붙이며 소수인지 판별하는 방식으로 변경

--- baekjoon/python/2023.py
+++ baekjoon/python/2023.py
@@ -1,18 +1,7 @@
 import sys
 
+
 class eratos:
-
-    @staticmethod
-    def prime_list(num):
-        prime_flag = [True] * num
-
-        m = int(num ** 0.5)
-        for i in range(2, m + 1):
-            if prime_flag[i]:
-                for j in range(i * i, num, i):
-                    prime_flag[j] = False
-
-        return [i for i in range(2, num) if prime_flag[i]]
 
     @staticmethod
     def is_decimal(num):
@@ -22,32 +11,9 @@ class eratos:
 
         return True
 
-    @staticmethod
-    def prime_count(num):
-        return len(eratos.prime_list(num))
 
+n = int(sys.stdin.readline())
 
-n = sys.stdin.readline()
-answer = []
-int_n = int(n)
-
-sosu_list = eratos.prime_list(10 ** int_n)
-filtered_sosu = []
-
-for sosu in sosu_list:
-    if len(str(sosu)) == int_n:
-        filtered_sosu.append(sosu)
-
-for n in filtered_sosu:
-    str_n = str(n)
-    flag = True
-    for i in range(1, len(str_n)+1):
-        if int(str_n[:i]) not in sosu_list:
-            flag = False
-
-    if flag:
-        answer.append(n)
-
-for i in answer:
-    print(i)
+answer_1 = [2, 3, 5, 7]
+answer_2 = []
 
