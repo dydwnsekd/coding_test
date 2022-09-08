9cb29a10937d62ef00f120cd134bf6da1384140f
dydwnsekd <dydwnsekd123@gmail.com>
2022-09-08 09:00:15 +0900

    백준 2023 신기한 소수 python 수 이어붙이는 함수 추가

--- baekjoon/python/2023.py
+++ baekjoon/python/2023.py
@@ -1,19 +1,23 @@
 import sys
 
 
-class eratos:
+def is_decimal(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
 
-    @staticmethod
-    def is_decimal(num):
-        for i in range(2, num):
-            if num % i == 0:
-                return False
+    return True
 
-        return True
+
+def add_sosu_num(arr: list):
+    sosu_list = ["1", "3", "5", "7", "9"]
+    answer = []
+    for i in arr:
+        for j in sosu_list:
+            if is_decimal(int(i+j)):
+                answer.append(i+j)
 
 
 n = int(sys.stdin.readline())
-
-answer_1 = [2, 3, 5, 7]
-answer_2 = []
+init_list = ["2", "3", "5", "7"]
 
