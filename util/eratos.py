39c97407cc93b26614a3b13dcf2dc9000383007b
dydwnsekd <dydwnsekd123@gmail.com>
2022-09-11 23:16:43 +0900

    eratos 소수 판별 수정

--- util/eratos.py
+++ util/eratos.py
@@ -1,3 +1,6 @@
+import math
+
+
 class eratos:
 
     @staticmethod
@@ -14,10 +17,10 @@ class eratos:
 
     @staticmethod
     def is_decimal(num):
-        for i in range(2, num):
+        for i in range(2, int(math.sqrt(num)) + 1):
             if num % i == 0:
                 return False
-        
+
         return True
 
     @staticmethod
