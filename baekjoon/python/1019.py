a9f8bbab407dc7145797a365df4daae7d5641f15
dydwnsekd <dydwnsekd123@gmail.com>
2022-09-12 23:36:47 +0900

    백준 1019 책 페이지 방식 변경 시간 초과

--- baekjoon/python/1019.py
+++ baekjoon/python/1019.py
@@ -1,26 +1,13 @@
-#https://www.acmicpc.net/problem/1019
+# https://www.acmicpc.net/problem/1019
 import sys
 
 n = int(sys.stdin.readline())
-num_list = []
+result_str = ""
 result_list = [0] * 10
 
 for i in range(1, n+1):
     str_num = str(i)
-    for j in str_num:
-        result_list[int(j)] += 1
+    result_str += str_num
 
-#https://mygumi.tistory.com/180
-#https://www.slideshare.net/Baekjoon/baekjoon-online-judge-1019
-while(True):
-    temp = n%10
-    n = int(n/10)
-    print (n)
-    num_list.append(temp)
-    
-    if n<10:
-        num_list.append(n)
-        break
-
-for i in result_list:
-    print (i, end=' ')
+for i in range(10):
+    print(result_str.count(str(i)), end=' ')
