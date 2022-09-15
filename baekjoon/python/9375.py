3b767910e4934814b39311b17429db760ba7754c
dydwnsekd <dydwnsekd123@gmail.com>
2022-09-15 22:15:21 +0900

    백준 9375 패션왕 신해빈 python default dict 형식 추가

--- baekjoon/python/9375.py
+++ baekjoon/python/9375.py
@@ -1,10 +1,18 @@
 import sys
+from collections import defaultdict
+
+category_dict = defaultdict(int)
 
 n = int(sys.stdin.readline())
 for _ in range(n):
     m = int(sys.stdin.readline())
-    clothes_dict = {}
+    category_dict = defaultdict(int)
 
     for _ in range(m):
-        name, type = sys.stdin.readline().split()
+        name, clothes_type = sys.stdin.readline().split()
+        category_dict[clothes_type] += 1
+
+    print(category_dict)
+
+
 
