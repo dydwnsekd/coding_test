2aaa04a24ae3ccc8416af6d4a72d4c97a25f8180
dydwnsekd <dydwnsekd123@gmail.com>
2022-09-16 23:19:04 +0900

    백준 9375 패션왕 신해빈 python 완료

--- baekjoon/python/9375.py
+++ baekjoon/python/9375.py
@@ -6,13 +6,15 @@ category_dict = defaultdict(int)
 n = int(sys.stdin.readline())
 for _ in range(n):
     m = int(sys.stdin.readline())
+    answer = 1
     category_dict = defaultdict(int)
 
     for _ in range(m):
         name, clothes_type = sys.stdin.readline().split()
         category_dict[clothes_type] += 1
 
-    print(category_dict)
+    for k, v in category_dict.items():
+        answer *= (v+1)
 
-
+    print(answer-1)
 
