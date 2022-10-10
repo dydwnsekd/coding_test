60004aee0781ef87b16ac64c1f733e31111bf1f1
dydwnsekd <dydwnsekd123@gmail.com>
2022-10-11 00:25:40 +0900

    백준 4564 숫자 카드놀이 python 완료

--- baekjoon/python/4564.py
+++ baekjoon/python/4564.py
@@ -5,14 +5,14 @@ while True:
     if n == "0":
         break
     else:
+        print(n, end=" ")
         while True:
             temp_value = 1
             if len(n) == 1:
-                print(n)
+                print()
                 break
             else:
                 for i in n:
                     temp_value *= int(i)
                     n = str(temp_value)
-                print(temp_value, end=" ")
-0
+                print(n, end=" ")
