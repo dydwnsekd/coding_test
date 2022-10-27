0442d9f4fbf66d7a68e880edd0a17d1fc13c0007
dydwnsekd <dydwnsekd123@gmail.com>
2022-10-27 09:36:03 +0900

    백준 4493 가위 바위 보? python def RPS init

--- baekjoon/python/4493.py
+++ baekjoon/python/4493.py
@@ -1,5 +1,26 @@
 import sys
 
+
+def RPS(p1, p2):
+    if p1 == p2:
+        return 0
+    elif p1 == "R":
+        if p2 == "P":
+            return 2
+        elif p2 == "S":
+            return 1
+    elif p1 == "P":
+        if p2 == "R":
+            return 1
+        elif p2 == "S":
+            return 2
+    elif p1 == "S":
+        if p2 == "R":
+            return 2
+        elif p2 == "P":
+            return 1
+
+
 n = int(sys.stdin.readline())
 
 for _ in range(n):
