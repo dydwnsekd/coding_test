8e2d9b431fa9bbb540523ff8521c51c47ce808f2
dydwnsekd <dydwnsekd123@gmail.com>
2022-10-31 23:38:13 +0900

    백준 4758 Filling Out the Team python init

---
+++ baekjoon/python/4758.py
@@ -0,0 +1,26 @@
+import sys
+
+wide_receiver = [4.5, 150, 200]
+lineman = [6.0, 300, 500]
+quarterback = [5.0, 200, 300]
+
+while True:
+    flag = True
+
+    speed, weight, strength = list(map(float, sys.stdin.readline().split()))
+    if speed == 0 and weight == 0 and strength == 0:
+        break
+    else:
+        if speed <= wide_receiver[0] and weight >= wide_receiver[1] and strength >= wide_receiver[2]:
+            flag = False
+            print("Wide Receiver ", end="")
+        if speed <= lineman[0] and weight >= lineman[1] and strength >= lineman[2]:
+            flag = False
+            print("Lineman ", end="")
+        if speed <= quarterback[0] and weight >= quarterback[1] and strength >= quarterback[2]:
+            flag = False
+            print("Quarterback ", end="")
+        if flag:
+            print("No positions")
+        print()
+
