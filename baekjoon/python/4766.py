de8b05a3a4caba1db6741517cffe4047df00e103
dydwnsekd <dydwnsekd123@gmail.com>
2022-11-05 07:07:48 +0900

    백준 4766 일반 화학 실험 python 완료

--- baekjoon/python/4766.py
+++ baekjoon/python/4766.py
@@ -1,11 +1,11 @@
 import sys
 
-prev_temp = 0
+prev_temp = -15
 
 while True:
     temp = float(sys.stdin.readline())
     if temp == 999:
         break
-    if prev_temp != 0:
-        print(temp-prev_temp)
+    if prev_temp >= -10:
+        print("{:.2f}".format(temp-prev_temp))
     prev_temp = temp
