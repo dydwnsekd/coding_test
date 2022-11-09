39959229411f9a68a77d51bb4ccd6d2aaa84261a
dydwnsekd <dydwnsekd123@gmail.com>
2022-11-10 00:40:54 +0900

    백준 5622 다이얼 python 완료

---
+++ baekjoon/python/5622.py
@@ -0,0 +1,24 @@
+import sys
+
+alphabet_list = sys.stdin.readline()
+result = 0
+
+for i in alphabet_list:
+    if i in "ABC":
+        result += 3
+    elif i in "DEF":
+        result += 4
+    elif i in "GHI":
+        result += 5
+    elif i in "JKL":
+        result += 6
+    elif i in "MNO":
+        result += 7
+    elif i in "PQRS":
+        result += 8
+    elif i in "TUV":
+        result += 9
+    elif i in "WXYZ":
+        result += 10
+
+print(result)
