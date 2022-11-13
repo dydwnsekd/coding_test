2f9e850f62bfefc353314881a93d044eb1892880
dydwnsekd <dydwnsekd123@gmail.com>
2022-11-14 01:31:57 +0900

    백준 10809 알파벳 찾기 python init

---
+++ baekjoon/python/10809.py
@@ -0,0 +1,12 @@
+import sys
+
+alphabet_list = [-1] * 26
+
+s = sys.stdin.readline().strip()
+
+for i in range(len(s)):
+    if alphabet_list[ord(s[i]) - 97] == -1:
+        alphabet_list[ord(i) - 97] = i
+
+for i in range(len(alphabet_list)):
+    print(alphabet_list[i], end=" ")
