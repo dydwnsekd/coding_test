bb6fa4c8c9f47e0a16ca9491839c4daa5ba1f924
dydwnsekd <dydwnsekd123@gmail.com>
2022-11-22 10:17:07 +0900

    백준 11726 2 x n 타일링 python 완료

--- baekjoon/python/11726.py
+++ baekjoon/python/11726.py
@@ -5,7 +5,7 @@ fibonacci_list[1] = 1
 
 for i in range(2, 1002):
     fibonacci_list[i] = fibonacci_list[i-1] + fibonacci_list[i-2]
-    fibonacci_list[i] = fibonacci_list[i] % 1007
+    fibonacci_list[i] = fibonacci_list[i] % 10007
 
 n = int(sys.stdin.readline())
 
