699d6b07bdc51f46041e12422600865495383b39
dydwnsekd <dydwnsekd123@gmail.com>
2022-11-24 00:33:09 +0900

    백준 11719 그대로 출력하기 2 python 완료

--- baekjoon/python/11719.py
+++ baekjoon/python/11719.py
@@ -1,8 +1,5 @@
-import sys
-
 while True:
-    s = sys.stdin.readline().replace("\n", "")
-    if len(s) != 0:
-        print(s)
-    else:
+    try:
+        print(input())
+    except:
         break
