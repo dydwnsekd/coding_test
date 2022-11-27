73f1a4cf50a1cab4f515d25cff5846a36b6ec9e7
dydwnsekd <dydwnsekd123@gmail.com>
2022-11-27 22:13:32 +0900

    백준 1157 단어 공부 python 완료

--- baekjoon/python/1157.py
+++ baekjoon/python/1157.py
@@ -1,6 +1,7 @@
 import sys
 
 alpha_dict = {}
+
 for i in range(65, 91):
     alpha_dict[chr(i)] = 0
 
@@ -9,4 +10,10 @@ s = sys.stdin.readline().strip().upper()
 for i in s:
     alpha_dict[i] = alpha_dict[i] + 1
 
+sorted_list = sorted(alpha_dict.items(), key=lambda x: x[1], reverse=True)
+max_value = sorted_list[0][1]
 
+if max_value == sorted_list[1][1]:
+    print("?")
+else:
+    print(sorted_list[0][0])
