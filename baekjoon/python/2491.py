02c8bb451c355fe633e2413aefc0fadd43f7ad68
dydwnsekd <dydwnsekd123@gmail.com>
2022-11-28 21:27:23 +0900

    백준 2491 수열 python 불필요 변수 제거

--- baekjoon/python/2491.py
+++ baekjoon/python/2491.py
@@ -1,14 +1,14 @@
+#TODO
 import sys
 
 n = int(sys.stdin.readline())
 length = 0
 asc_length = 1
 desc_length = 1
-equal_length = 0
 pre_value = None
 flag = None     # True = 커지고 있을 때, False = 작아지고 있을 때
 
-num_list = list(map(int, sys.stdin.readline().split(" ")))
+num_list = list(map(int, sys.stdin.readline().strip().split(" ")))
 
 pre_value = num_list[0]
 
@@ -35,10 +35,10 @@ else:
                 length = asc_length
             asc_length = 1
         pre_value = i
-    
+
 if asc_length > length:
     length = asc_length
 elif desc_length > length:
     length = desc_length
 
-print (length)
+print(length)
