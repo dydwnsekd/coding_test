8b40e77caee291069f6322173a0d79e3620acff2
dydwnsekd <dydwnsekd123@gmail.com>
2022-12-13 21:58:21 +0900

    fibonacci 개선

--- util/fibonacci.py
+++ util/fibonacci.py
@@ -1,12 +1,24 @@
-def fibonacci(n):
-    if n == 0:
+def get_fibonacci_num(num):
+    if num == 0:
         return 0
-    elif n == 1:
+    elif num == 1:
         return 1
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        return get_fibonacci_num(num - 1) + get_fibonacci_num(num - 2)
+
+
+def get_fibonacci_list(num):
+    fibonacci_list = [0, 1, 1]
+
+    for i in range(3, num+1):
+        fibonacci_list.append(fibonacci_list[i-2] + fibonacci_list[i-1])
+
+    return fibonacci_list
 
 
 if __name__ == "__main__":
     for i in range(10):
-        print(i, fibonacci(i))
+        print(i, get_fibonacci_num(i))
+
+    print(get_fibonacci_list(10))
+
