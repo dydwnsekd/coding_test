4ae50d0c8fff626fdaf26cff6fa4f265038d922d
dydwnsekd <dydwnsekd123@gmail.com>
2022-12-17 10:22:54 +0900

    백준 8462 배열의 힘 python 시간 초과

---
+++ baekjoon/python/8462.py
@@ -0,0 +1,19 @@
+import sys
+from collections import defaultdict
+
+n, t = list(map(int, sys.stdin.readline().split()))
+array = list(map(int, sys.stdin.readline().split()))
+
+for _ in range(t):
+    num_dict = defaultdict(int)
+    result = 0
+    l, r = list(map(int, sys.stdin.readline().split()))
+
+    for i in range(l-1, r):
+        num_dict[array[i]] += 1
+
+    for k, v in num_dict.items():
+        result += k * v * v
+
+    print(result)
+
