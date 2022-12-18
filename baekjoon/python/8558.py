66694cceea0fcbccc36e4a2946ffb97ae34bd951
dydwnsekd <dydwnsekd123@gmail.com>
2022-12-19 00:20:47 +0900

    백준 8558 Silnia python typeError

---
+++ baekjoon/python/8558.py
@@ -0,0 +1,12 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+def factorial(num):
+    if num > 1:
+        return (num * factorial(num-1)) % 10
+    elif num == 1:
+        return 1
+
+n = int(sys.stdin.readline())
+result = factorial(n)
+print(result % 10)
