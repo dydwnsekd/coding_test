8e8ab493d14c46be7a44e12168f17d0566073f59
dydwnsekd <dydwnsekd123@gmail.com>
2022-12-20 21:56:34 +0900

    백준 8558 Silnia python 완료

--- baekjoon/python/8558.py
+++ baekjoon/python/8558.py
@@ -4,7 +4,7 @@ sys.setrecursionlimit(10**7)
 def factorial(num):
     if num > 1:
         return (num * factorial(num-1)) % 10
-    elif num == 1:
+    elif num <= 1:
         return 1
 
 n = int(sys.stdin.readline())
