c8540ed961628673a5d91a2c8b3c3a594a394a39
dydwnsekd <dydwnsekd123@gmail.com>
2022-12-24 21:43:19 +0900

    백준 8678 Zbiór python 시간초과

--- baekjoon/python/8678.py
+++ baekjoon/python/8678.py
@@ -4,12 +4,17 @@ import sys
 def get_divisor(num):
 
     divisor_list = []
-    for i in range(1, num//2):
-        if num % i == 0:
-            if i not in divisor_list:
-                divisor_list.append(i)
-                divisor_list.append(num // i)
+    if num == 2:
+        return [1, 2]
+    elif num == 3:
+        return [1, 3]
 
+    for i in range(1, num//2 + 1):
+        if num % i == 0:
+            divisor_list.append(i)
+            divisor_list.append(num // i)
+
+    divisor_list = list(set(divisor_list))
     divisor_list.sort()
     return divisor_list
 
