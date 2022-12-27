a4266279a68230ae4a71ce8b960a488431cf5856
dydwnsekd <dydwnsekd123@gmail.com>
2022-12-28 00:14:24 +0900

    백준 8710 Koszykarz python 완료

--- baekjoon/python/8710.py
+++ baekjoon/python/8710.py
@@ -3,7 +3,10 @@ import sys
 k, w, m = list(map(int, sys.stdin.readline().split()))
 count = 0
 
-if w-k < 0:
+if w-k <= 0:
     print(0)
 else:
-    print(((w-k) // m) + 1)
+    if (w-k) % m == 0:
+        print((w-k) // m)
+    else:
+        print(((w-k) // m) + 1)
