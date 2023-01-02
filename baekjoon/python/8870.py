faa36915f6ffd457d6a14525a3fca438f7362bca
dydwnsekd <dydwnsekd123@gmail.com>
2023-01-03 01:11:08 +0900

    백준 8870 Zadanie próbne python init

---
+++ baekjoon/python/8870.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+ret = 0
+
+for s in range(1, n+1):
+    for k in range(s, n+1):
+        for i in range(k, n+1):
+            ret = (ret+s*k//i) % 2010
+
+print(ret)
