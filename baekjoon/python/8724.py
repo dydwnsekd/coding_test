4071b600993059b18cf18f1ab2b9ee8da0a72f72
dydwnsekd <dydwnsekd123@gmail.com>
2023-01-02 09:21:18 +0900

    백준 8724 Pinezki python 완료

--- baekjoon/python/8724.py
+++ baekjoon/python/8724.py
@@ -1,18 +1,18 @@
 import sys
 
 n = int(sys.stdin.readline())
-plankboard_list = list(map(int, sys.stdin.readline().split()))
+plankboard_list = sys.stdin.readline().split()
 
 result = 0
 temp = 0
 
 for i in plankboard_list:
-    if i == 1:
+    if i == '1':
         temp += 1
     else:
         if result < temp:
             result = temp
-            temp = 0
+        temp = 0
 
 if result < temp:
     print(temp)
