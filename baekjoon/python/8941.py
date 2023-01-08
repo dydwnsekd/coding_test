309e8e788954e4ffe89e867c283b4d8c81c2a885
dydwnsekd <dydwnsekd123@gmail.com>
2023-01-08 18:55:17 +0900

    백준 8941 Molar mass python 로직 수정

--- baekjoon/python/8941.py
+++ baekjoon/python/8941.py
@@ -3,6 +3,7 @@ import re
 
 atomic_weight = {"C": 12.01, "H": 1.008, "O": 16.00, "N": 14.01}
 atomic_regex = re.compile("[CHON]")
+num_regex = re.compile("[0-9]")
 
 n = int(sys.stdin.readline())
 
@@ -10,11 +11,10 @@ for i in range(n):
     atomic = sys.stdin.readline().strip()
     atomic_count = {"C": 0, "H": 0, "O": 0, "N": 0}
 
-    for j in range(len(atomic)-1):
+    for j in range(len(atomic)):
         if atomic_regex.match(atomic[j]):
-            if atomic_regex.match(atomic[j+1]):
-                atomic_count[atomic[j]] += 1
-            else:
-                atomic_count[atomic[j]] += int(atomic[j+1])
+            atomic_count[atomic[j]] += 1
+        else:
+            atomic_count[atomic[j-1]] += int(atomic[j])-1
 
     print(atomic_count)
