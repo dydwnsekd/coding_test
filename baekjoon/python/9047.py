2beeabd16bc3e02349983784c0a7bc443e0e46ac
dydwnsekd <dydwnsekd123@gmail.com>
2023-01-17 09:43:12 +0900

    백준 9047 6174 python 완료

--- baekjoon/python/9047.py
+++ baekjoon/python/9047.py
@@ -6,11 +6,13 @@ n = int(sys.stdin.readline())
 for _ in range(n):
     num = sys.stdin.readline().strip()
     count = 0
-    while True:
-        if num == "6174":
-            break
-
+    while num != "6174":
         count += 1
+
+        if len(num) < 4:
+            while len(num) != 4:
+                num += "0"
+
         max_str = ''.join(sorted(num, reverse=True))
         max_value = int(max_str)
         min_value = int(max_str[::-1])
