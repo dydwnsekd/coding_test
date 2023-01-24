4f05fcd0b1ee9da3512de9e8ec709ea8aa787514
dydwnsekd <dydwnsekd123@gmail.com>
2023-01-24 17:53:07 +0900

    백준 9492 Perfect Shuffle python 완료

--- baekjoon/python/9492.py
+++ baekjoon/python/9492.py
@@ -7,18 +7,18 @@ while True:
 
     card_list = []
     for _ in range(n):
-        card_list.append(sys.stdin.readline().split())
+        card_list.append(sys.stdin.readline().strip())
 
     if n % 2 == 0:
-        for i in range(0, n, 2):
-            print(card_list[i])
-        for i in range(1, n, 2):
-            print(card_list[i])
-
+        a = card_list[:n//2]
+        b = card_list[n//2:]
+        for i in range(n // 2):
+            print(a[i])
+            print(b[i])
     else:
-        print(card_list[0])
-        for i in range(1, n, 2):
-            print(card_list[i])
-        for i in range(2, n, 2):
-            print(card_list[i])
-
+        a = card_list[:(n//2)+1]
+        b = card_list[(n//2)+1:]
+        for i in range(n // 2):
+            print(a[i])
+            print(b[i])
+        print(a[-1])
