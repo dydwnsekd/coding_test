37f01798108d84998174766d692c33197e26eb76
dydwnsekd <dydwnsekd123@gmail.com>
2023-01-30 00:19:21 +0900

    백준 9557 Arabic and English python 완료

--- baekjoon/python/9557.py
+++ baekjoon/python/9557.py
@@ -3,23 +3,23 @@ import sys
 cases = int(sys.stdin.readline())
 for _ in range(cases):
     word_count = int(sys.stdin.readline())
-    for _ in range(word_count):
-        word = sys.stdin.readline().strip()
-        split_word = word.split()
 
-        englist_index = -1
+    word = sys.stdin.readline().strip()
+    split_word = word.split()
 
-        for i in range(len(split_word)):
-            if not split_word[i].startswith("#"):
-                englist_index = i
-                break
+    englist_index = -1
 
-        if englist_index == -1:
-            print(word)
-        else:
-            result = [word[englist_index+1:]]
-            result.append(word[englist_index])
-            result.append(word[:englist_index])
-            print(" ".join(result))
+    for i in range(len(split_word)):
+        if not split_word[i].startswith("#"):
+            englist_index = i
+            break
+
+    if englist_index == -1:
+        print(word)
+    else:
+        result = split_word[englist_index+1:]
+        result.append(split_word[englist_index])
+        result.extend(split_word[:englist_index])
+        print(" ".join(result))
 
 
