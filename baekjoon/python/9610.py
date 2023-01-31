82489b4d1de6f1770cc3d4def522c3bb53e46ffe
dydwnsekd <dydwnsekd123@gmail.com>
2023-02-01 00:13:49 +0900

    백준 9610 사분면 python 완료

--- baekjoon/python/9610.py
+++ baekjoon/python/9610.py
@@ -6,16 +6,16 @@ q_list = [0, 0, 0, 0, 0]
 
 for _ in range(cases):
     x, y = list(map(int, sys.stdin.readline().split()))
-    if x == 0 or y == 0:
-        q_list[4] += 1
-    elif x > 0 and y > 0:
+    if x > 0 and y > 0:
         q_list[0] += 1
-    elif x > 0 and y < 0:
-        q_list[1] += 1
     elif x < 0 and y > 0:
+        q_list[1] += 1
+    elif x < 0 and y < 0:
         q_list[2] += 1
-    else:
+    elif x > 0 and y < 0:
         q_list[3] += 1
+    else:
+        q_list[4] += 1
 
 for i in range(len(quadrant_name)):
     print(f"{quadrant_name[i]}: {q_list[i]}")
