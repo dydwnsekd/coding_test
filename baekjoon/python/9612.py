9c9c176c85f84d1be4652e65e71fe7fefd9ccbea
dydwnsekd <dydwnsekd123@gmail.com>
2023-02-02 09:36:05 +0900

    백준 9612 Maximum Word Frequency python init

---
+++ baekjoon/python/9612.py
@@ -0,0 +1,12 @@
+import sys
+from collections import defaultdict
+
+word_dict = defaultdict(int)
+cases = int(sys.stdin.readline())
+
+for _ in range(cases):
+    word_dict[sys.stdin.readline().strip()] += 1
+
+sorted_list = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+
+print(sorted_list[0][0], sorted_list[0][1])
