ad155639908e0ed22c54969ae3640f244c5af142
dydwnsekd <dydwnsekd123@gmail.com>
2023-02-15 00:06:02 +0900

    백준 9656 돌 게임2 python DP 완료

--- baekjoon/python/9656.py
+++ baekjoon/python/9656.py
@@ -1,7 +1,18 @@
-# TODO
 import sys
 
-if int(sys.stdin.readline()) % 2 == 0:
+n = int(sys.stdin.readline())
+
+result_list = [0] * 1001
+result_list[0] = 0
+result_list[1] = 1
+result_list[2] = 2
+result_list[3] = 1
+
+for i in range(4, 1001):
+    result_list[i] = min(result_list[i-1], result_list[i-3]) + 1
+
+if result_list[n] % 2 == 0:
     print("SK")
 else:
     print("CY")
+
