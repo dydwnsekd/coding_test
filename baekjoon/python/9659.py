e022ded18b4d7628518ce7df52b1c3e070a96758
dydwnsekd <dydwnsekd123@gmail.com>
2023-02-17 20:10:34 +0900

    백준 9659 돌 게임5 python 완료

--- baekjoon/python/9659.py
+++ baekjoon/python/9659.py
@@ -2,17 +2,8 @@ import sys
 
 n = int(sys.stdin.readline())
 
-result_list = [0] * 1000000000001
-result_list[0] = 0
-result_list[1] = 1
-result_list[2] = 2
-result_list[3] = 1
-
-for i in range(4, 1000000000001):
-    result_list[i] = min(result_list[i-1], result_list[i-3]) + 1
-
-if result_list[n] % 2 == 0:
-    print("SK")
-else:
+if n % 2 == 0:
     print("CY")
+else:
+    print("SK")
 
