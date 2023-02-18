2e0780a8e026fdb705fd88407f3dee9957b6e63c
dydwnsekd <dydwnsekd123@gmail.com>
2023-02-18 13:10:57 +0900

    백준 9584 Fraud Busters python 완료

---
+++ baekjoon/python/9584.py
@@ -0,0 +1,18 @@
+import sys
+import re
+
+car_pattern = re.compile(sys.stdin.readline().strip().replace("*", "."))
+cases = int(sys.stdin.readline())
+result = 0
+result_list = []
+
+for _ in range(cases):
+    t = sys.stdin.readline().strip()
+    if car_pattern.match(t):
+        result += 1
+        result_list.append(t)
+
+
+print(result)
+for c in result_list:
+    print(c)
