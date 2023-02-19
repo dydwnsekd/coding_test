91a7eeee418d8129ba2cff8a1fd1cd3fd8808d2e
dydwnsekd <dydwnsekd123@gmail.com>
2023-02-20 00:56:24 +0900

    백준 9657 돌 게임3 python 완료

--- baekjoon/python/9657.py
+++ baekjoon/python/9657.py
@@ -2,19 +2,17 @@ import sys
 
 n = int(sys.stdin.readline())
 
-result_list = [0] * 1001
-result_list[0] = False
-result_list[1] = False
-result_list[2] = True
-result_list[3] = False
-result_list[4] = False
+result_list = [False] * 1001
+result_list[1] = True
+result_list[2] = False
+result_list[3] = True
+result_list[4] = True
 result_list[5] = True
 
 for i in range(6, 1001):
-    result_list[i] = False if any([result_list[i-1], result_list[i-3], result_list[i-4]]) else True
+    result_list[i] = False if all([result_list[i-1], result_list[i-3], result_list[i-4]]) else True
 
 if result_list[n]:
-    print("CY")
-else:
     print("SK")
-
+else:
+    print("CY")
