035448dc26a1a09daa94d88f92ea4df3e622f1f2
dydwnsekd <dydwnsekd123@gmail.com>
2023-02-26 23:37:56 +0900

    백준 9594 Odd and Even Zeroes python 중간 0 값 예외로직 추가 필요

--- baekjoon/python/9594.py
+++ baekjoon/python/9594.py
@@ -1,17 +1,16 @@
 import sys
 
-count = 0
-
 while True:
     n = int(sys.stdin.readline())
+    count = 1
     temp = 1
     if n == -1:
         break
 
-    for i in range(n+1):
+    for i in range(1, n+1):
         temp *= i
 
-    if str(temp).count("0") % 2 == 0:
-        count += 1
+        if str(temp).count("0") % 2 == 0:
+            count += 1
 
     print(count)
