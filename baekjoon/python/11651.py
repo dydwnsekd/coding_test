e2d43b94eab0863e6ffa0c61370514ffee19c832
dydwnsekd <dydwnsekd123@gmail.com>
2023-03-03 19:55:02 +0900

    백준 11651 좌표 정렬하기 2 python 완료

---
+++ baekjoon/python/11651.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+coordinates_list = []
+for _ in range(n):
+    coordinates_list.append(list(map(int, sys.stdin.readline().split())))
+
+coordinates_list = sorted(coordinates_list, key=lambda coordinates_list: [coordinates_list[1], coordinates_list[0]])
+
+for x, y in coordinates_list:
+    print(x, y)
