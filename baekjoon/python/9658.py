0e223080af7747fcaa005ccd167d5f6301318135
dydwnsekd <dydwnsekd123@gmail.com>
2023-03-04 23:28:11 +0900

    백준 9658 돌게임4 로직 변경

--- baekjoon/python/9658.py
+++ baekjoon/python/9658.py
@@ -10,9 +10,9 @@ result_list[4] = True
 result_list[5] = True
 
 for i in range(6, 1001):
-    result_list[i] = False if all([result_list[i-1], result_list[i-3], result_list[i-4]]) else True
+    result_list[i] = False if any([result_list[i-1], result_list[i-3], result_list[i-4]]) else True
 
 if result_list[n]:
-    print("CY")
-else:
     print("SK")
+else:
+    print("CY")
