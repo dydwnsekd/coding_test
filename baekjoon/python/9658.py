14fb9e8a51b48d919b1a2ccace293004ec2c587f
dydwnsekd <dydwnsekd123@gmail.com>
2023-03-05 21:31:51 +0900

    백준 9658 돌게임4 로직 변경

--- baekjoon/python/9658.py
+++ baekjoon/python/9658.py
@@ -2,15 +2,16 @@ import sys
 
 n = int(sys.stdin.readline())
 
+# False -> CY 승, True -> SK 승
 result_list = [False] * 1001
-result_list[1] = True
-result_list[2] = False
-result_list[3] = True
+result_list[1] = False
+result_list[2] = True
+result_list[3] = False
 result_list[4] = True
-result_list[5] = True
+result_list[5] = False
 
 for i in range(6, 1001):
-    result_list[i] = False if any([result_list[i-1], result_list[i-3], result_list[i-4]]) else True
+    result_list[i] = True if any([result_list[i-1], result_list[i-3], result_list[i-4]]) else False
 
 if result_list[n]:
     print("SK")
