8bace211e367952f28db13af93f0b0dc435ca128
dydwnsekd <dydwnsekd123@gmail.com>
2023-03-06 00:37:48 +0900

    백준 9658 돌게임4 완료

--- baekjoon/python/9658.py
+++ baekjoon/python/9658.py
@@ -8,10 +8,10 @@ result_list[1] = False
 result_list[2] = True
 result_list[3] = False
 result_list[4] = True
-result_list[5] = False
+result_list[5] = True
 
 for i in range(6, 1001):
-    result_list[i] = True if any([result_list[i-1], result_list[i-3], result_list[i-4]]) else False
+    result_list[i] = False if all([result_list[i-1], result_list[i-3], result_list[i-4]]) else True
 
 if result_list[n]:
     print("SK")
