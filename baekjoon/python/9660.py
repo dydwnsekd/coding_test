d51102aa07098c876664d1d50309c6a3741d000a
dydwnsekd <dydwnsekd123@gmail.com>
2023-03-07 00:27:30 +0900

    백준 9660 돌게임6 python 완료

--- baekjoon/python/9660.py
+++ baekjoon/python/9660.py
@@ -1,19 +1,8 @@
-# TODO
 import sys
 
 n = int(sys.stdin.readline())
 
-result_list = [False] * (n+1)
-result_list[1] = True
-result_list[2] = False
-result_list[3] = True
-result_list[4] = True
-result_list[5] = True
-
-for i in range(6, n+1):
-    result_list[i] = False if all([result_list[i-1], result_list[i-3], result_list[i-4]]) else True
-
-if result_list[n]:
-    print("SK")
-else:
+if n % 7 == 0 or n % 7 == 2:
     print("CY")
+else:
+    print("SK")
