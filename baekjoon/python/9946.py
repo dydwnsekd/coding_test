87cfe7584e9f51981a00995314b602b5435b08bb
dydwnsekd <dydwnsekd123@gmail.com>
2023-03-14 02:21:07 +0900

    백준 9946 단어 퍼즐 python 완료

--- baekjoon/python/9946.py
+++ baekjoon/python/9946.py
@@ -1,9 +1,12 @@
 import sys
+from collections import defaultdict
 
 case_count = 1
 
 while True:
     flag = True
+    word_dict = defaultdict(int)
+    piece_dict = defaultdict(int)
     word = sys.stdin.readline().strip()
     piece = sys.stdin.readline().strip()
 
@@ -12,12 +15,15 @@ while True:
 
     if len(word) == len(piece):
         for w in word:
-            if w not in piece:
+            word_dict[w] += 1
+        for w in piece:
+            piece_dict[w] += 1
+
+        for k in word_dict.keys():
+            if word_dict[k] != piece_dict[k]:
                 flag = False
                 break
-            else:
-                del_index = piece.find(w)
-                del piece[del_index]
+
     else:
         flag = False
 
