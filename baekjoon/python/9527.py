7a877876e50f31ccacc695fa23c998953995afd7
dydwnsekd <dydwnsekd123@gmail.com>
2023-03-19 21:48:57 +0900

    백준 9527 1의 개수 세기 python 로직 변경 시간 초과

--- baekjoon/python/9527.py
+++ baekjoon/python/9527.py
@@ -16,14 +16,19 @@ def to_binary(n):
     binary_list.insert(0, str(n))
     return str(int(''.join(binary_list)))
 
+def count_one(n):
+    count = 0
+    while n:
+        n &= n-1
+        count += 1
+    return count
 
 start_num, end_num = map(int, sys.stdin.readline().split(" "))
 
 count = 0
 
 for i in range(start_num, end_num+1):
-    temp = to_binary(i)
-    count += temp.count("1")
+    count += count_one(i)
 
 print(count)
 
