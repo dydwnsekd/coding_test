dab6647adbab7622c486a587c36ed5dd7f9a3e52
dydwnsekd <dydwnsekd123@gmail.com>
2023-03-25 12:47:22 +0900

    백준 9527 1의 개수 세기 python 불필요 로직 제거

--- baekjoon/python/9527.py
+++ baekjoon/python/9527.py
@@ -4,20 +4,6 @@
 import sys
 import math
 
-def to_binary(n):
-
-    binary_list = list()
-    
-    while n > 1:
-        if math.floor(n % 2) == 1:
-            binary_list.insert(0, '1')
-        elif math.floor(n % 2) == 0:
-            binary_list.insert(0, '0')
-        n = math.floor(n / 2)
-
-    binary_list.insert(0, str(n))
-    return str(int(''.join(binary_list)))
-
 def count_one(n):
     count = 0
     while n:
@@ -33,5 +19,4 @@ for i in range(start_num, end_num+1):
     count += count_one(i)
 
 print(count)
-
 
