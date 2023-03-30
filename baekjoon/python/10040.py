778b73e5a989bb5acdca8cf2d76a8c8fceb99d5c
dydwnsekd <dydwnsekd123@gmail.com>
2023-03-31 01:11:18 +0900

    백준 10040 투표 python 로직 수정 틀림

--- baekjoon/python/10040.py
+++ baekjoon/python/10040.py
@@ -17,12 +17,12 @@ for _ in range(m):
 for m_score in m_list:
     for i in range(len(n_list)):
         if m_score >= n_list[i]:
-            vote_list.append(i)
+            vote_list.append(i+1)
             break
 
 for i in range(len(vote_list)):
     if result < vote_list.count(i):
-        result = vote_list.count(i)
+        result = i
 
 print(result)
 
