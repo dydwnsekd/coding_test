51f2f6569be20db2dcf27a1f5376ad9e7e397d8e
dydwnsekd <dydwnsekd123@gmail.com>
2023-04-07 01:47:14 +0900

    백준 10040 투표 python 로직 변경 테스트 실패

--- baekjoon/python/10040.py
+++ baekjoon/python/10040.py
@@ -1,4 +1,4 @@
-# TODO
+"""
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
@@ -27,4 +27,25 @@ for i in range(len(vote_list)):
         result = vote_list[i]
 
 print(max_index)
+"""
 
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+costs = [int(sys.stdin.readline()) for _ in range(n)]
+votes = [0] * n
+
+for i in range(m):
+    song_cost = int(sys.stdin.readline())
+    min_cost, min_idx = float('inf'), -1
+    for j in range(n):
+        if costs[j] <= song_cost and costs[j] < min_cost:
+            min_cost, min_idx = costs[j], j
+    if min_idx >= 0:
+        votes[min_idx] += 1
+
+max_idx = 0
+for i in range(1, n):
+    if votes[i] > votes[max_idx]:
+        max_idx = i
+print(max_idx+1)
