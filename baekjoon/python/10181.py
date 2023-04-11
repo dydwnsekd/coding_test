83598c1018acab5862d374384aef04b47f6eba67
dydwnsekd <dydwnsekd123@gmail.com>
2023-04-12 01:29:56 +0900

    백준 10181 Federation Favorites python init

---
+++ baekjoon/python/10181.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def divisor_list(num):
+    divisor_list = []
+
+    for i in range(1, num):
+        if num % i == 0:
+            divisor_list.append(i)
+
+    return divisor_list
+
+
+while True:
+    num = int(sys.stdin.readline())
+
+    if num == -1:
+        break
+
+    if sum(divisor_list(num)) == num:
+        print(f"{num} = ", end="")
+        print(" + ".join(divisor_list(num)))
+    else:
+        print(f"{num} is NOT perfect.")
+
