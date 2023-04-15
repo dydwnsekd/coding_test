4a59e9814e0a363c61db861243a1110e4ac08602
dydwnsekd <dydwnsekd123@gmail.com>
2023-04-15 22:20:43 +0900

    백준 10162 전자레인지 python 로직 개선1

--- baekjoon/python/10162.py
+++ baekjoon/python/10162.py
@@ -1,4 +1,3 @@
-#TODO
 import sys
 
 t1 = 300
@@ -7,14 +6,14 @@ t3 = 10
 
 t = int(sys.stdin.readline())
 
-c1 = t // t1
-t = t % t1
-c2 = t // t2
-t = t % t2
-c3 = t // t3
-t = t % t3
+if t % t3 == 0:
+    c1 = t // t1
+    t = t % t1
+    c2 = t // t2
+    t = t % t2
+    c3 = t // t3
+    t = t % t3
 
-if t == 0:
     print(f"{c1} {c2} {c3}")
 else:
     print(-1)
