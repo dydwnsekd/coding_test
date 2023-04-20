659a26d93646cb8ed78a1dbfbf8999d294b698fd
dydwnsekd <dydwnsekd123@gmail.com>
2023-04-21 01:20:08 +0900

    백준 10176 Opposite Words python 완료 동일한 alphbet 이 있는 경우 고려 필요

--- baekjoon/python/10176.py
+++ baekjoon/python/10176.py
@@ -1,8 +1,27 @@
 import sys
 
+def get_reverse_alphabet(alphabet):
+    alphabet_list = list("-abcdefghijklmnopqrstuvwxyz")
+    alphabet_len = len(alphabet_list)
+    alphabet_index = alphabet_list.index(alphabet)
+    if alphabet_index < 14:
+        return alphabet_list[-alphabet_index]
+    else:
+        return alphabet_list[alphabet_len-alphabet_index]
+
 cases = int(sys.stdin.readline())
-reverse_words = list("abcdefghijklmnopqrstuvwxyz")
-print(reverse_words)
 
 for _ in range(cases):
-    words = sys.stdin.readline().strip().lower()
+    flag = True
+    reverse_word = ""
+    word = sys.stdin.readline().strip().lower()
+
+    for w in word:
+        if get_reverse_alphabet(w) not in word:
+            flag = False
+            break
+
+    if flag:
+        print("Yes")
+    else:
+        print("No")
