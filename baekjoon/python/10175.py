b650ff73fff564586d128a0eb60e34e1617abc04
dydwnsekd <dydwnsekd123@gmail.com>
2023-04-23 21:05:50 +0900

    백준 10175 Dominant Species python 완료

--- baekjoon/python/10175.py
+++ baekjoon/python/10175.py
@@ -3,6 +3,7 @@ from collections import Counter
 
 cases = int(sys.stdin.readline())
 weight_dict = {"B": 2, "C": 1, "M": 4, "W": 3}
+animal_dict = {"B": "Bobcat", "C": "Coyote", "M": "Mountain Lion", "W": "Wolf"}
 
 for _ in range(cases):
     result_dict = {}
@@ -13,4 +14,8 @@ for _ in range(cases):
     for k, v in count_species.items():
         result_dict[k] = v * weight_dict[k]
 
-    print(result_dict)
+    sorted_list = sorted(result_dict.items(), key=lambda x: x[1], reverse=True)
+    if sorted_list[0][1] == sorted_list[1][1]:
+        print(f"{region}: There is no dominant species")
+    else:
+        print(f"{region}: The {animal_dict[sorted_list[0][0]]} is the dominant species")
