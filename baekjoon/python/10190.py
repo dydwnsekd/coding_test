1f105cd35075393a38cfe21cf5dea9f6f55a57a7
dydwnsekd <dydwnsekd123@gmail.com>
2023-04-27 00:20:19 +0900

    백준 10190 Acronyms python 완료

--- baekjoon/python/10190.py
+++ baekjoon/python/10190.py
@@ -5,7 +5,12 @@ cases = int(sys.stdin.readline())
 for _ in range(cases):
     match_text, temp_case = sys.stdin.readline().split()
     temp_case = int(temp_case)
+    print(match_text)
 
     for _ in range(temp_case):
-        text = sys.stdin.readline().replace("\n", "")
-        print("".join(text.split(" ")))
+        text = sys.stdin.readline().strip()
+        split_text = text.split()
+
+        head_text = ''.join([t[0] for t in split_text])
+        if match_text == head_text:
+            print(text)
