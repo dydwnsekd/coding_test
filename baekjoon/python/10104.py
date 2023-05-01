a66e345db74aeb1e44ddaef315d6b08c5c66a4a1
dydwnsekd <dydwnsekd123@gmail.com>
2023-05-02 00:14:11 +0900

    백준 10104 Party Invitation python init

---
+++ baekjoon/python/10104.py
@@ -0,0 +1,24 @@
+import sys
+
+human_count = int(sys.stdin.readline())
+human_list = [i for i in range(human_count+1)]
+
+cases = int(sys.stdin.readline())
+
+for _ in range(cases):
+    index = int(sys.stdin.readline())
+    remove_index = []
+
+    for i in range(1, len(human_list)):
+        if i % index == 0:
+            remove_index.append(i)
+
+    remove_index.reverse()
+
+    for i in remove_index:
+        human_list.remove(i)
+
+for h in human_list[1:]:
+    print(h)
+
+
