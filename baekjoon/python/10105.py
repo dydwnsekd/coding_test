7ecc906080afadd356b1ce16c9299e5bd6b5c1d4
dydwnsekd <dydwnsekd123@gmail.com>
2023-05-01 20:40:46 +0900

    백준 10105 Assigning Partners python 완료

--- baekjoon/python/10105.py
+++ baekjoon/python/10105.py
@@ -9,12 +9,16 @@ cases = int(sys.stdin.readline())
 
 human_list1 = sys.stdin.readline().strip().split()
 human_list2 = sys.stdin.readline().strip().split()
+flag = True
 
 for i in range(cases):
     partner_dict1[human_list1[i]] = human_list2[i]
     partner_dict2[human_list2[i]] = human_list1[i]
 
-if partner_dict1 == partner_dict2:
+    if human_list1[i] == human_list2[i]:
+        flag = False
+
+if partner_dict1 == partner_dict2 and flag:
     print("good")
 else:
     print("bad")
