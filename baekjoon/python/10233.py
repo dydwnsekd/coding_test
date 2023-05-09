27ea448d2765b3295e774804244cbcf3390569b4
dydwnsekd <dydwnsekd123@gmail.com>
2023-05-10 01:26:24 +0900

    백준 10233 Tax Rate Changed python 로직 변경

--- baekjoon/python/10233.py
+++ baekjoon/python/10233.py
@@ -9,23 +9,24 @@ while True:
     else:
         for i in range(1, s//2):
             j = s - i
-            i_origin = math.ceil(i*100/(100+x))
-            j_origin = math.ceil(j*100/(100+x))
+            i_origin = math.floor(i*100/(100+x))
+            j_origin = math.floor(j*100/(100+x))
 
-            while True:
-                j_temp = math.floor(j_origin * (100 + x) / 100)
+            for i_temp in range(i_origin, i_origin + 2):
+                while True:
+                    j_temp = math.floor(j_origin * (100 + x) / 100)
 
-                if i + j_temp == s:
-                    i_value = math.floor(i_origin * (100 + y) / 100)
-                    j_value = math.floor(j_origin * (100 + y) / 100)
+                    if i_temp + j_temp == s:
+                        i_value = math.floor(i_temp * (100 + y) / 100)
+                        j_value = math.floor(i_temp * (100 + y) / 100)
 
-                    temp = i_value + j_value
+                        temp = i_value + j_value
 
-                    if temp > max_value:
-                        max_value = temp
-                elif i + j_temp > s:
-                    break
+                        if temp > max_value:
+                            max_value = temp
+                    elif i + j_temp > s:
+                        break
 
-                j_origin += 1
+                    j_origin += 1
 
         print(max_value)
