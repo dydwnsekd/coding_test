b4b13bab35011c7629c954191a509306695b2505
dydwnsekd <dydwnsekd123@gmail.com>
2023-05-19 00:58:14 +0900

    백준 25206 너의 평점은 python init

---
+++ baekjoon/python/25206.py
@@ -0,0 +1,14 @@
+import sys
+
+grade_dict = {"A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
+result_grade = 0
+sum_grade = 0
+
+for _ in range(20):
+    subject, grade, grade_score = sys.stdin.readline().strip().split()
+    grade = float(grade)
+
+    result_grade += grade * grade_dict[grade_score]
+    sum_grade = grade
+
+print(result_grade/sum_grade)
