ccf5edc5fea6968629c5914af16729bd9dfb9b09
dydwnsekd <dydwnsekd123@gmail.com>
2023-05-23 00:53:49 +0900

    백준 2501 약수 구하기 python 완료

---
+++ baekjoon/python/2501.py
@@ -0,0 +1,13 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+count = 0
+
+for i in range(1, n+1):
+    if n % i == 0:
+        count += 1
+        if count == k:
+            print(i)
+            exit()
+
+print(0)
