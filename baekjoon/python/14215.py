e31e68e31c0d5738ee39c7fc5ceae17db7f0f308
dydwnsekd <dydwnsekd123@gmail.com>
2023-06-05 00:14:40 +0900

    백준 14215 세 막대 python init

---
+++ baekjoon/python/14215.py
@@ -0,0 +1,9 @@
+import sys
+
+length_list = list(map(int, sys.stdin.readline().split()))
+length_list.sort()
+
+if length_list[0] == length_list[2]:
+    print(length_list[0] * 3)
+else:
+    print(length_list[0] + length_list[1] + (length_list[0] + length_list[1] - 1))
