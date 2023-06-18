5521c9341997c744c79baf92396e3b2d24e4a559
dydwnsekd <dydwnsekd123@gmail.com>
2023-06-18 23:26:42 +0900

    백준 17577 Scores of Final Examination python 로직 수정

--- baekjoon/python/17577.py
+++ baekjoon/python/17577.py
@@ -1,17 +1,23 @@
 import sys
 
+score_list = []
+
 while True:
     n, m = map(int, sys.stdin.readline().split())
+    max_sum = 0
 
     if n == 0 and m == 0:
         break
 
-    max_sum = 0
-
     for _ in range(m):
-        score_list = list(map(int, sys.stdin.readline().split()))
-        if max_sum < sum(score_list):
-            max_sum = sum(score_list)
+        score_list.append(list(map(int, sys.stdin.readline().split())))
+
+    for i in range(n):
+        temp = 0
+        for j in range(m):
+            temp += score_list[j][i]
+            if temp > max_sum:
+                max_sum = temp
 
     print(max_sum)
 
