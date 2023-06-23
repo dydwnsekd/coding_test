5f1e1559967cd6547d4ad744a42aba9cf80938f6
dydwnsekd <dydwnsekd123@gmail.com>
2023-06-23 21:02:10 +0900

    백준 24831 Two Rabbits python 완료

--- baekjoon/python/24831.py
+++ baekjoon/python/24831.py
@@ -5,15 +5,11 @@ cases = int(sys.stdin.readline())
 for _ in range(cases):
     x, y, a, b = map(int, sys.stdin.readline().split())
     times = 1
-    while True:
-        if x+a == y-b:
-            print(times)
-            break
-        elif x+a > y-b:
-            print(-1)
-            break
-        else:
-            x += a
-            y -= b
-            times += 1
 
+    distance = y - x
+    jump_distance = a + b
+
+    if distance % jump_distance == 0:
+        print(distance // jump_distance)
+    else:
+        print(-1)
