dcb0d866084573c0c093d343111f70b4dee5fd9c
dydwnsekd <dydwnsekd123@gmail.com>
2023-06-27 00:19:39 +0900

    백준 7581 Cuboids python 완료 로직 개선 필요

--- baekjoon/python/7581.py
+++ baekjoon/python/7581.py
@@ -7,11 +7,16 @@ while True:
         break
 
     if values[3] == 0:
-        print(values[0] * values[1] * values[2])
+        values[3] = values[0] * values[1] * values[2]
     else:
         temp = 1
         for i in values[:3]:
             if i != 0:
                 temp *= i
 
-        print(values[3] // temp)
+        change_index = values.index(0)
+        values[change_index] = values[3] // temp
+
+    for i in values:
+        print(i, end=" ")
+    print()
