9c4096e804ed35b575b806a902cf5ef4fa072880
dydwnsekd <dydwnsekd123@gmail.com>
2023-07-12 01:46:52 +0900

    백준 26512 Complimentary python 완료

--- baekjoon/python/26512.py
+++ baekjoon/python/26512.py
@@ -1,5 +1,12 @@
 import sys
 
+def twos_complement(decimal_number, length):
+    bin_number = bin(decimal_number)[2:].zfill(length)
+    temp_number = ''.join('1' if b == '0' else '0' for b in bin_number)
+    return_number = bin(int("0b" + temp_number, 2) + 1)[2:].zfill(length)
+
+    return return_number
+
 while True:
     x, y = map(int, sys.stdin.readline().split())
 
@@ -11,20 +18,12 @@ while True:
 
     print(f"{x} = {bin(x)[2:].zfill(8)}")
     print(f"{y} = {bin(y)[2:].zfill(8)}")
+    print(f"-{x} = {twos_complement(x, 8)}")
+    print(f"-{y} = {twos_complement(y, 8)}")
+    if x >= y:
+        print(f"{x-y} = {bin(x-y)[2:].zfill(8)}")
+    else:
+        print(f"{x-y} = {twos_complement(y-x, 8)}")
 
-    bin2_x = ""
-    for i in bin_x:
-        if i == "0":
-            bin2_x += "1"
-        elif i == "1":
-            bin2_x += "0"
+    print()
 
-    bin2_y = ""
-    for i in bin_y:
-        if i == "0":
-            bin2_y += "1"
-        elif i == "1":
-            bin2_y += "0"
-
-    print(f"-{x} = {bin2_x}")
-    print(f"-{y} = {bin2_y}")
