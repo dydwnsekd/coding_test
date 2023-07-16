5a82ee3f8366b1b3e4333ff32109ed5dd53a31e6
dydwnsekd <dydwnsekd123@gmail.com>
2023-07-16 17:25:14 +0900

    백준 23808 골뱅이 찍기 - ㅂ python 완료

---
+++ baekjoon/python/23808.py
@@ -0,0 +1,17 @@
+import sys
+
+def print_head(n):
+    for i in range(n):
+        print("@"*n + " "*3*n + "@"*n)
+
+def print_body(n):
+    for i in range(n):
+        print("@" * 5 * n)
+
+n = int(sys.stdin.readline())
+
+print_head(n)
+print_head(n)
+print_body(n)
+print_head(n)
+print_body(n)
