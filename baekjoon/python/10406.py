0e848fde1e58664ba143ef4d8a199b0f7a97648b
dydwnsekd <dydwnsekd123@gmail.com>
2023-07-24 00:09:33 +0900

    백준 10406 The fellowship of the ring python 완료

--- baekjoon/python/10406.py
+++ baekjoon/python/10406.py
@@ -6,7 +6,7 @@ w, n, p = map(int, sys.stdin.readline().split())
 punch_list = list(map(int, sys.stdin.readline().split()))
 
 for punch in punch_list:
-    if punch < w or punch > n:
+    if w <= punch <= n:
         count += 1
 
 print(count)
