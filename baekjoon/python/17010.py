1858c1190375995bf298219f823220024e1001b4
dydwnsekd <dydwnsekd123@gmail.com>
2023-07-26 00:37:02 +0900

    백준 17010 Time to Decompress python 완료 2개 결과 동일

--- baekjoon/python/17010.py
+++ baekjoon/python/17010.py
@@ -3,5 +3,6 @@ import sys
 cases = int(sys.stdin.readline())
 
 for _ in range(cases):
-    n, x = sys.stdin.readline().rstrip().split()
+    n, x = sys.stdin.readline().strip().split()
+    # n, x = sys.stdin.readline().rstrip().split()
     print(x * int(n))
