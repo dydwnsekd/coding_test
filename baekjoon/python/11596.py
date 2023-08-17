03da3f224b585b58ff25f31fe76257f8ce4ffc3f
dydwnsekd <dydwnsekd123@gmail.com>
2023-08-18 00:22:39 +0900

    백준 11596 Triangle python init

---
+++ baekjoon/python/11596.py
@@ -0,0 +1,19 @@
+import sys
+
+flag = True
+triangle_1 = list(map(int, sys.stdin.readline().split()))
+triangle_2 = list(map(int, sys.stdin.readline().split()))
+
+triangle_1.sort()
+triangle_2.sort()
+
+if triangle_1[0]**2 + triangle_1[1]**2 != triangle_1[2]**2:
+    flag = False
+
+if triangle_2[0]**2 + triangle_2[1]**2 != triangle_2[2]**2:
+    flag = False
+
+if flag:
+    print("YES")
+else:
+    print("NO")
