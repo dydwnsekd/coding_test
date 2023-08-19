f51b0d69499d168a37e77b2718e1dd69d48c1d2c
dydwnsekd <dydwnsekd123@gmail.com>
2023-08-20 01:58:48 +0900

    백준 26584 Playtime python init

---
+++ baekjoon/python/26584.py
@@ -0,0 +1,30 @@
+import sys
+
+cases = int(sys.stdin.readline())
+
+min_hour = 60
+min_day = min_hour * 24
+min_year = min_day * 365
+
+for _ in range(cases):
+    game_name, playtime = sys.stdin.readline().strip().split(",")
+    playtime = int(playtime)
+
+    result_text = ""
+
+    if playtime // min_year > 0:
+        result_text += f"{playtime // min_year} hour(s) "
+        playtime = playtime % min_year
+
+    if playtime // min_day > 0:
+        result_text += f"{playtime // min_day} day(s) "
+        playtime = playtime % min_day
+
+    if playtime // min_hour > 0:
+        result_text += f"{playtime // min_hour} hour(s) "
+        playtime = playtime % min_hour
+
+    if playtime > 0:
+        result_text += f"{playtime} minutes(s)"
+
+    print(result_text)
