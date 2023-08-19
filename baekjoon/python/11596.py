7ef87be78c02fbe63da73f4c18b0659d216ecb7b
dydwnsekd <dydwnsekd123@gmail.com>
2023-08-19 11:46:15 +0900

    백준 11596 Triangle python 완료

--- baekjoon/python/11596.py
+++ baekjoon/python/11596.py
@@ -13,6 +13,9 @@ if triangle_1[0]**2 + triangle_1[1]**2 != triangle_1[2]**2:
 if triangle_2[0]**2 + triangle_2[1]**2 != triangle_2[2]**2:
     flag = False
 
+if triangle_1[0] != triangle_2[0] or triangle_1[1] != triangle_2[1]:
+    flag = False
+
 if flag:
     print("YES")
 else:
