5dbda72246ab2d8a12e8c0219e60296ac31f27ba
dydwnsekd <dydwnsekd123@gmail.com>
2023-08-24 00:08:15 +0900

    백준 26584 Playtime python 로직 변경

--- baekjoon/python/26584.py
+++ baekjoon/python/26584.py
@@ -2,9 +2,7 @@ import sys
 
 cases = int(sys.stdin.readline())
 
-min_hour = 60
-min_day = min_hour * 24
-min_year = min_day * 365
+time_dict = {"year(s)": 60 * 24 * 365, "day(s)": 60 * 24, "hour(s)": 60, "minute(s)": 1}
 
 for _ in range(cases):
     game_name, playtime = sys.stdin.readline().strip().split(",")
@@ -12,19 +10,9 @@ for _ in range(cases):
 
     result_text = ""
 
-    if playtime // min_year > 0:
-        result_text += f"{playtime // min_year} year(s) "
-        playtime = playtime % min_year
-
-    if playtime // min_day > 0:
-        result_text += f"{playtime // min_day} day(s) "
-        playtime = playtime % min_day
-
-    if playtime // min_hour > 0:
-        result_text += f"{playtime // min_hour} hour(s) "
-        playtime = playtime % min_hour
-
-    if playtime > 0:
-        result_text += f"{playtime} minute(s)"
+    for k, v in time_dict.items():
+        if playtime // v > 0:
+            result_text += f"{playtime // v} {k} "
+            playtime = playtime % v
 
     print(game_name + " - " + result_text)
