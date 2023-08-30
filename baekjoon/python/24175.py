32570707e39d417b37d901424e5b0bbd2fd63e9b
dydwnsekd <dydwnsekd123@gmail.com>
2023-08-31 01:05:19 +0900

    백준 24175 Tokyo2020 python 로직 수정

--- baekjoon/python/24175.py
+++ baekjoon/python/24175.py
@@ -19,5 +19,24 @@ while True:
 
     sorted_gold = sorted(gold_dict.items(), key=lambda item: item[1], reverse=True)
     sorted_medal = sorted(medal_dict.items(), key=lambda item: item[1], reverse=True)
+    gold_max = sorted_gold[0][1]
+    medal_max = sorted_medal[0][1]
 
-    print(sorted_gold[0][0], sorted_medal[0][0])
+    gold_year = 9999
+    medal_year = 9999
+
+    for i in sorted_gold:
+        if i[1] == gold_max:
+            if gold_year > int(i[0]):
+                gold_year = int(i[0])
+            else:
+                break
+
+    for i in sorted_medal:
+        if i[1] == medal_max:
+            if medal_year > int(i[0]):
+                medal_year = int(i[0])
+            else:
+                break
+
+    print(gold_year, medal_year)
