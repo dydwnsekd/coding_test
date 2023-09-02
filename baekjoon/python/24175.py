0840eb953a36102ba129d32b3068fb6eb63e1782
dydwnsekd <dydwnsekd123@gmail.com>
2023-09-02 13:13:00 +0900

    백준 24175 Tokyo2020 python 풀이 완료 수정 필요

--- baekjoon/python/24175.py
+++ baekjoon/python/24175.py
@@ -2,7 +2,7 @@
 
 import sys
 from collections import defaultdict
-
+"""
 while True:
     n = int(sys.stdin.readline())
 
@@ -41,3 +41,47 @@ while True:
                 break
 
     print(gold_year, medal_year)
+"""
+
+while True:
+    n = int(sys.stdin.readline())
+
+    if n == 0:
+        break
+
+    gold_list = []
+    medal_list = []
+
+    for _ in range(n):
+        year, event, medal = sys.stdin.readline().strip().split()
+        if medal == "Gold":
+            gold_list.append(year)
+
+        medal_list.append(year)
+
+    set_gold = set(gold_list)
+    set_medal = set(medal_list)
+
+    max_gold = 0
+    gold_year = 9999
+    max_medal = 0
+    medal_year = 9999
+
+    for g in set_gold:
+        if gold_list.count(g) > max_gold:
+            gold_year = g
+            max_gold = gold_list.count(g)
+        elif gold_list.count(g) == max_gold:
+            if gold_year > g:
+                gold_year = g
+
+    for m in set_medal:
+        if medal_list.count(m) > max_medal:
+            medal_year = m
+            max_medal = medal_list.count(m)
+        elif medal_list.count(m) == max_medal:
+            if medal_year > m:
+                medal_year = m
+
+    print(gold_year, medal_year)
+
