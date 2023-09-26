31075c7083ffec78ef2f03de9f27bc506c62d16f
dydwnsekd <dydwnsekd123@gmail.com>
2023-09-26 22:24:53 +0900

    백준 3034 앵그리 창영 python init

---
+++ baekjoon/python/3034.py
@@ -0,0 +1,12 @@
+# init
+import sys, math
+
+n, w, h = list(map(int, sys.stdin.readline().split()))
+max_value = math.sqrt(w**2 + h**2)
+
+for _ in range(n):
+    if int(sys.stdin.readline()) < n:
+        print("DA")
+    else:
+        print("NE")
+
