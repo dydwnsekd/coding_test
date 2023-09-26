2e68371730f26b10cdb7a9edb43b60c394bd09e3
dydwnsekd <dydwnsekd123@gmail.com>
2023-09-27 00:08:11 +0900

    백준 3034 앵그리 창영 python 완료

--- baekjoon/python/3034.py
+++ baekjoon/python/3034.py
@@ -1,11 +1,10 @@
-# init
 import sys, math
 
 n, w, h = list(map(int, sys.stdin.readline().split()))
 max_value = math.sqrt(w**2 + h**2)
 
 for _ in range(n):
-    if int(sys.stdin.readline()) < n:
+    if int(sys.stdin.readline()) <= max_value:
         print("DA")
     else:
         print("NE")
