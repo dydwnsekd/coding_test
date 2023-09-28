ff8476547b78f9ce3d870fb2f570bfb1cb5c9988
dydwnsekd <dydwnsekd123@gmail.com>
2023-09-28 14:43:45 +0900

    백준 28235 코드마스터 2023 python 완료

---
+++ baekjoon/python/28235.py
@@ -0,0 +1,13 @@
+import sys
+
+text = sys.stdin.readline().strip()
+
+if text == "SONGDO":
+    print("HIGHSCHOOL")
+elif text == "CODE":
+    print("MASTER")
+elif text == "2023":
+    print("0611")
+elif text == "ALGORITHM":
+    print("CONTEST")
+
