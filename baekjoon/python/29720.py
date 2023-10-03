91f0aabafc30dd5b2a9ceda711f91c1b62d2198c
dydwnsekd <dydwnsekd123@gmail.com>
2023-10-04 00:09:30 +0900

    백준 29720 그래서 님 푼 문제 수가? python 완료

--- baekjoon/python/29720.py
+++ baekjoon/python/29720.py
@@ -1,15 +1,12 @@
 import sys
 
-min_value, max_value = 0, 0
 n, m, k = list(map(int, sys.stdin.readline().split()))
 
-solve_problem = m * k
-
-if n > solve_problem:
-    min_value = n - solve_problem
-    max_value = min_value + m - 1
-elif n < solve_problem:
+if n - m * k > 0:
+    min_value = n - m * k
+else:
     min_value = 0
-    max_value = min_value + m - 1
+
+max_value = n - m * (k -1) - 1
 
 print(min_value, max_value)
