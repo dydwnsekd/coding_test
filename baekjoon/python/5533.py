3e7b7d9eec2b68715bf153cbd3d26a7e82379b98
dydwnsekd <dydwnsekd123@gmail.com>
2023-10-12 09:57:53 +0900

    백준 5533 유니크 python 로직 수정

--- baekjoon/python/5533.py
+++ baekjoon/python/5533.py
@@ -1,15 +1,19 @@
 import sys
 
+temp_score = []
 n = int(sys.stdin.readline())
-score_list = [0, 0, 0]
+
+score_list = [0] * n
 
 for _ in range(n):
-    temp_score = list(map(int, sys.stdin.readline().split()))
+    temp_score.append(list(map(int, sys.stdin.readline().split())))
 
-    for i in range(3):
-        if temp_score.count(temp_score[i]) == 1:
-            score_list[i] += temp_score[i]
+pivot_score = [[row[i] for row in temp_score] for i in range(3)]
 
 for i in range(n):
-    print(i)
+    for j in range(3):
+        if pivot_score[i].count(pivot_score[i][j]) == 1:
+            score_list[i] += pivot_score[i][j]
+
+print(score_list)
 
