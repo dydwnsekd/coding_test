941cbe94e965f171e39bbcb8bcc892e9e8d42f2f
dydwnsekd <dydwnsekd123@gmail.com>
2023-10-13 21:35:16 +0900

    백준 5533 유니크 python 완료

--- baekjoon/python/5533.py
+++ baekjoon/python/5533.py
@@ -12,8 +12,8 @@ pivot_score = [[row[i] for row in temp_score] for i in range(3)]
 
 for i in range(n):
     for j in range(3):
-        if pivot_score[i].count(pivot_score[i][j]) == 1:
-            score_list[i] += pivot_score[i][j]
+        if pivot_score[j].count(pivot_score[j][i]) == 1:
+            score_list[i] += pivot_score[j][i]
 
-print(score_list)
-
+for s in score_list:
+    print(s)
