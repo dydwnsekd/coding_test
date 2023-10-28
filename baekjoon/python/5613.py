9b3431088e398fb7394378793e52a2882e069f8b
dydwnsekd <dydwnsekd123@gmail.com>
2023-10-28 17:45:00 +0900

    백준 5613 계산기 프로그램 python init

---
+++ baekjoon/python/5613.py
@@ -0,0 +1,26 @@
+import sys
+
+result = 0
+operator = ""
+
+is_first = True
+sequence = 0
+
+while True:
+    s = sys.stdin.readline().strip()
+
+    if sequence == 0:
+        result = int(s)
+    elif sequence % 2 == 1:
+        operator = s
+    elif sequence % 2 == 0:
+        if operator == "+":
+            result += int(s)
+        elif operator == "-":
+            result -= int(s)
+        elif operator == "*":
+            result *= int(s)
+        elif operator == "/":
+            result //= int(s)
+        elif operator == "=":
+            print(result)
