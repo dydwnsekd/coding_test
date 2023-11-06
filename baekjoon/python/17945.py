ccf202607a163adf38f730c8099da0252c3950c1
dydwnsekd <dydwnsekd123@gmail.com>
2023-11-07 00:44:07 +0900

    백준 17945 통학의 신 python 완료

--- baekjoon/python/17945.py
+++ baekjoon/python/17945.py
@@ -8,4 +8,11 @@ b, c = map(int, sys.stdin.readline().split())
 answer.append(int(((-2*b) + math.sqrt(((2*b) ** 2) - 4*c)) // 2))
 answer.append(int(((-2*b) - math.sqrt(((2*b) ** 2) - 4*c)) // 2))
 
-print(answer)
+answer.sort()
+
+if answer[0] == answer[1]:
+    print(answer[0])
+
+else:
+    print(*answer)
+
