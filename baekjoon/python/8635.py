d39afb6c19ceb2cab88d787e358c1e636fd8a89f
dydwnsekd <dydwnsekd123@gmail.com>
2023-11-22 00:26:17 +0900

    백준 8635 Zliczacz liter python 완료

--- baekjoon/python/8635.py
+++ baekjoon/python/8635.py
@@ -2,14 +2,25 @@ import sys
 from collections import Counter
 
 cases = int(sys.stdin.readline())
-all_str = ""
+upper_str = ""
+lower_str = ""
 
 for _ in range(cases):
-    all_str += sys.stdin.readline().strip().replace(" ", "")
+    s = sys.stdin.readline().strip().replace(" ", "")
+    for i in s:
+        if ord("a") <= ord(i) <= ord("z"):
+            lower_str += i
+        else:
+            upper_str += i
 
-count_str = Counter(all_str)
-sorted_dict = sorted(count_str.items())
+count_lower_str = Counter(lower_str)
+count_upper_str = Counter(upper_str)
+sorted_lower_dict = sorted(count_lower_str.items())
+sorted_upper_dict = sorted(count_upper_str.items())
 
-for i in sorted_dict:
+for i in sorted_lower_dict:
+    print(i[0], i[1])
+
+for i in sorted_upper_dict:
     print(i[0], i[1])
 
