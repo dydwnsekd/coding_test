f24909d9b9cc2443d97446c03d5a490070e41c45
dydwnsekd <dydwnsekd123@gmail.com>
2023-11-24 10:00:27 +0900

    백준 11772 POT python 완료

--- baekjoon/python/11772.py
+++ baekjoon/python/11772.py
@@ -4,7 +4,7 @@ result = 0
 cases = int(sys.stdin.readline())
 
 for _ in range(cases):
-    temp = sys.stdin.readline().split()
+    temp = sys.stdin.readline().strip()
 
     result += int(temp[:-1]) ** int(temp[-1])
 
