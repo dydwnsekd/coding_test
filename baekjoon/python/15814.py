31a2a62d3ea3176d1b5cedc2c2af7a3382623dd8
dydwnsekd <dydwnsekd123@gmail.com>
2023-11-26 13:04:51 +0900

    백준 15814 야바위 대장 python 완료

--- baekjoon/python/15814.py
+++ baekjoon/python/15814.py
@@ -6,8 +6,8 @@ cases = int(sys.stdin.readline())
 for _ in range(cases):
     n1, n2 = map(int, sys.stdin.readline().split())
 
-    temp = s[n1+1]
-    s[n1+1] = "a"
-    s[n2+1] = temp
+    temp = s[n1]
+    s[n1] = s[n2]
+    s[n2] = temp
 
 print("".join(s))
