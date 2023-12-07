a388933cfb0f931c5808773d6e702466a1cb743c
dydwnsekd <dydwnsekd123@gmail.com>
2023-12-08 00:35:59 +0900

    백준 29483 Химия python 로직 변경

--- baekjoon/python/29483.py
+++ baekjoon/python/29483.py
@@ -5,11 +5,13 @@ result = 0
 s = sys.stdin.readline().strip()
 atom_dict = {"H": 1, "C": 12, "N": 14, "O": 16}
 
-for i in range(len(s)):
+for i in range(len(s)-1):
     if s[i] in atom_dict.keys():
         if s[i+1] not in atom_dict.keys():
             result += int(s[i+1]) * atom_dict[s[i]]
         else:
+            if i == len(s)-1:
+                result += atom_dict[s[i+1]]
             result += atom_dict[s[i]]
 
 print(result)
