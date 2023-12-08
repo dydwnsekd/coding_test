64e0b1b0f3135988d90e5e269e450278d7346160
dydwnsekd <dydwnsekd123@gmail.com>
2023-12-09 08:31:14 +0900

    백준 29483 Химия python 완료

--- baekjoon/python/29483.py
+++ baekjoon/python/29483.py
@@ -5,13 +5,13 @@ result = 0
 s = sys.stdin.readline().strip()
 atom_dict = {"H": 1, "C": 12, "N": 14, "O": 16}
 
-for i in range(len(s)-1):
+for i in range(len(s)):
     if s[i] in atom_dict.keys():
-        if s[i+1] not in atom_dict.keys():
+        if i == len(s)-1:
+            result += atom_dict[s[i]]
+        elif s[i+1] not in atom_dict.keys():
             result += int(s[i+1]) * atom_dict[s[i]]
         else:
-            if i == len(s)-1:
-                result += atom_dict[s[i+1]]
             result += atom_dict[s[i]]
 
 print(result)
