1b7bf4aee4bf471626fa70868111d2bd651bda9f
dydwnsekd <dydwnsekd123@gmail.com>
2023-12-10 09:54:21 +0900

    백준 29483 Химия python 로직 수정

--- baekjoon/python/29483.py
+++ baekjoon/python/29483.py
@@ -7,11 +7,9 @@ atom_dict = {"H": 1, "C": 12, "N": 14, "O": 16}
 
 for i in range(len(s)):
     if s[i] in atom_dict.keys():
-        if i == len(s)-1:
+        if i == len(s)-1 or s[i+1] in atom_dict.keys():
             result += atom_dict[s[i]]
         elif s[i+1] not in atom_dict.keys():
             result += int(s[i+1]) * atom_dict[s[i]]
-        else:
-            result += atom_dict[s[i]]
 
 print(result)
