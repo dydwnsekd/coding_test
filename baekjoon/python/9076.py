0cec22e3abe51a24e36a46899506a570b49ae609
dydwnsekd <dydwnsekd123@gmail.com>
2023-12-21 00:03:56 +0900

    백준 9076 점수 집계 python 완료

--- baekjoon/python/9076.py
+++ baekjoon/python/9076.py
@@ -5,7 +5,7 @@ cases = int(sys.stdin.readline())
 for _ in range(cases):
     score_list = list(map(int, sys.stdin.readline().split()))
     score_list = sorted(score_list)
-    if score_list[-2] - score_list[2] >= 4:
+    if score_list[-2] - score_list[1] >= 4:
         print("KIN")
     else:
-        print(sum(score_list[2:-2]))
+        print(sum(score_list[1:-1]))
