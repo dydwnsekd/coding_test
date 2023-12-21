c7355538be606a153b8685cc49f680ea01314876
dydwnsekd <dydwnsekd123@gmail.com>
2023-12-22 02:12:09 +0900

    백준 23348 스트릿 코딩 파이터 python init

---
+++ baekjoon/python/23348.py
@@ -0,0 +1,17 @@
+import sys
+
+max_score = 0
+coding_score = list(map(int, sys.stdin.readline().split()))
+
+cases = int(sys.stdin.readline())
+
+for _ in range(cases):
+    temp = 0
+    scores = list(map(int, sys.stdin.readline().split()))
+    for i in range(0, 3):
+        temp += scores[i] * coding_score[i]
+
+    if max_score < temp:
+        max_score = temp
+
+print(max_score)
