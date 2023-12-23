cd227c7a813cc28db943d09a9f1f2e932c2b27f5
dydwnsekd <dydwnsekd123@gmail.com>
2023-12-23 21:14:26 +0900

    백준 23348 스트릿 코딩 파이터 python 완료

--- baekjoon/python/23348.py
+++ baekjoon/python/23348.py
@@ -7,9 +7,10 @@ cases = int(sys.stdin.readline())
 
 for _ in range(cases):
     temp = 0
-    scores = list(map(int, sys.stdin.readline().split()))
-    for i in range(0, 3):
-        temp += scores[i] * coding_score[i]
+    for _ in range(0, 3):
+        scores = list(map(int, sys.stdin.readline().split()))
+        for i in range(0, 3):
+            temp += scores[i] * coding_score[i]
 
     if max_score < temp:
         max_score = temp
