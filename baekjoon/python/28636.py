8dc0ad1fa94ee36d0b5c806100e9c1c6869431a3
dydwnsekd <dydwnsekd123@gmail.com>
2023-12-27 18:59:32 +0900

    백준 28636 Марафонец python 완료

---
+++ baekjoon/python/28636.py
@@ -0,0 +1,17 @@
+# commit 대기
+import sys
+
+cases = int(sys.stdin.readline())
+h, m, s = 0, 0, 0
+
+for _ in range(cases):
+    mm, ss = map(int, sys.stdin.readline().split(":"))
+    m += mm
+    s += ss
+
+m += s // 60
+s = s % 60
+h += m // 60
+m = m % 60
+
+print(f"{str(h).zfill(2)}:{str(m).zfill(2)}:{str(s).zfill(2)}")
