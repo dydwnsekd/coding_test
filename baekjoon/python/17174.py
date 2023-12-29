ebd1b537d316c4258bd6aecba44f590903625893
dydwnsekd <dydwnsekd123@gmail.com>
2023-12-29 21:31:26 +0900

    백준 17174 전체 계산 횟수 python 로직 수정

--- baekjoon/python/17174.py
+++ baekjoon/python/17174.py
@@ -4,9 +4,9 @@ count = 0
 n, m = map(int, sys.stdin.readline().split())
 
 count += n
-count += n // m
 
-if n // m > 1:
-    count += 1
+while n // m >= m:
+    count += n // m
+    n = n // m
 
 print(count)
