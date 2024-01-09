9c19709b2ce0eb440f53023ce23103b2ba1cb21c
dydwnsekd <dydwnsekd123@gmail.com>
2024-01-10 00:24:58 +0900

    백준 2702 초6 수학 python 완료

---
+++ baekjoon/python/2702.py
@@ -0,0 +1,23 @@
+import sys
+
+def getGCD(num1, num2):
+    r = -1
+    if num1 < num2: num1, num2 = num2, num1
+
+    while (r != 0):
+        r = num1 % num2
+        num1, num2 = num2, r
+
+    return (num1)
+
+
+def getLCM(num1, num2):
+    return int((num1 * num2) / getGCD(num1, num2))
+
+
+cases = int(sys.stdin.readline())
+
+for _ in range(cases):
+    a, b = map(int, sys.stdin.readline().split())
+    print(getLCM(a, b), getGCD(a, b))
+
