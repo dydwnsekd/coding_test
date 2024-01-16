e915893b857d0b803ffe4f33790f2a0f890a678f
dydwnsekd <dydwnsekd123@gmail.com>
2024-01-17 01:01:27 +0900

    백준 25630 팰린드롬 소떡소떡 python 완료

--- baekjoon/python/25630.py
+++ baekjoon/python/25630.py
@@ -4,24 +4,10 @@ count = 0
 
 n = int(sys.stdin.readline())
 s = sys.stdin.readline().strip()
+reverse_s = s[::-1]
 
-if n % 2 == 1:
-    a = s[:n // 2]
-    b = s[n // 2 + 1:]
+for i in range(n):
+    if s[i] != reverse_s[i]:
+        count += 1
 
-    for i in range(n//2):
-        if a[i] != b[i]:
-            count += 1
-
-else:
-    a = s[:n // 2]
-    b = s[n // 2:]
-
-    print(a)
-    print(b)
-
-    for i in range(n//2):
-        if a[i] != b[i]:
-            count += 1
-
-print(count)
+print(count//2)
