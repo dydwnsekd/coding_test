cdc27c50a8af548f16cc8593c9102ceb86e55563
dydwnsekd <dydwnsekd123@gmail.com>
2024-01-24 00:55:22 +0900

    백준 9838 XMAS python 완료

---
+++ baekjoon/python/9838.py
@@ -0,0 +1,11 @@
+# commit 대기
+import sys
+
+n = int(sys.stdin.readline())
+gift_list = [0] * n
+
+for i in range(1, n+1):
+    gift_list[int(sys.stdin.readline()) - 1] = i
+
+for i in gift_list:
+    print(i)
