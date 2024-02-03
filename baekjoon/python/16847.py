0874ef89ee31cbaa28d8b69384d61ba3a990004b
dydwnsekd <dydwnsekd123@gmail.com>
2024-02-04 00:13:16 +0900

    백준 16847 Teenage Mutant python 완료

--- baekjoon/python/16847.py
+++ baekjoon/python/16847.py
@@ -1,9 +1,8 @@
 import sys
 
-
 cases = int(sys.stdin.readline())
 
-for _ in range(cases):
+for i in range(cases):
     n, k = map(int, sys.stdin.readline().strip().split())
     my_gene = sys.stdin.readline().strip()
 
@@ -13,3 +12,10 @@ for _ in range(cases):
 
     pivot_gene = [[row[i] for row in ancestors_gene] for i in range(k)]
 
+    result = 0
+    for j in range(k):
+        if my_gene[j] not in pivot_gene[j]:
+            result += 1
+
+    print(f"Data Set {i+1}:")
+    print(f"{result}/{k}\n")
