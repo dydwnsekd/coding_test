c90c0f1af0e07596bfcf9bf3412166894e5bf0d0
dydwnsekd <dydwnsekd123@gmail.com>
2024-02-06 00:54:05 +0900

    백준 15279 Heart Rate python 완료

--- baekjoon/python/15279.py
+++ baekjoon/python/15279.py
@@ -5,8 +5,9 @@ n = int(sys.stdin.readline())
 for _ in range(n):
     b, p = map(float, sys.stdin.readline().strip().split())
 
-    min_abpm = round(60 / (p / (b-1)), 4)
-    bpm = round((b/p) * 60, 4)
-    max_abpm = round(min_abpm + bpm, 4)
+    min_abpm = 60 / (p / (b-1))
+    bpm = (b/p) * 60
+    max_abpm = 2*bpm - min_abpm
 
-    print(min_abpm, bpm, max_abpm)
+    print(f"{min_abpm:0.4f} {bpm:0.4f} {max_abpm:0.4f}")
+
