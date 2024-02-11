3627c83465245771aef7ed0dc05e52c98220c3a7
dydwnsekd <dydwnsekd123@gmail.com>
2024-02-11 19:13:28 +0900

    백준 13224 Chop Cup python 완료

---
+++ baekjoon/python/13224.py
@@ -0,0 +1,24 @@
+# commit 대기
+import sys
+
+ball_position = 1
+s = list(sys.stdin.readline())
+
+for i in s:
+    if i == "A":
+        if ball_position == 1:
+            ball_position = 2
+        elif ball_position == 2:
+            ball_position = 1
+    elif i == "B":
+        if ball_position == 2:
+            ball_position = 3
+        elif ball_position == 3:
+            ball_position = 2
+    elif i == "C":
+        if ball_position == 1:
+            ball_position = 3
+        elif ball_position == 3:
+            ball_position = 1
+
+print(ball_position)
