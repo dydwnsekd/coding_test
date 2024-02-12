67c360563ea5ed130a506516a46a5c50fd1cc164
dydwnsekd <dydwnsekd123@gmail.com>
2024-02-12 21:11:05 +0900

    백준 7215 Ateivių kojinės python init

---
+++ baekjoon/python/7215.py
@@ -0,0 +1,20 @@
+import sys
+
+result = 0
+n, k = map(int, sys.stdin.readline().strip().split())
+
+socks_list = list(map(int, sys.stdin.readline().strip().split()))
+
+if max(socks_list) < n:
+    print(-1)
+else:
+    socks_list.sort()
+
+    for i in socks_list:
+        if i > n:
+            result += n - 1
+        else:
+            result += i
+
+    print(result + 1)
+
