26fed5267e97b75a023c5decb56edb43ddb6758f
dydwnsekd <dydwnsekd123@gmail.com>
2024-02-16 00:34:38 +0900

    백준 7241 Skaičiai python init

---
+++ baekjoon/python/7241.py
@@ -0,0 +1,15 @@
+import sys
+from itertools import permutations
+
+num_list = []
+a, b, c = map(int, sys.stdin.readline().strip().split())
+x = int(sys.stdin.readline())
+permutation_list = permutations([5, 3, 8], 3)
+
+for i in permutation_list:
+    num_list.append(int("".join(map(str, i))))
+
+num_list.sort()
+
+print(num_list.index(x) + 1)
+
