079d9b8ab5842322f9bb1b4dce715d6cd47f977a
dydwnsekd <dydwnsekd123@gmail.com>
2024-02-17 11:31:25 +0900

    백준 7241 Skaičiai python 완료

--- baekjoon/python/7241.py
+++ baekjoon/python/7241.py
@@ -4,7 +4,7 @@ from itertools import permutations
 num_list = []
 a, b, c = map(int, sys.stdin.readline().strip().split())
 x = int(sys.stdin.readline())
-permutation_list = permutations([5, 3, 8], 3)
+permutation_list = permutations([a, b, c], 3)
 
 for i in permutation_list:
     num_list.append(int("".join(map(str, i))))
