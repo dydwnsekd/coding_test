be5e9a97d5fa637057526065e52251155a465ad5
dydwnsekd <dydwnsekd123@gmail.com>
2024-05-01 00:10:13 +0900

    백준 30224 Lucky 7 python 완료

---
+++ baekjoon/python/30224.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+if '7' not in str(n) and n % 7 != 0:
+    print(0)
+elif '7' not in str(n) and n % 7 == 0:
+    print(1)
+elif '7' in str(n) and n % 7 != 0:
+    print(2)
+elif '7' in str(n) and n % 7 == 0:
+    print(3)
