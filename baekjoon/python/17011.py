0acc1dfc915942ab11422c47ddbf6f136516aa8b
dydwnsekd <dydwnsekd123@gmail.com>
2024-06-10 00:02:58 +0900

    백준 17011 Cold Compress python 완료

--- baekjoon/python/17011.py
+++ baekjoon/python/17011.py
@@ -6,16 +6,20 @@ for _ in range(n):
     s = sys.stdin.readline().strip()
 
     result_text = ""
-    prev_char = ""
-    char_count = 1
+    prev_char = s[0]
+    char_count = 0
 
     for i in s:
         if prev_char == i:
             char_count += 1
         else:
-            result_text += f"{str(char_count)} {i} "
-            char_count = 0
+            result_text += f"{str(char_count)} {prev_char} "
+            char_count = 1
             prev_char = i
+
+    result_text += f"{str(char_count)} {i} "
+    char_count = 1
+    prev_char = i
 
     print(result_text)
 
