7e97917ee92fc4489d94d948eae3ba8d62067e27
dydwnsekd <dydwnsekd123@gmail.com>
2024-07-29 09:42:19 +0900

    백준 24736 Football Scoring python 로직 수정

--- baekjoon/python/24736.py
+++ baekjoon/python/24736.py
@@ -1,15 +1,11 @@
 import sys
 
-home_score = 0
-away_score = 0
-
 score_board = [6, 3, 2, 1, 2]
 
 home = list(map(int, sys.stdin.readline().strip().split()))
 away = list(map(int, sys.stdin.readline().strip().split()))
 
-for i in range(5):
-    home_score += score_board[i] * home[i]
-    away_score += score_board[i] * away[i]
+home_score = sum(s * h for s, h in zip(score_board, home))
+away_score = sum(s * a for s, a in zip(score_board, away))
 
 print(home_score, away_score)
