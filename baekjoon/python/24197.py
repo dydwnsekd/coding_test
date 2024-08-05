e3285260cf2b0688c51767e8813ed00b1028cea7
dydwnsekd <dydwnsekd123@gmail.com>
2024-08-06 00:55:52 +0900

    백준 24197 Tabbtabbande python 완료

--- baekjoon/python/24197.py
+++ baekjoon/python/24197.py
@@ -1,14 +1,13 @@
 import sys
 
 count = 0
-tabs, current_tab = map(int, sys.stdin.readline().strip().split())
+current_tab = 1
+n, m = map(int, sys.stdin.readline().strip().split())
 tab_list = list(map(int, sys.stdin.readline().strip().split()))
 tab_count = len(tab_list)
 
 for i in range(tab_count):
-    print("(abs(current_tab - tab_list[i]):::", (abs(current_tab - tab_list[i])))
-    print("tabs - current_tab + tab_list[i]:::", tabs - current_tab + tab_list[i])
-    count += min(abs(current_tab - tab_list[i]), tabs - current_tab + tab_list[i])
-    current_tab = min(abs(current_tab - tab_list[i]), tabs - current_tab + tab_list[i])
+    count += min((current_tab - tab_list[i] + n) % n, (tab_list[i] - current_tab + n) % n)
+    current_tab = tab_list[i]
 
 print(count)
