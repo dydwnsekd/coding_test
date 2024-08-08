7dec33fd1ab9af2afe41eaa4b44d65a7e928c71c
dydwnsekd <dydwnsekd123@gmail.com>
2024-08-08 10:29:53 +0900

    백준 11655 ROT13 python init

---
+++ baekjoon/python/11655.py
@@ -0,0 +1,29 @@
+import sys
+
+result = ""
+
+capital = [i for i in range(65, 91)]
+rot_capital = []
+small_letter = [i for i in range(97, 123)]
+rot_small_letter = []
+c = ord("z") - ord("a") + 1
+
+s = sys.stdin.readline().strip()
+
+for i in s:
+    if ord("A") <= ord(i) <= ord("Z"):
+        if ord(i) + 13 > ord("Z"):
+            result += chr(ord(i) + 13 - c)
+        else:
+            result += chr(ord(i) + 13)
+    elif ord("a") <= ord(i) <= ord("z"):
+        if ord(i) + 13 > ord("z"):
+            result += chr(ord(i) + 13 - c)
+        else:
+            result += chr(ord(i) + 13)
+    else:
+        result += i
+
+(result.strip())
+
+
