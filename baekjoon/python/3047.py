b03f88408c366adefe223ea155080ee72defaf51
dydwnsekd <dydwnsekd123@gmail.com>
2024-08-14 09:56:33 +0900

    백준 3047 ABC python 로직 개선

--- baekjoon/python/3047.py
+++ baekjoon/python/3047.py
@@ -1,3 +1,4 @@
+'''
 import sys
 
 num_list = list(map(int, sys.stdin.readline().strip().split()))
@@ -12,4 +13,13 @@ for s in sort_str:
         print(num_list[1], end=" ")
     elif s == "C":
         print(num_list[2], end=" ")
+'''
 
+import sys
+
+num_list = sorted(map(int, sys.stdin.readline().strip().split()))
+sort_str = sys.stdin.readline().strip()
+
+order_map = {'A': 0, 'B': 1, 'C': 2}
+
+print(" ".join(str(num_list[order_map[char]]) for char in sort_str))
