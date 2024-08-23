b836f2cdff24656f69adbfa7690edce53f965f47
dydwnsekd <dydwnsekd123@gmail.com>
2024-08-24 01:08:03 +0900

    백준 31994 강당 대관  python init

---
+++ baekjoon/python/31994.py
@@ -0,0 +1,12 @@
+import sys
+
+seminar_dict = {}
+
+for i in range(7):
+    k, v = sys.stdin.readline().strip().split()
+    seminar_dict[k] = v
+
+
+sorted_dict = sorted(seminar_dict, key=lambda x: seminar_dict[x], reverse=True)
+
+print(sorted_dict[0])
