843d21cf5903bbf02bf04778318bc8af8fc140c7
dydwnsekd <dydwnsekd123@gmail.com>
2024-08-25 18:51:59 +0900

    백준 31994 강당 대관 python 완료

--- baekjoon/python/31994.py
+++ baekjoon/python/31994.py
@@ -1,10 +1,12 @@
+# 백준 30794 가희와 클럽 오디션 1 python 완료
+
 import sys
 
 seminar_dict = {}
 
 for i in range(7):
     k, v = sys.stdin.readline().strip().split()
-    seminar_dict[k] = v
+    seminar_dict[k] = int(v)
 
 
 sorted_dict = sorted(seminar_dict, key=lambda x: seminar_dict[x], reverse=True)
