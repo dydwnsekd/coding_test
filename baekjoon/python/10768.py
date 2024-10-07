8129e48c6f08df2b56f43657175d00d7ebe3b83b
dydwnsekd <dydwnsekd123@gmail.com>
2024-10-08 00:16:28 +0900

    백준 10768 특별한 날 python datetime 이용 방식 추가

--- baekjoon/python/10768.py
+++ baekjoon/python/10768.py
@@ -1,16 +1,27 @@
 import sys
+from datetime import datetime
 
 month = int(sys.stdin.readline())
 day = int(sys.stdin.readline())
 
-if month < 2:
+# if month < 2:
+#     print("Before")
+# elif month == 2:
+#     if day < 18:
+#         print("Before")
+#     elif day == 18:
+#         print("Special")
+#     else:
+#         print("After")
+# else:
+#     print("After")
+
+special_date = datetime(2015, 2, 18)
+input_date = datetime(2015, month, day)
+
+if special_date > input_date:
     print("Before")
-elif month == 2:
-    if day < 18:
-        print("Before")
-    elif day == 18:
-        print("Special")
-    else:
-        print("After")
+elif special_date == input_date:
+    print("Special")
 else:
     print("After")
