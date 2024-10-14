b93d17f3bc9dad69e16511e0d999730bbefbd44d
dydwnsekd <dydwnsekd123@gmail.com>
2024-10-15 00:10:56 +0900

    백준 11098 첼시를 도와줘! python 완료

---
+++ baekjoon/python/11098.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    p = int(sys.stdin.readline())
+    player_dict = {}
+
+    for _ in range(p):
+        value, name = sys.stdin.readline().strip().split()
+        player_dict[name] = int(value)
+
+    sorted_player = sorted(player_dict.items(), key=lambda x: x[1], reverse=True)
+
+    print(sorted_player[0][0])
