e6cef1bef37ff56a5f6a8dfb374a094f774ea543
dydwnsekd <dydwnsekd123@gmail.com>
2024-10-20 23:32:38 +0900

    백준 15947 아기 석환 뚜루루 뚜루 python remainder 정리 완료

--- baekjoon/python/15947.py
+++ baekjoon/python/15947.py
@@ -1,9 +1,9 @@
 import sys
 
 s = "baby sukhwan tururu turu very cute tururu turu in bed tururu turu baby sukhwan"
-word_count = len(s.split()) + 1
+word_count = len(s.split())
 
-n = int(sys.stdin.readline())
+n = int(sys.stdin.readline()) - 1
 quotient = n // word_count
 remainder = n % word_count
 
@@ -13,6 +13,7 @@ for _ in range(quotient):
     s = s.replace("turu", "tururu")
 
 split_s = s.split()
+print(split_s)
 
 print(split_s[remainder])
 
