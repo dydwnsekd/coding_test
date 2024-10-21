7e8aaa0eb2e4863ca045edf3f44ccf48cd8fa8d4
dydwnsekd <dydwnsekd123@gmail.com>
2024-10-22 00:46:24 +0900

    백준 15947 아기 석환 뚜루루 뚜루 python 완료

--- baekjoon/python/15947.py
+++ baekjoon/python/15947.py
@@ -1,4 +1,5 @@
 import sys
+from re import split
 
 s = "baby sukhwan tururu turu very cute tururu turu in bed tururu turu baby sukhwan"
 word_count = len(s.split())
@@ -7,14 +8,18 @@ n = int(sys.stdin.readline()) - 1
 quotient = n // word_count
 remainder = n % word_count
 
-for _ in range(quotient):
-    s = s.replace("turu", "tururu")
-
 split_s = s.split()
 
-if split_s[remainder].count("ru") >= 5:
-    print(f"tu+ru*{split_s[remainder].count('ru')}")
+if remainder in [2, 6, 10]:
+    return_s = "tu" + "ru" * (quotient + 2)
+elif remainder in [3, 7, 11]:
+    return_s = "tu" + "ru" * (quotient + 1)
 else:
-    print(split_s[remainder])
+    return_s = split_s[remainder]
+
+if return_s.count("ru") >= 5:
+    print(f"tu+ru*{return_s.count('ru')}")
+else:
+    print(return_s)
 
 
