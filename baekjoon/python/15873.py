bdef8a88d0184f92b030d7dfdca63f84e4defff7
dydwnsekd <dydwnsekd123@gmail.com>
2024-10-24 01:33:22 +0900

    백준 15873 공백 없는 A+B python 완료

--- baekjoon/python/15873.py
+++ baekjoon/python/15873.py
@@ -5,12 +5,14 @@ nums = sys.stdin.readline().strip()
 if nums.count('0') == 0:
     num1 = int(nums[0])
     num2 = int(nums[1])
-else:
+elif nums.count('0') == 1:
     if nums.index('0') == 1:
         num1 = int(nums[:2])
         num2 = int(nums[2])
     else:
         num1 = int(nums[0])
         num2 = int(nums[1:])
+else:
+    num1 = num2 = 10
 
 print(num1 + num2)
