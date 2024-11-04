08950ddf49d30a09390d2108786b59dcd0cd7d2b
dydwnsekd <dydwnsekd123@gmail.com>
2024-11-04 09:35:28 +0900

    백준 29734 집에선 안돼잉 python 완료

--- baekjoon/python/29734.py
+++ baekjoon/python/29734.py
@@ -3,8 +3,17 @@ import sys
 n, m = map(int, sys.stdin.readline().strip().split())
 t, s = map(int, sys.stdin.readline().strip().split())
 
-dok = (m // 8) * (2 * t + s) + n
-zip = ((n // 8) * s) + m
+if m % 8 == 0:
+    dok_mok = (m // 8) - 1
+else:
+    dok_mok = m // 8
+if n % 8 == 0:
+    zip_mok = (n // 8) - 1
+else:
+    zip_mok = n // 8
+
+dok = t + dok_mok * (2 * t + s) + m
+zip = zip_mok * s + n
 
 if dok < zip:
     print("Dok")
