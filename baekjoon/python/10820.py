0873ee0543336b8cce38e90173ce62996deee4db
dydwnsekd <dydwnsekd123@gmail.com>
2024-11-06 00:55:00 +0900

    백준 10820 문자열 분석 python init

---
+++ baekjoon/python/10820.py
@@ -0,0 +1,21 @@
+import sys
+
+while True:
+    upper_count = 0
+    lower_count = 0
+    num_count = 0
+    blank_count = 0
+
+    s = sys.stdin.readline().strip()
+
+    for i in s:
+        if ord("A") <= ord(i) <= ord("Z"):
+            upper_count += 1
+        elif ord("a") <= ord(i) <= ord("z"):
+            lower_count += 1
+        elif ord("0") <= ord(i) <= ord("9"):
+            num_count += 1
+        else:
+            blank_count += 1
+
+    print(lower_count, upper_count, num_count, blank_count)
