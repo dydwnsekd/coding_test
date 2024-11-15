75e556cc295e2a5000de899b586a9546b07a337c
dydwnsekd <dydwnsekd123@gmail.com>
2024-11-15 23:10:31 +0900

    백준 5618 공약수 python 로직 변경

--- baekjoon/python/5618.py
+++ baekjoon/python/5618.py
@@ -1,10 +1,24 @@
 import sys
 
-n = int(sys.stdin.readline())
+def getGCD(num1, num2):
+    r = -1
+    if num1 < num2: num1, num2 = num2, num1
 
+    while (r!=0):
+        r = num1 % num2
+        num1, num2 = num2, r
+
+    return(num1)
+
+n = int(sys.stdin.readline())
 num_list = list(map(int, sys.stdin.readline().strip().split()))
 
-for i in range(1, min(num_list) + 1):
-    if all(num % i == 0 for num in num_list):
+gcd = getGCD(num_list[0], num_list[1])
+
+if n == 3:
+    gcd = getGCD(gcd, num_list[2])
+
+for i in range(1, gcd+1):
+    if gcd % i == 0:
         print(i)
 
