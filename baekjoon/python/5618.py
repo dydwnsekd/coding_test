87b97387f5e82bbffe2b09511aca65045e51c6e0
dydwnsekd <dydwnsekd123@gmail.com>
2024-11-16 20:31:55 +0900

    백준 5618 공약수 python 완료

--- baekjoon/python/5618.py
+++ baekjoon/python/5618.py
@@ -10,6 +10,25 @@ def getGCD(num1, num2):
 
     return(num1)
 
+def get_divisor(num):
+
+    divisor_list = []
+    if num == 1:
+        return [1]
+    elif num == 2:
+        return [1, 2]
+    elif num == 3:
+        return [1, 3]
+
+    for i in range(1, num//2 + 1):
+        if num % i == 0:
+            divisor_list.append(i)
+            divisor_list.append(num // i)
+
+    divisor_list = list(set(divisor_list))
+    divisor_list.sort()
+    return divisor_list
+
 n = int(sys.stdin.readline())
 num_list = list(map(int, sys.stdin.readline().strip().split()))
 
@@ -18,7 +37,8 @@ gcd = getGCD(num_list[0], num_list[1])
 if n == 3:
     gcd = getGCD(gcd, num_list[2])
 
-for i in range(1, gcd+1):
-    if gcd % i == 0:
-        print(i)
+divisor = get_divisor(gcd)
+for i in divisor:
+    print(i)
+
 
