807d7370cb4f4e2a118653234e49758f850049c4
dydwnsekd <dydwnsekd123@gmail.com>
2024-11-21 09:45:37 +0900

    백준 2851 슈퍼 마리오 python 완료

--- baekjoon/python/2851.py
+++ baekjoon/python/2851.py
@@ -6,10 +6,10 @@ for i in range(9):
     temp = int(sys.stdin.readline())
     num_list.append(num_list[i] + temp)
 
-result = 100
+result = 101
 
 for i in range(len(num_list)):
-    if abs(100 - num_list[i]) < result:
+    if abs(100 - num_list[i]) <= result:
         result = abs(100 - num_list[i])
         result_index = i
 
