86301eab65d950e60dcfa5a122835840df3e37d0
dydwnsekd <dydwnsekd123@gmail.com>
2024-11-27 09:45:11 +0900

    백준 2755 이번학기 평점은 몇점? python init

---
+++ baekjoon/python/2755.py
@@ -0,0 +1,18 @@
+import sys
+
+result = 0
+total_grade = 0
+
+grade_dict = {"A+": 4.3, "A0": 4.0, "A-": 3.7, "B+": 3.3, "B0": 3.0, "B-": 2.7,
+              "C+": 2.3, "C0": 2.0, "C-": 1.7, "D+": 1.3, "D0": 1.0, "D-": 0.7, "F": 0.0}
+
+n = int(sys.stdin.readline())
+
+for i in range(n):
+    subject_info = sys.stdin.readline().strip().split()
+    total_grade += int(subject_info[1])
+
+    result += int(subject_info[1]) * grade_dict[subject_info[2]]
+
+
+print(f"{(result / total_grade):.2f}")
