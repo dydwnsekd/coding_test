cec27a5a9cfd55dcd2bb05a4cf114c7bdd753fd1
dydwnsekd <dydwnsekd123@gmail.com>
2024-12-15 21:50:32 +0900

    소인수분해 python utils

--- util/prime_factors.py
+++ util/prime_factors.py
@@ -1,24 +1,27 @@
-def prime_factors(n):
-    factors = []
-    while n % 2 == 0:
-        factors.append(2)
-        n //= 2
+class PrimeFactors:
+    def prime_factors(n):
+        factors = []
+        while n % 2 == 0:
+            factors.append(2)
+            n //= 2
 
-    for i in range(3, int(n ** 0.5) + 1, 2):
-        while n % i == 0:
-            factors.append(i)
-            n //= i
+        for i in range(3, int(n ** 0.5) + 1, 2):
+            while n % i == 0:
+                factors.append(i)
+                n //= i
 
-    if n > 2:
-        factors.append(n)
+        if n > 2:
+            factors.append(n)
 
-    return factors
+        return factors
 
 
 if __name__ == '__main__':
-    print(prime_factors(4))
-    print(prime_factors(30))
-    print(prime_factors(50))
-    print(prime_factors(125))
-    print(prime_factors(1024))
+    pf = PrimeFactors()
+
+    print(pf.prime_factors(4))
+    print(pf.prime_factors(30))
+    print(pf.prime_factors(50))
+    print(pf.prime_factors(125))
+    print(pf.prime_factors(1024))
 
