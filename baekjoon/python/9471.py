3aef4ce6b7dae66c905588ab7049c39ca393c13d
dydwnsekd <dydwnsekd123@gmail.com>
2024-12-18 01:01:09 +0900

    백준 9471 피사노 주기 python 완료

--- baekjoon/python/9471.py
+++ baekjoon/python/9471.py
@@ -1,33 +1,14 @@
 import sys
 
-def prime_factors(n):
-    factors = []
-    while n % 2 == 0:
-        factors.append(2)
-        n //= 2
+def pisano_period(m):
+    previous, current = 0, 1
+    for i in range(0, m * m):
+        previous, current = current, (previous + current) % m
 
-    for i in range(3, int(n ** 0.5) + 1, 2):
-        while n % i == 0:
-            factors.append(i)
-            n //= i
-
-    if n > 2:
-        factors.append(n)
-
-    return factors
-
+        if previous == 0 and current == 1:
+            return i + 1
 
 n = int(sys.stdin.readline())
-
-for i in range(n):
+for _ in range(n):
     case_num, m = map(int, sys.stdin.readline().strip().split())
-    m_factors = prime_factors(m)
-
-    if all(factor == 2 for factor in m_factors):
-        print(case_num, 3 * (2 ** (len(m_factors) - 1)))
-    elif all(factor == 5 for factor in m_factors):
-        print(case_num, 4 * (5 ** len(m_factors)))
-    elif m_factors.count(2) == 1 and m_factors.count(5) == len(m_factors) - 1:
-        print(case_num, 6 * len(m_factors))
-
-
+    print(case_num, pisano_period(m))
