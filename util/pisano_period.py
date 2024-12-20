9b6e8e24155c8c1cdcb2839758865ece66ce5635
dydwnsekd <dydwnsekd123@gmail.com>
2024-12-20 09:06:25 +0900

    pisano_period util python 수정

--- util/pisano_period.py
+++ util/pisano_period.py
@@ -3,18 +3,31 @@ class PisanoPeriod:
     def pisano_period(self, m):
         previous, current = 0, 1
         for i in range(0, m * m):
-            # 피보나치 수열의 나머지 계산
             previous, current = current, (previous + current) % m
 
-            # 주기가 시작되는 조건
             if previous == 0 and current == 1:
                 return i + 1
+
+    def pisano_period_list(self, m):
+        previous, current = 0, 1
+        pisano_list = [0]
+        for i in range(0, m * m):
+            previous, current = current, (previous + current) % m
+
+            if previous == 0 and current == 1:
+                return pisano_list
+            pisano_list.append(previous)
 
 
 if __name__ == '__main__':
     pisano_period = PisanoPeriod()
     print(pisano_period.pisano_period(4))
+    print(pisano_period.pisano_period_list(4))
     print(pisano_period.pisano_period(5))
+    print(pisano_period.pisano_period_list(5))
     print(pisano_period.pisano_period(11))
-    print(pisano_period.pisano_period(123456))
-    print(pisano_period.pisano_period(987654))
+    print(pisano_period.pisano_period_list(11))
+    # print(pisano_period.pisano_period(123456))
+    # print(pisano_period.pisano_period_list(123456))
+    # print(pisano_period.pisano_period(987654))
+    # print(pisano_period.pisano_period_list(987654))
