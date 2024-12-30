f596ad131e3f16c0baeba4f0256673f8022abb3b
dydwnsekd <dydwnsekd123@gmail.com>
2024-12-30 20:44:14 +0900

    백준 14920 3n+1 수열 python init

--- baekjoon/python/14920.py
+++ baekjoon/python/14920.py
@@ -2,27 +2,22 @@ import sys
 
 n = int(sys.stdin.readline())
 
+sequence_list = [n]
+
 def sequence(num):
     if num == 1:
-        return n
+        return sequence_list[0]
     elif num % 2 == 1:
-        return sequence(sequence(num-1)//2)
+        return sequence_list[num-2] // 2
     elif num % 2 == 0:
-        return 3 * sequence(num-1) + 1
+        return 3 * sequence_list[num-2] + 1
 
 count = 1
 
-sequence_list = [n]
-print(sequence(2))
-
 while True:
     count += 1
-
     sequence_list.append(sequence(count))
 
-    if sequence_list[count-1] == 1:
+    if sequence_list[count-1] % 100 == 1:
         print(count)
         break
-
-
-
