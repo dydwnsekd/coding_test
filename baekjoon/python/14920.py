24ffff789774853e9378453db446c92c3d9291ca
dydwnsekd <dydwnsekd123@gmail.com>
2025-01-02 09:52:04 +0900

    백준 14920 3n+1 수열 python 로직 수정

--- baekjoon/python/14920.py
+++ baekjoon/python/14920.py
@@ -1,29 +1,47 @@
+# import sys
+#
+# n = int(sys.stdin.readline())
+#
+# sequence_list = [n]
+#
+# def sequence(num):
+#     if num == 1:
+#         return sequence_list[0]
+#     elif sequence_list[num-2] % 2 == 0:
+#         return sequence_list[num-2] // 2
+#     elif sequence_list[num-2] % 2 == 1:
+#         return 3 * sequence_list[num-2] + 1
+#
+# count = 1
+#
+# while True:
+#     count += 1
+#
+#     sequence_list.append(sequence(count))
+#
+#     if n == 1:
+#         print(1)
+#         break
+#
+#     if sequence_list[count-1] == 1:
+#         print(count)
+#         break
+#
+
 import sys
 
-n = int(sys.stdin.readline())
+n = int(sys.stdin.readline().strip())
 
 sequence_list = [n]
 
-def sequence(num):
-    if num == 1:
-        return sequence_list[0]
-    elif sequence_list[num-2] % 2 == 0:
-        return sequence_list[num-2] // 2
-    elif sequence_list[num-2] % 2 == 1:
-        return 3 * sequence_list[num-2] + 1
+def generate_sequence(num):
+    while num != 1:
+        if num % 2 == 0:
+            num //= 2
+        else:
+            num = 3 * num + 1
+        sequence_list.append(num)
 
-count = 1
+generate_sequence(n)
 
-while True:
-    count += 1
-
-    sequence_list.append(sequence(count))
-
-    if n == 1:
-        print(1)
-        break
-
-    if sequence_list[count-1] == 1:
-        print(count)
-        break
-
+print(len(sequence_list))
