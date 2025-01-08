dcbf02d1d5d04a9f47fa56e6b1e739785707effb
dydwnsekd <dydwnsekd123@gmail.com>
2025-01-08 20:21:40 +0900

    백준 6131 완전 제곱수 python 개선

--- baekjoon/python/6131.py
+++ baekjoon/python/6131.py
@@ -3,10 +3,9 @@ import sys
 count = 0
 n = int(sys.stdin.readline())
 
-for i in range(1, 1000):
-    for j in range(i, 1000):
+for i in range(1, 500):
+    for j in range(i, 500):
         if j ** 2 - i ** 2 == n:
             count += 1
-
 
 print(count)
