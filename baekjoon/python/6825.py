1db8696a244762d451aacf6ae71180bd663dcdbd
dydwnsekd <dydwnsekd123@gmail.com>
2025-02-08 20:30:23 +0900

    백준 6825 Body Mass Index python init

---
+++ baekjoon/python/6825.py
@@ -0,0 +1,14 @@
+import sys
+
+weight = float(sys.stdin.readline())
+height = float(sys.stdin.readline())
+
+BMI = weight / (height ** 2)
+
+if BMI < 18.5:
+    print("Underweight")
+elif BMI < 25:
+    print("Normal weight")
+else:
+    print("Overweight")
+
