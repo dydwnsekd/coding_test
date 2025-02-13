67cb764a4c8df3e15653954f97212a9a523117fa
dydwnsekd <dydwnsekd123@gmail.com>
2025-02-14 00:39:55 +0900

    백준 2954 창영이의 일기장 python 완료

--- baekjoon/python/2954.py
+++ baekjoon/python/2954.py
@@ -4,7 +4,7 @@ s = sys.stdin.readline().strip()
 result = ""
 skip_count = 0
 
-for i in range(len(s) - 1):
+for i in range(len(s)):
     if skip_count > 0:
         skip_count -= 1
         continue
