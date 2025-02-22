1c1def9332a39600b08abf1da4f37a6f62350ca1
dydwnsekd <dydwnsekd123@gmail.com>
2025-02-22 19:01:17 +0900

    백준 33171 いずれか片方 (Either, but Not Both) python 완료

--- baekjoon/python/33171.py
+++ baekjoon/python/33171.py
@@ -6,7 +6,7 @@ n = int(sys.stdin.readline())
 a = int(sys.stdin.readline())
 b = int(sys.stdin.readline())
 
-for i in range(n):
+for i in range(1, n+1):
     if i % a != 0 and i % b == 0:
         count += 1
     elif i % a == 0 and i % b != 0:
