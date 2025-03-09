10f7782e68b206d377cd6e441df8d682308d32fe
dydwnsekd <dydwnsekd123@gmail.com>
2025-03-10 00:44:17 +0900

    백준 11383 뚊 python 개선 코드 추가

--- baekjoon/python/11383.py
+++ baekjoon/python/11383.py
@@ -1,28 +1,41 @@
+# import sys
+#
+# n, m = map(int, sys.stdin.readline().strip().split())
+# s_list = []
+# ss_list = []
+#
+# for _ in range(n):
+#     s_list.append(sys.stdin.readline().strip())
+#
+# for _ in range(n):
+#     ss_list.append(sys.stdin.readline().strip())
+#
+# flag = True
+#
+# for i in range(n):
+#     result = ""
+#
+#     for j in s_list[i]:
+#         result += f"{j}{j}"
+#
+#     if result != ss_list[i]:
+#         flag = False
+#
+# if flag:
+#     print("Eyfa")
+# else:
+#     print("Not Eyfa")
+#
+
 import sys
 
-n, m = map(int, sys.stdin.readline().strip().split())
-s_list = []
-ss_list = []
+n, m = map(int, sys.stdin.readline().split())
 
-for _ in range(n):
-    s_list.append(sys.stdin.readline().strip())
+s_list = [sys.stdin.readline().strip() for _ in range(n)]
+ss_list = [sys.stdin.readline().strip() for _ in range(n)]
 
-for _ in range(n):
-    ss_list.append(sys.stdin.readline().strip())
-
-flag = True
-
-for i in range(n):
-    result = ""
-
-    for j in s_list[i]:
-        result += f"{j}{j}"
-
-    if result != ss_list[i]:
-        flag = False
-
-if flag:
+# 모든 행이 조건을 만족하는지 확인
+if all("".join(ch * 2 for ch in s_list[i]) == ss_list[i] for i in range(n)):
     print("Eyfa")
 else:
     print("Not Eyfa")
-
