04a7114203e1940bb7c076157ef48db1baa55847
dydwnsekd <dydwnsekd123@gmail.com>
2025-03-14 08:48:40 +0900

    백준 16917 양념 반 후라이드 반 python 완료

--- baekjoon/python/16917.py
+++ baekjoon/python/16917.py
@@ -5,12 +5,21 @@ result = 0
 a, b, c, x, y = map(int, sys.stdin.readline().strip().split())
 
 if a + b >= 2 * c:
+    temp1 = 99999999999
+    temp2 = 99999999999
     if x > y:
         result += y * 2 * c
         result += (x-y) * a
+        temp1 = x * 2 * c
     elif x < y:
         result += x * 2 * c
         result += (y-x) * b
+        temp2 = y * 2 * c
+    else:
+        result += x * 2 * c
+
+    result = min(result, temp1, temp2)
+
 else:
     result += a * x + b * y
 
