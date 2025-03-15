91baa0ba327ed5576324ef7a8a9a705f5359ead5
dydwnsekd <dydwnsekd123@gmail.com>
2025-03-15 22:40:01 +0900

    백준 16917 양념 반 후라이드 반 python 개선 코드 추가

--- baekjoon/python/16917.py
+++ baekjoon/python/16917.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 result = 0
@@ -22,6 +23,28 @@ if a + b >= 2 * c:
 
 else:
     result += a * x + b * y
+
+print(result)
+"""
+
+import sys
+
+a, b, c, x, y = map(int, sys.stdin.readline().split())
+
+# `2c`가 `a + b`보다 저렴하면 `2c`로 구입하는 것이 유리
+if 2 * c < a + b:
+    # `x`와 `y` 중 작은 개수만큼 `2c`로 먼저 구매
+    min_xy = min(x, y)
+    result = min_xy * 2 * c
+
+    # 남은 피자의 개수 처리
+    if x > y:
+        result += min((x - y) * a, (x - y) * 2 * c)  # `a` 또는 `2c` 중 최소 비용 선택
+    elif y > x:
+        result += min((y - x) * b, (y - x) * 2 * c)  # `b` 또는 `2c` 중 최소 비용 선택
+else:
+    # 개별 가격으로 구매
+    result = a * x + b * y
 
 print(result)
 
