89d162dec4e255c2397bf85affbb1aeb58a7f3b8
dydwnsekd <dydwnsekd123@gmail.com>
2025-03-30 00:42:18 +0900

    백준 2998 8진수 python 수정 코드

--- baekjoon/python/2998.py
+++ baekjoon/python/2998.py
@@ -1,24 +1,35 @@
+# import sys
+#
+# def binary_to_decimal(binary_num):
+#     decimal_num = 0
+#
+#     binary_num = binary_num[::-1]
+#     for i in range(len(binary_num)):
+#         decimal_num += int(binary_num[i]) * 2 ** i
+#
+#     return decimal_num
+#
+# def decimal_to_octal(decimal_number):
+#     octal_num = ""
+#     if decimal_number == 0:
+#         return "0"
+#     while decimal_number > 0:
+#         octal_num = str(decimal_number % 8) + octal_num
+#         decimal_number = decimal_number // 8
+#
+#     return octal_num
+#
+#
+# bin_num = sys.stdin.readline().strip()
+# print(decimal_to_octal(binary_to_decimal(bin_num)))
+
+# =======================================================
+
 import sys
 
-def binary_to_decimal(binary_num):
-    decimal_num = 0
+def binary_to_octal(binary_str: str) -> str:
+    decimal = int(binary_str, 2)
+    return oct(decimal)[2:]
 
-    binary_num = binary_num[::-1]
-    for i in range(len(binary_num)):
-        decimal_num += int(binary_num[i]) * 2 ** i
-
-    return decimal_num
-
-def decimal_to_octal(decimal_number):
-    octal_num = ""
-    if decimal_number == 0:
-        return "0"
-    while decimal_number > 0:
-        octal_num = str(decimal_number % 8) + octal_num
-        decimal_number = decimal_number // 8
-
-    return octal_num
-
-
-bin_num = sys.stdin.readline().strip()
-print(decimal_to_octal(binary_to_decimal(bin_num)))
+binary_input = sys.stdin.readline().strip()
+print(binary_to_octal(binary_input))
