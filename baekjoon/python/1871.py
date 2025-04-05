bcd4ea9bcc8feb4f71a2c0a6e61e449c92e5b434
dydwnsekd <dydwnsekd123@gmail.com>
2025-04-06 08:32:38 +0900

    백준 1871 좋은 자동차 번호판 python 코드 수정

--- baekjoon/python/1871.py
+++ baekjoon/python/1871.py
@@ -1,19 +1,32 @@
+# import sys
+#
+# n = int(sys.stdin.readline())
+#
+# for _ in range(n):
+#     l, d = sys.stdin.readline().strip().split("-")
+#     l = l[::-1]
+#
+#     l_score = 0
+#     d_score = int(d)
+#
+#     for i in range(len(l)):
+#         l_score += (26 ** i) * abs(ord(l[i]) - ord("A"))
+#
+#     if abs(d_score - l_score) <= 100:
+#         print("nice")
+#     else:
+#         print("not nice")
+
+
+# -----------------------------------------------------------------------
+
 import sys
 
 n = int(sys.stdin.readline())
 
 for _ in range(n):
     l, d = sys.stdin.readline().strip().split("-")
-    l = l[::-1]
-
-    l_score = 0
+    l_score = sum((26 ** (2 - i)) * (ord(ch) - ord('A')) for i, ch in enumerate(l))
     d_score = int(d)
 
-    for i in range(len(l)):
-        l_score += (26 ** i) * abs(ord(l[i]) - ord("A"))
-
-    if abs(d_score - l_score) <= 100:
-        print("nice")
-    else:
-        print("not nice")
-
+    print("nice" if abs(l_score - d_score) <= 100 else "not nice")
