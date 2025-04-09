dd341b11f7a8da9df1fdbb9e0db9a3956a988ce4
dydwnsekd <dydwnsekd123@gmail.com>
2025-04-09 09:47:01 +0900

    백준 33653 Search Wizard python init

---
+++ baekjoon/python/33653.py
@@ -0,0 +1,15 @@
+import sys
+
+count = 0
+
+w = sys.stdin.readline().strip()
+m = int(sys.stdin.readline())
+
+s_list = sys.stdin.readline().strip().split()
+
+for s in s_list:
+    for i in range(len(s)):
+        if w == s[i:len(w)]:
+            count += 1
+
+print(count)
