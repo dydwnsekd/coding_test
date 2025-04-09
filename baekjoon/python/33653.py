731ab883f1da478a40dbdd0336f08706559a6fd6
dydwnsekd <dydwnsekd123@gmail.com>
2025-04-10 00:20:59 +0900

    백준 33653 Search Wizard python 완료

--- baekjoon/python/33653.py
+++ baekjoon/python/33653.py
@@ -9,7 +9,8 @@ s_list = sys.stdin.readline().strip().split()
 
 for s in s_list:
     for i in range(len(s)):
-        if w == s[i:len(w)]:
+        if w == s[i:i+len(w)]:
             count += 1
 
 print(count)
+
