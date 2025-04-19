32977bece7ca79d112dfb8385d8ae58be59ec40f
dydwnsekd <dydwnsekd123@gmail.com>
2025-04-19 22:49:38 +0900

    백준 13610 Volta python 수정

--- baekjoon/python/13610.py
+++ baekjoon/python/13610.py
@@ -1,10 +1,10 @@
+import sys
 from math import gcd
 
 def overtake_time(x: int, y: int) -> int:
     lcm = x * y // gcd(x, y)
-    return lcm // max(x, y)
+    return lcm // abs(x - y)
 
-# 입력 및 실행
-x, y = map(int, input().split())
+# 입력
+x, y = map(int, sys.stdin.readline().strip().split())
 print(overtake_time(x, y))
-
