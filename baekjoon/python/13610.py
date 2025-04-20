4a832897534b582549049d0a741ae79b09e0b3d3
dydwnsekd <dydwnsekd123@gmail.com>
2025-04-20 15:57:13 +0900

    백준 13610 Volta python 완료

--- baekjoon/python/13610.py
+++ baekjoon/python/13610.py
@@ -1,26 +1,3 @@
-"""
-import math
-import sys
-
-
-def overtake_time(x: int, y: int) -> int:
-    if x == y:
-        return -1
-
-    v1 = 1 / x
-    v2 = 1 / y
-    relative_speed = abs(v1 - v2)
-    time_to_overtake = 1 / relative_speed
-    return math.ceil(time_to_overtake)
-
-
-# 입력
-x, y = map(int, sys.stdin.readline().strip().split())
-print(overtake_time(x, y))
-"""
-
-# --------------------------------------------------------------------------
-
 import sys
 
 x, y = map(int, sys.stdin.readline().strip().split())
