f3581ade0b47deaf7b83aea0353b90db89ae6516
dydwnsekd <dydwnsekd123@gmail.com>
2025-04-20 15:57:05 +0900

    백준 13610 Volta python 완료

--- baekjoon/python/13610.py
+++ baekjoon/python/13610.py
@@ -1,10 +1,40 @@
+"""
+import math
 import sys
-from math import gcd
+
 
 def overtake_time(x: int, y: int) -> int:
-    lcm = x * y // gcd(x, y)
-    return lcm // abs(x - y)
+    if x == y:
+        return -1
+
+    v1 = 1 / x
+    v2 = 1 / y
+    relative_speed = abs(v1 - v2)
+    time_to_overtake = 1 / relative_speed
+    return math.ceil(time_to_overtake)
+
 
 # 입력
 x, y = map(int, sys.stdin.readline().strip().split())
 print(overtake_time(x, y))
+"""
+
+# --------------------------------------------------------------------------
+
+import sys
+
+x, y = map(int, sys.stdin.readline().strip().split())
+
+result = 0
+
+x_lab = 0
+y_lab = 1
+
+while x_lab < y_lab:
+    result += 1
+
+    x_lab += 1
+    y_lab += x/y
+
+print(result)
+
