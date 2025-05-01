9b3e1c15d12c8f0457ef0fa7e5ab96c0693223f7
dydwnsekd <dydwnsekd123@gmail.com>
2025-05-02 00:55:06 +0900

    백준 23275 Knot Knowledge python 완료

--- baekjoon/python/23275.py
+++ baekjoon/python/23275.py
@@ -2,14 +2,13 @@ import sys
 
 n = int(sys.stdin.readline())
 
-for _ in range(n):
-    x = sys.stdin.readline().strip().split()
-    y = sys.stdin.readline().strip().split()
+x = sys.stdin.readline().strip().split()
+y = sys.stdin.readline().strip().split()
 
-    x_y = []
+x_y = []
 
-    for xi in x:
-        if xi not in y:
-            x_y.append(xi)
+for xi in x:
+    if xi not in y:
+        x_y.append(xi)
 
 print("".join(x_y))
