d234e15bd2b85734c589e88d29c4884340fef332
dydwnsekd <dydwnsekd123@gmail.com>
2025-05-04 09:20:57 +0900

    백준 27222 Штангист python 완료

--- baekjoon/python/27222.py
+++ baekjoon/python/27222.py
@@ -4,10 +4,10 @@ n = int(sys.stdin.readline())
 i = list(map(int, sys.stdin.readline().split()))
 result = 0
 
-for i in range(n):
+for j in range(n):
     x, y = map(int, sys.stdin.readline().split())
 
-    if i == 1 and y > x:
+    if i[j] == 1 and y > x:
         result += y - x
 
 print(result)
