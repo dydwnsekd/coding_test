ba55397877de48e10f24e1d4157f3e1c6f343915
dydwnsekd <dydwnsekd123@gmail.com>
2025-05-22 01:06:58 +0900

    백준 6766 Big Bang Secrets python 완료

--- baekjoon/python/6766.py
+++ baekjoon/python/6766.py
@@ -6,6 +6,7 @@ k = int(sys.stdin.readline())
 s = sys.stdin.readline().strip()
 
 for i in range(len(s)):
-    result += chr(ord("A") + (ord(s[i]) + (3 * (i+1)) + k - ord("A")) % 26)
+    result += chr(ord("A") + (ord(s[i]) - (3 * (i+1)) - k - ord("A")) % 26)
 
 print(result)
+
