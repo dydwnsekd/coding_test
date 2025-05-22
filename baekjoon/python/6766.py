becbc98d0a8e014c503bfb3bfe44bd3cd834422a
dydwnsekd <dydwnsekd123@gmail.com>
2025-05-23 00:06:58 +0900

    백준 6766 Big Bang Secrets python 개선 코드 추가

--- baekjoon/python/6766.py
+++ baekjoon/python/6766.py
@@ -1,12 +1,27 @@
-import sys
+# import sys
+#
+# result = ""
+#
+# k = int(sys.stdin.readline())
+# s = sys.stdin.readline().strip()
+#
+# for i in range(len(s)):
+#     result += chr(ord("A") + (ord(s[i]) - (3 * (i+1)) - k - ord("A")) % 26)
+#
+# print(result)
 
-result = ""
+import sys
 
 k = int(sys.stdin.readline())
 s = sys.stdin.readline().strip()
 
-for i in range(len(s)):
-    result += chr(ord("A") + (ord(s[i]) - (3 * (i+1)) - k - ord("A")) % 26)
+base_ord = ord('A')
+decoded_chars = []
 
-print(result)
+for idx, ch in enumerate(s):
+    offset = (ord(ch) - (3 * (idx + 1)) - k - base_ord) % 26
+    decoded_char = chr(base_ord + offset)
+    decoded_chars.append(decoded_char)
+
+print(''.join(decoded_chars))
 
