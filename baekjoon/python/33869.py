9efb4517c22efad601b5873ae5a0ad1efd9cb2ac
dydwnsekd <dydwnsekd123@gmail.com>
2025-05-26 00:37:05 +0900

    백준 33869 일기 암호화하기 python 완료

--- baekjoon/python/33869.py
+++ baekjoon/python/33869.py
@@ -10,12 +10,19 @@ for i in range(ord('A'), ord('Z') + 1):
 w = sys.stdin.readline().strip()
 
 for c in w:
-    secret_dict[secret_alphabet] = c
-    alphabet_list.remove(secret_alphabet)
-    secret_alphabet = chr(ord(secret_alphabet) + 1)
+    if c in alphabet_list:
+        secret_dict[secret_alphabet] = c
+        alphabet_list.remove(c)
+        secret_alphabet = chr(ord(secret_alphabet) + 1)
 
 for c in alphabet_list:
     secret_dict[secret_alphabet] = c
     secret_alphabet = chr(ord(secret_alphabet) + 1)
 
-print(secret_dict)
+s = sys.stdin.readline().strip()
+result = ''
+
+for c in s:
+    result += secret_dict[c]
+
+print(result)
