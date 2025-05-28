6bd8aa82f18e914e905e654b3c02f31e47561102
dydwnsekd <dydwnsekd123@gmail.com>
2025-05-29 00:04:47 +0900

    백준 17851 This Problem’s a Slam Dunk python 수정 코드 추가

--- baekjoon/python/17851.py
+++ baekjoon/python/17851.py
@@ -1,12 +1,21 @@
+# import sys
+#
+# count = 0
+#
+# team1 = sorted(map(int, sys.stdin.readline().strip().split()))
+# team2 = sorted(map(int, sys.stdin.readline().strip().split()))
+#
+# for i in range(len(team1)):
+#     if team1[i] > team2[i]:
+#         count += 1
+#
+# print(count)
+
 import sys
 
-count = 0
+team1 = sorted(map(int, sys.stdin.readline().split()))
+team2 = sorted(map(int, sys.stdin.readline().split()))
 
-team1 = sorted(map(int, sys.stdin.readline().strip().split()))
-team2 = sorted(map(int, sys.stdin.readline().strip().split()))
+wins = sum(a > b for a, b in zip(team1, team2))
+print(wins)
 
-for i in range(len(team1)):
-    if team1[i] > team2[i]:
-        count += 1
-
-print(count)
