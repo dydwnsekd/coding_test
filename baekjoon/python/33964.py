89c44af6c54c3acd5ac4b31c4c82fff24d096d6a
dydwnsekd <dydwnsekd123@gmail.com>
2025-05-31 15:34:17 +0900

    백준 33964 레퓨닛의 덧셈 python 코드 수정

--- baekjoon/python/33964.py
+++ baekjoon/python/33964.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 x, y = map(int, sys.stdin.readline().strip().split())
@@ -14,3 +15,14 @@ x_num = int(x_num)
 y_num = int(y_num)
 
 print(x_num + y_num)
+"""
+
+import sys
+
+x, y = map(int, sys.stdin.readline().strip().split())
+
+x_num = int('1' * x)
+y_num = int('1' * y)
+
+print(x_num + y_num)
+
