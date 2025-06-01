2d3129f5dd738a3f6de2a7fb7a68c2b13342afb2
dydwnsekd <dydwnsekd123@gmail.com>
2025-06-01 18:48:17 +0900

    백준 33639 Candy Store python 완료

---
+++ baekjoon/python/33639.py
@@ -0,0 +1,21 @@
+import sys
+from collections import defaultdict
+
+n, q = map(int, sys.stdin.readline().strip().split())
+name_dict = defaultdict(list)
+
+for _ in range(n):
+    name = sys.stdin.readline().strip()
+    initials = name.split()[0][0] + name.split()[1][0]
+
+    if name not in name_dict[initials]:
+        name_dict[initials].append(name)
+
+for _ in range(q):
+    find_initials = sys.stdin.readline().strip()
+    if len(name_dict[find_initials]) == 1:
+        print(name_dict[find_initials][0])
+    elif len(name_dict[find_initials]) > 1:
+        print("ambiguous")
+    else:
+        print("nobody")
