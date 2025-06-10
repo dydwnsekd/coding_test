acdfa953d3ad4035f1c2b487ef01c2c4cad2f0c7
dydwnsekd <dydwnsekd123@gmail.com>
2025-06-10 10:04:19 +0900

    백준 5222 Vigenère Cipher python init

---
+++ baekjoon/python/5222.py
@@ -0,0 +1,18 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    plain_keyword, plaintext = sys.stdin.readline().split()
+    keyword = ""
+    result = ""
+
+    while len(plaintext) > len(keyword):
+        keyword += plain_keyword
+
+    keyword = keyword[:len(plaintext)]
+
+    for i in range(len(plaintext)):
+        result += chr(ord(plaintext[i]) + (ord(keyword[i]) - ord("A")))
+
+    print(result)
