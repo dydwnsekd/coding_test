9e3cd1b32df3e3d0a58c01f0ce4760a499e370e9
dydwnsekd <dydwnsekd123@gmail.com>
2025-06-12 10:56:51 +0900

    백준 5222 Vigenère Cipher python 수정 코드 추가

--- baekjoon/python/5222.py
+++ baekjoon/python/5222.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 t = int(sys.stdin.readline())
@@ -14,5 +15,22 @@ for _ in range(t):
 
     for i in range(len(plaintext)):
         result += chr((ord(plaintext[i]) + ord(keyword[i]) - ord("A") - ord("A") ) % 26 + ord("A"))
+
+    print("Ciphertext:", result)
+"""
+
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    plain_keyword, plaintext = sys.stdin.readline().split()
+
+    keyword = (plain_keyword * ((len(plaintext) + len(plain_keyword) - 1) // len(plain_keyword)))[:len(plaintext)]
+
+    result = ''.join(
+        chr((ord(p) + ord(k) - 2 * ord('A')) % 26 + ord('A'))
+        for p, k in zip(plaintext, keyword)
+    )
 
     print("Ciphertext:", result)
