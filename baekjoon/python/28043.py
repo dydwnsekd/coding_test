cfa5d2b0130fe5ec6a203d9b0926edd0790e15a0
dydwnsekd <dydwnsekd123@gmail.com>
2025-06-24 00:14:33 +0900

    백준 28043 Daily Trips python 일부 수정

--- baekjoon/python/28043.py
+++ baekjoon/python/28043.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 n, h, w = map(int, sys.stdin.readline().strip().split())
@@ -21,3 +22,28 @@ for _ in range(n):
         result += 'N'
 
     print(result)
+"""
+
+import sys
+
+n, h, w = map(int, sys.stdin.readline().split())
+
+for _ in range(n):
+    rain_h, rain_w = sys.stdin.readline().split()
+    res = []
+
+    if rain_h == 'Y' or (rain_h == 'N' and w == 0):
+        h -= 1
+        w += 1
+        res.append('Y')
+    else:
+        res.append('N')
+
+    if rain_w == 'Y' or (rain_w == 'N' and h == 0):
+        h += 1
+        w -= 1
+        res.append('Y')
+    else:
+        res.append('N')
+
+    print(' '.join(res))
