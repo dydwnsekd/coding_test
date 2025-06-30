03cf21085160400ab046bf5e94b3fd5f0fe49539
dydwnsekd <dydwnsekd123@gmail.com>
2025-07-01 00:04:07 +0900

    백준 10551 STROJOPIS python 개선 코드 추가

--- baekjoon/python/10551.py
+++ baekjoon/python/10551.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 type_text = \
@@ -23,4 +24,32 @@ for c in s:
 
 for i in range(len(finger_count)):
     print(finger_count[i])
+"""
+
+import sys
+
+finger_mapping = {}
+type_text = [
+    ["1", "Q", "A", "Z"],
+    ["2", "W", "S", "X"],
+    ["3", "E", "D", "C"],
+    ["4", "R", "F", "V", "5", "T", "G", "B"],
+    ["6", "Y", "H", "N", "7", "U", "J", "M"],
+    ["8", "I", "K", ","],
+    ["9", "O", "L", "."],
+    ["0", "P", ";", "/", "-", "[", "'", "=", "]"]
+]
+
+for finger_idx, keys in enumerate(type_text):
+    for key in keys:
+        finger_mapping[key] = finger_idx
+
+finger_count = [0] * 8
+s = sys.stdin.readline().strip()
+
+for c in s:
+    if c in finger_mapping:
+        finger_count[finger_mapping[c]] += 1
+
+print(*finger_count, sep='\n')
 
