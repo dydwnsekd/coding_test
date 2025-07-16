55df6857891e279a7ddeda9f506e485344e83fa1
dydwnsekd <dydwnsekd123@gmail.com>
2025-07-17 00:39:21 +0900

    백준 13580 Andando no tempo python 개선 코드 추가

--- baekjoon/python/13580.py
+++ baekjoon/python/13580.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 year_list = list(map(int, sys.stdin.readline().strip().split()))
@@ -6,6 +7,16 @@ year_list.sort()
 if year_list[0] == year_list[1] or year_list[1] == year_list[2]:
     print("S")
 elif year_list[2] == year_list[0] + year_list[1]:
+    print("S")
+else:
+    print("N")
+"""
+
+import sys
+
+years = sorted(map(int, sys.stdin.readline().split()))
+
+if years[0] == years[1] or years[1] == years[2] or years[2] == years[0] + years[1]:
     print("S")
 else:
     print("N")
