63be325278f7c35878a6405b1033712376533f6f
dydwnsekd <dydwnsekd123@gmail.com>
2025-07-21 00:05:21 +0900

    백준 15008 Falling Apart python 개선 코드 추가

--- baekjoon/python/15008.py
+++ baekjoon/python/15008.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 bob, alice = 0, 0
@@ -13,5 +14,18 @@ for i in range(len(a_list)):
         alice += a_list[i]
 
 print(bob, alice)
+"""
 
+import sys
 
+n = int(sys.stdin.readline())
+cards = sorted(map(int, sys.stdin.readline().split()), reverse=True)
+
+bob, alice = 0, 0
+for idx, value in enumerate(cards):
+    if idx % 2 == 0:
+        bob += value
+    else:
+        alice += value
+
+print(bob, alice)
