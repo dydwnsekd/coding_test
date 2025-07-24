3ed9364240d76f0a93e52fd0ac09d41bac062443
dydwnsekd <dydwnsekd123@gmail.com>
2025-07-24 09:52:26 +0900

    백준 5575 타임 카드 python 개선 코드 추가

--- baekjoon/python/5575.py
+++ baekjoon/python/5575.py
@@ -1,3 +1,4 @@
+"""
 import sys
 from datetime import datetime
 
@@ -15,4 +16,21 @@ for _ in range(3):
     seconds = total_seconds % 60
 
     print(hours, minutes, seconds)
+"""
 
+import sys
+
+for _ in range(3):
+    t1_h, t1_m, t1_s, t2_h, t2_m, t2_s = map(int, sys.stdin.readline().split())
+
+    # 시, 분, 초를 모두 초 단위로 변환
+    t1_seconds = t1_h * 3600 + t1_m * 60 + t1_s
+    t2_seconds = t2_h * 3600 + t2_m * 60 + t2_s
+
+    total_seconds = t2_seconds - t1_seconds
+
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+
+    print(hours, minutes, seconds)
