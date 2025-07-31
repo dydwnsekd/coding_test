b7b284ee516772fc718cdc59f42065d221a990a6
dydwnsekd <dydwnsekd123@gmail.com>
2025-07-31 09:07:43 +0900

    백준 28938 Конвейер python 일부 코드 개선

--- baekjoon/python/28938.py
+++ baekjoon/python/28938.py
@@ -3,9 +3,12 @@ import sys
 n = int(sys.stdin.readline())
 sequence_list = list(map(int, sys.stdin.readline().split()))
 
-if sequence_list.count(1) == sequence_list.count(-1):
+count_plus_1 = sequence_list.count(1)
+count_minus_1 = sequence_list.count(-1)
+
+if count_plus_1 == count_minus_1:
     print("Stay")
-elif sequence_list.count(1) > sequence_list.count(-1):
+elif count_plus_1 > count_minus_1:
     print("Right")
 else:
     print("Left")
