d5cf089eda083868f843b3b47c62dccdf2ac160c
dydwnsekd <dydwnsekd123@gmail.com>
2025-08-08 00:10:04 +0900

    백준 33990 3대 512 python init

---
+++ baekjoon/python/33990.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+k512 = sys.maxsize
+
+for _ in range(n):
+    temp = sum(list(map(int, sys.stdin.readline().strip().split())))
+    if temp >= 512 and (512 - temp) < k512:
+        k512 = temp
+
+if k512 == sys.maxsize:
+    print(-1)
+else:
+    print(k512)
