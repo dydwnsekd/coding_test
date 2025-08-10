a1521ee6dc62d1a5554d87d18e888b961bbed946
dydwnsekd <dydwnsekd123@gmail.com>
2025-08-10 10:45:58 +0900

    백준 33990 3대 512 python 개선 코드 추가

--- baekjoon/python/33990.py
+++ baekjoon/python/33990.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 n = int(sys.stdin.readline())
@@ -12,3 +13,17 @@ if k512 == sys.maxsize:
     print(-1)
 else:
     print(k512)
+"""
+
+import sys
+
+n = int(sys.stdin.readline())
+k512 = sys.maxsize
+
+for _ in range(n):
+    temp = sum(map(int, sys.stdin.readline().split()))
+    if temp >= 512:
+        k512 = min(k512, temp)
+
+print(-1 if k512 == sys.maxsize else k512)
+
