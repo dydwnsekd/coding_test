6e2850619d5824ced5c1dc51ed81f4b27deb7eeb
dydwnsekd <dydwnsekd123@gmail.com>
2025-08-11 09:47:28 +0900

    백준 30569 Last One Standing python init

---
+++ baekjoon/python/30569.py
@@ -0,0 +1,31 @@
+import sys
+
+p1h, p1d, p1t = map(int, sys.stdin.readline().strip().split())
+p2h, p2d, p2t = map(int, sys.stdin.readline().strip().split())
+
+p1h -= p2d
+p2h -= p1d
+
+p1k = 1
+p2k = 1
+
+while True:
+    if p1h <= p2d * p2k:
+        p2k = p2k * p2t
+        break
+    else:
+        p2k += 1
+
+while True:
+    if p2h <= p1d * p1k:
+        p1k = p1k * p1t
+        break
+    else:
+        p1k += 1
+
+if p1k > p2k:
+    print("player two")
+elif p1k < p2k:
+    print("player one")
+else:
+    print("draw")
