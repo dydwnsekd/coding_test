8ecaf5b26c64ae24a5bf583fc0c4cd076e2632c4
dydwnsekd <dydwnsekd123@gmail.com>
2025-08-12 00:50:13 +0900

    백준 30569 Last One Standing python 코드 수정

--- baekjoon/python/30569.py
+++ baekjoon/python/30569.py
@@ -9,23 +9,30 @@ p2h -= p1d
 p1k = 1
 p2k = 1
 
-while True:
-    if p1h <= p2d * p2k:
-        p2k = p2k * p2t
-        break
-    else:
-        p2k += 1
-
-while True:
-    if p2h <= p1d * p1k:
-        p1k = p1k * p1t
-        break
-    else:
-        p1k += 1
-
-if p1k > p2k:
-    print("player two")
-elif p1k < p2k:
-    print("player one")
-else:
+if p1h < 0 and p2h < 0:
     print("draw")
+elif p1h > 0 and p2h < 0:
+    print("player one")
+elif p1h < 0 and p2h > 0:
+    print("player two")
+else:
+    while True:
+        if p1h <= p2d * p2k:
+            p2k = p2k * p2t
+            break
+        else:
+            p2k += 1
+
+    while True:
+        if p2h <= p1d * p1k:
+            p1k = p1k * p1t
+            break
+        else:
+            p1k += 1
+
+    if p1k > p2k:
+        print("player two")
+    elif p1k < p2k:
+        print("player one")
+    else:
+        print("draw")
