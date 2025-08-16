a7166d1711c95570d618e714cc9d16e39082f931
dydwnsekd <dydwnsekd123@gmail.com>
2025-08-16 23:44:10 +0900

    백준 10539 수빈이와 수열 python 완료

--- baekjoon/python/10539.py
+++ baekjoon/python/10539.py
@@ -5,7 +5,7 @@ b_list = list(map(int, sys.stdin.readline().strip().split()))
 a_list = b_list[:1]
 
 for i in range(1, n):
-    a_list.append((b_list[i-1] * i) - sum(a_list))
+    a_list.append((b_list[i] * (i+1)) - sum(a_list))
 
 print(*a_list)
 
