23a79c7b4cd3febfd23535074514b1a46712a929
dydwnsekd <dydwnsekd123@gmail.com>
2025-08-17 00:48:32 +0900

    백준 10539 수빈이와 수열 python 개선 코드 추가

--- baekjoon/python/10539.py
+++ baekjoon/python/10539.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 n = int(sys.stdin.readline())
@@ -8,5 +9,19 @@ for i in range(1, n):
     a_list.append((b_list[i] * (i+1)) - sum(a_list))
 
 print(*a_list)
+"""
 
+import sys
+
+n = int(sys.stdin.readline())
+b_list = list(map(int, sys.stdin.readline().split()))
+
+a_list = []
+total = 0
+for i in range(n):
+    a_i = b_list[i] * (i + 1) - total
+    a_list.append(a_i)
+    total += a_i
+
+print(*a_list)
 
