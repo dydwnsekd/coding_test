3c8d7508686ad109f7a2b1eed0a53ea99456953c
dydwnsekd <dydwnsekd123@gmail.com>
2025-08-21 00:28:48 +0900

    백준 34071 첫 번째 문제는 정말 쉬운 문제일까? 개선 코드 추가

--- baekjoon/python/34071.py
+++ baekjoon/python/34071.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 difficult = []
@@ -12,3 +13,27 @@ elif max(difficult) == difficult[0]:
     print("hard")
 else:
     print("?")
+"""
+
+import sys
+
+n = int(sys.stdin.readline())
+first = int(sys.stdin.readline())
+
+min_val = first
+max_val = first
+
+for _ in range(n - 1):
+    x = int(sys.stdin.readline())
+    if x < min_val:
+        min_val = x
+    if x > max_val:
+        max_val = x
+
+if min_val == first:
+    print("ez")
+elif max_val == first:
+    print("hard")
+else:
+    print("?")
+
