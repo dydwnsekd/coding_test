b510a7af4bd81f5ffb123c16f1aab15593b299e7
dydwnsekd <dydwnsekd123@gmail.com>
2025-09-03 10:05:37 +0900

    rps 함수 추가

--- util/python_rps.py
+++ util/python_rps.py
@@ -1,5 +1,17 @@
 import random
 
+
+def rps(p1, p2):
+    RPS_RULES = {
+        ("R", "S"): 1, ("R", "P"): 2,
+        ("P", "R"): 1, ("P", "S"): 2,
+        ("S", "P"): 1, ("S", "R"): 2,
+    }
+
+    if p1 == p2:
+        return 0
+    return RPS_RULES.get((p1, p2), 0)
+
 n = random.randrange(1,4)
 
 if n == 1:
