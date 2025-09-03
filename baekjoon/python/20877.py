958fb231436287ad7e583a9156b0cf9e62cd5116
dydwnsekd <dydwnsekd123@gmail.com>
2025-09-04 00:48:10 +0900

    백준 20877 Minigolf python init

---
+++ baekjoon/python/20877.py
@@ -0,0 +1,15 @@
+import sys
+
+result = 0
+
+n = int(sys.stdin.readline())
+storkes = list(map(int, sys.stdin.readline().split()))
+
+for i in range(len(storkes)):
+    if i % 2 == 0:
+        result += storkes[i] - 2
+    else:
+        result += storkes[i] - 3
+
+print(result)
+
