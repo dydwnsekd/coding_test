9d7f41092619314fe6906d8182135cba9b510401
dydwnsekd <dydwnsekd123@gmail.com>
2025-09-05 09:33:31 +0900

    백준 20877 Minigolf python 완료

--- baekjoon/python/20877.py
+++ baekjoon/python/20877.py
@@ -6,6 +6,9 @@ n = int(sys.stdin.readline())
 storkes = list(map(int, sys.stdin.readline().split()))
 
 for i in range(len(storkes)):
+    if storkes[i] > 7:
+        storkes[i] = 7
+
     if i % 2 == 0:
         result += storkes[i] - 2
     else:
