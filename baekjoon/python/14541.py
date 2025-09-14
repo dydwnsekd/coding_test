2b9d73e304fd318bcef835abe3fefd37a2e8b5b8
dydwnsekd <dydwnsekd123@gmail.com>
2025-09-15 01:05:49 +0900

    백준 14541 Speed Limit python 개선 코드 추가

--- baekjoon/python/14541.py
+++ baekjoon/python/14541.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 while True:
@@ -15,3 +16,20 @@ while True:
             else:
                 distance += speed_list[i][0] * (speed_list[i][1] - speed_list[i-1][1])
     print(f"{distance} miles")
+"""
+import sys
+
+while True:
+    n = int(sys.stdin.readline())
+    if n == -1:
+        break
+
+    distance = 0
+    prev_time = 0
+    for _ in range(n):
+        speed, time = map(int, sys.stdin.readline().split())
+        distance += speed * (time - prev_time)
+        prev_time = time
+
+    print(f"{distance} miles")
+
