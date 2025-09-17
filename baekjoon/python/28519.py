3f29728af6c5fe274ad0194c7f38ec916ae0c165
dydwnsekd <dydwnsekd123@gmail.com>
2025-09-17 21:08:25 +0900

    백준 28519 Планеты двух измерений python 코드 개선

--- baekjoon/python/28519.py
+++ baekjoon/python/28519.py
@@ -1,8 +1,4 @@
 import sys
 
-x, y = map(int, sys.stdin.readline().strip().split())
-
-if x == y:
-    print(x + y)
-else:
-    print(min(x, y) * 2 + 1)
+x, y = map(int, sys.stdin.readline().split())
+print(min(x + y, 2 * min(x, y) + 1))
