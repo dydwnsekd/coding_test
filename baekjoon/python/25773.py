aebd73b0cf6e1ae4de9ba3a75c8a35966b409547
dydwnsekd <dydwnsekd123@gmail.com>
2025-09-19 00:29:10 +0900

    백준 25773 Number Maximization python 코드 개선

--- baekjoon/python/25773.py
+++ baekjoon/python/25773.py
@@ -1,11 +1,5 @@
 import sys
 
 n = sys.stdin.readline().strip()
-split_n = []
-
-for i in n:
-    split_n.append(i)
-
-split_n = sorted(split_n, reverse=True)
-print("".join(split_n))
+print("".join(sorted(n, reverse=True)))
 
