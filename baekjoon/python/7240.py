e6d0008c8d8f3b3b83963e384f1259973603107d
dydwnsekd <dydwnsekd123@gmail.com>
2025-09-22 09:22:59 +0900

    백준 7240 Rogutės python 개선 코드 추가

--- baekjoon/python/7240.py
+++ baekjoon/python/7240.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 n, s = map(int, sys.stdin.readline().strip().split())
@@ -10,3 +11,18 @@ for _ in range(n-1):
         current_speed -= 1
 
 print(current_speed + int(sys.stdin.readline()))
+"""
+
+import sys
+
+n, s = map(int, sys.stdin.readline().split())
+changes = [int(sys.stdin.readline()) for _ in range(n)]
+
+current_speed = 0
+for i, delta in enumerate(changes):
+    current_speed += delta
+    if i < n - 1 and current_speed > s:
+        current_speed -= 1
+
+print(current_speed)
+
