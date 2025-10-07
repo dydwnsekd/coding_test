efb49475a26c8c2cbafa0be5035d426a13481c9c
dydwnsekd <dydwnsekd123@gmail.com>
2025-10-07 18:44:47 +0900

    백준 33709 치매예방수칙 3.3.3 python 개선 코드 추가

--- baekjoon/python/33709.py
+++ baekjoon/python/33709.py
@@ -1,6 +1,16 @@
+"""
 import sys
 
 n = int(sys.stdin.readline())
 num_list = list(map(int, sys.stdin.readline().strip().replace('|', '.').replace(':', '.').replace('#', '.').split('.')))
 
 print(sum(num_list))
+"""
+
+import re
+import sys
+
+n = int(sys.stdin.readline())
+numbers = list(map(int, re.split(r'[.|:#]', sys.stdin.readline().strip())))
+
+print(sum(numbers))
