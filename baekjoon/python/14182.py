895f5c1e15914fb630dbe8125a16eee6c2b4c8e1
dydwnsekd <dydwnsekd123@gmail.com>
2025-10-10 00:11:11 +0900

    백준 14182 tax python 개선 코드 추가

--- baekjoon/python/14182.py
+++ baekjoon/python/14182.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 while True:
@@ -12,3 +13,22 @@ while True:
         print(int(n - n * 0.1))
     else:
         print(int(n - n * 0.2))
+"""
+
+import sys
+
+while True:
+    n = int(sys.stdin.readline().strip())
+    if n == 0:
+        break
+
+    if n <= 1_000_000:
+        discount = 0
+    elif n <= 5_000_000:
+        discount = 0.1
+    else:
+        discount = 0.2
+
+    final_price = int(n * (1 - discount))
+    print(final_price)
+
