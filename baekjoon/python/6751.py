3bed08d119020c47deb87f4fecf0abe3f07fcc41
dydwnsekd <dydwnsekd123@gmail.com>
2025-10-13 09:06:26 +0900

    백준 6751 From 1987 to 2013 python init

---
+++ baekjoon/python/6751.py
@@ -0,0 +1,17 @@
+import sys
+from collections import defaultdict
+
+y = sys.stdin.readline().strip()
+
+while True:
+    y = str(int(y) + 1)
+
+    num_dict = defaultdict(int)
+    for c in y:
+        num_dict[c] += 1
+
+    if len(num_dict) == 4:
+        break
+
+print(y)
+
