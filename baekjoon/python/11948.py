46936825f97e2284d3396bdf08591a2025676650
dydwnsekd <dydwnsekd123@gmail.com>
2025-10-17 09:03:31 +0900

    백준 11948 과목선택 python 개선 코드 추가

--- baekjoon/python/11948.py
+++ baekjoon/python/11948.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 science_score = []
@@ -12,3 +13,20 @@ for i in range(6):
 score = sum(science_score) + sum(society_score) - min(science_score) - min(society_score)
 
 print(score)
+"""
+
+import sys
+
+scores = [int(sys.stdin.readline().strip()) for _ in range(6)]
+
+science_scores = scores[:4]
+society_scores = scores[4:]
+
+total_score = (
+    sum(science_scores)
+    + sum(society_scores)
+    - min(science_scores)
+    - min(society_scores)
+)
+
+print(total_score)
