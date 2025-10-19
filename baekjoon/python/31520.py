9f8c53affd1ccdf736db56e488c9f9c3e705edd8
dydwnsekd <dydwnsekd123@gmail.com>
2025-10-19 12:35:54 +0900

    백준 31520 Champernowne Verification python 완료

--- baekjoon/python/31520.py
+++ baekjoon/python/31520.py
@@ -6,13 +6,17 @@ find_num = '1'
 find_index = 0
 
 while True:
-    if n[find_index:find_index+len(find_num)] == find_num:
+    n_num = n[find_index:find_index+len(find_num)]
+    if n_num == find_num:
+        find_index += len(find_num)
         find_num = str(int(find_num) + 1)
+    elif find_index == len(n):
+        break
     else:
         champernowne_flag = False
         break
 
 if champernowne_flag:
-    print(find_num)
+    print(int(find_num) - 1)
 else:
     print(-1)
