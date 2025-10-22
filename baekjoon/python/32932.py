5d4dc855f9762a98c6ab0a0bb05de4c52dedc627
dydwnsekd <dydwnsekd123@gmail.com>
2025-10-22 10:07:02 +0900

    백준 32932 드론 조작 python 완료

--- baekjoon/python/32932.py
+++ baekjoon/python/32932.py
@@ -1,7 +1,7 @@
 import sys
 
 block = []
-start = [0, 0]
+coordinate = [0, 0]
 n, k = map(int, sys.stdin.readline().strip().split())
 
 for _ in range(n):
@@ -11,17 +11,17 @@ controlls = sys.stdin.readline().strip()
 
 for controll in controlls:
     if controll == 'U':
-        if [start[0], start[1] + 1] not in block:
-            start = [start[0], start[1] + 1]
+        if [coordinate[0], coordinate[1] + 1] not in block:
+            coordinate = [coordinate[0], coordinate[1] + 1]
     elif controll == 'D':
-        if [start[0], start[1] - 1] not in block:
-            start = [start[0], start[1] - 1]
+        if [coordinate[0], coordinate[1] - 1] not in block:
+            coordinate = [coordinate[0], coordinate[1] - 1]
     elif controll == 'L':
-        if [start[0], start[1] + 1] not in block:
-            start = [start[0], start[1] + 1]
+        if [coordinate[0] - 1, coordinate[1]] not in block:
+            coordinate = [coordinate[0] - 1, coordinate[1]]
     elif controll == 'R':
-        if [start[0], start[1] - 1] not in block:
-            start = [start[0], start[1] - 1]
+        if [coordinate[0] + 1, coordinate[1]] not in block:
+            coordinate = [coordinate[0] + 1, coordinate[1]]
 
-print(' '.join(map(str, start)))
+print(' '.join(map(str, coordinate)))
 
