06a46b318515d4f769622d9ed6c1f6b68e2811e5
dydwnsekd <dydwnsekd123@gmail.com>
2025-11-06 01:45:26 +0900

    백준 30068 Stovėjimo aikštelė python 개선 코드 추가

--- baekjoon/python/30068.py
+++ baekjoon/python/30068.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 m = int(sys.stdin.readline())
@@ -15,4 +16,18 @@ for _ in range(m):
 
 for r in result_list:
     print(*r)
+"""
 
+import sys
+
+m = int(sys.stdin.readline().strip())
+parking_times = {}
+
+for _ in range(m):
+    time, car_id = map(int, sys.stdin.readline().strip().split())
+
+    if car_id not in parking_times:
+        parking_times[car_id] = time
+    else:
+        print(car_id, time - parking_times[car_id])
+        del parking_times[car_id]
