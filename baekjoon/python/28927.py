db6322e7a3cac1831fd8929926307ee758f935ff
dydwnsekd <dydwnsekd123@gmail.com>
2025-11-12 10:19:55 +0900

    백준 28927 Киноманы python 개선 코드 추가

--- baekjoon/python/28927.py
+++ baekjoon/python/28927.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 movie_time = [3, 20, 120]
@@ -18,4 +19,21 @@ elif max_view < mel_view:
     print("Mel")
 else:
     print("Draw")
+"""
 
+import sys
+
+movie_durations = [3, 20, 120]
+
+max_scores = list(map(int, sys.stdin.readline().strip().split()))
+mel_scores = list(map(int, sys.stdin.readline().strip().split()))
+
+max_total = sum(a * b for a, b in zip(max_scores, movie_durations))
+mel_total = sum(a * b for a, b in zip(mel_scores, movie_durations))
+
+if max_total > mel_total:
+    print("Max")
+elif max_total < mel_total:
+    print("Mel")
+else:
+    print("Draw")
