52b7a8d7b3ecf591af8e7100b420f754d7699fa8
dydwnsekd <dydwnsekd123@gmail.com>
2025-11-27 23:50:55 +0900

    백준 16427 Time Limits python 개선 코드 추가

--- baekjoon/python/16427.py
+++ baekjoon/python/16427.py
@@ -1,3 +1,4 @@
+"""
 import sys
 import math
 
@@ -6,4 +7,16 @@ n, s = map(int, sys.stdin.readline().strip().split())
 solve_time = max(map(int, sys.stdin.readline().strip().split()))
 
 print(math.ceil(solve_time * s / 1000))
+"""
 
+import sys
+import math
+
+n, s = map(int, sys.stdin.readline().strip().split())
+max_time = 0
+
+for t in map(int, sys.stdin.readline().strip().split()):
+    if t > max_time:
+        max_time = t
+
+print(math.ceil(max_time * s / 1000))
