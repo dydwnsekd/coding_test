2be7175e10181cf2eb9a1a9906582950ec08f3b1
dydwnsekd <dydwnsekd123@gmail.com>
2025-11-29 23:44:12 +0900

    백준 15366 Olivander python init

---
+++ baekjoon/python/15366.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+x_list = list(map(int, sys.stdin.readline().split()))
+y_list = list(map(int, sys.stdin.readline().split()))
+flag = True
+
+for i in range(n):
+    if x_list[i] > y_list[i]:
+        flag = False
+        break
+
+if flag:
+    print("DA")
+else:
+    print("NE")
+
