6ea5a0ca55878cf3051753afccf8245848ecb42f
dydwnsekd <dydwnsekd123@gmail.com>
2025-12-02 10:53:33 +0900

    백준 32661 Airfare Grants python 완료

--- baekjoon/python/32661.py
+++ baekjoon/python/32661.py
@@ -6,5 +6,9 @@ flight_list = []
 for _ in range(n):
     flight_list.append(int(sys.stdin.readline().strip()))
 
-print(abs(max(flight_list) // 2 - min(flight_list)))
+if max(flight_list) // 2 > min(flight_list):
+    print(0)
+else:
+    print(min(flight_list) - max(flight_list) // 2)
+
 
