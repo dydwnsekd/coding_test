6e51f14b74452a79d8a42a8359cc0e328ded4d19
dydwnsekd <dydwnsekd123@gmail.com>
2025-12-08 22:19:10 +0900

    백준 34509 2, 4, 6, 8 python logic 추가

--- baekjoon/python/34509.py
+++ baekjoon/python/34509.py
@@ -1,1 +1,22 @@
-print(42)
+# print(42)
+
+result = []
+
+for n in range(10, 100):
+    s = str(n)
+
+    if '8' in s:
+        continue
+
+    rev = int(s[::-1])
+
+    if rev % 4 != 0:
+        continue
+
+    digit_sum = sum(int(ch) for ch in s)
+    if digit_sum % 6 != 0:
+        continue
+
+    result.append(n)
+
+print(result)
