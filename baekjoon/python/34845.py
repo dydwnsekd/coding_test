1d8b850edfee5140615d92f1e6211b4a0538c648
dydwnsekd <dydwnsekd123@gmail.com>
2025-12-12 21:15:44 +0900

    백준 34845 강의평 python 완료

--- baekjoon/python/34845.py
+++ baekjoon/python/34845.py
@@ -3,14 +3,12 @@ import sys
 n, x = map(int, sys.stdin.readline().strip().split())
 score_list = list(map(int, sys.stdin.readline().strip().split()))
 
+total = sum(score_list)
 count = 0
 
-while True:
-    if sum(score_list) / n >= x:
-        break
-    else:
-        count += 1
-        n += 1
-        score_list.append(100)
+while total / n < x:
+    total += 100
+    n += 1
+    count += 1
 
 print(count)
