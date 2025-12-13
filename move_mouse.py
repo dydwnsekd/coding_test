8125ca4e85380deeb2c8c1b4dddaba93b3dc1558
dydwnsekd <dydwnsekd123@gmail.com>
2025-12-13 19:20:47 +0900

    자동 마우스 무빙 일부 수정

--- move_mouse.py
+++ move_mouse.py
@@ -15,22 +15,28 @@ def move_random_mouse(duration=10):
         time.sleep(0.3)
 
 def main():
-    run_limit = 3600  # 1시간
+    hour = 10
+
+    run_limit = 3600 * hour
     start_time = time.time()
 
     while True:
         # 종료 여부 체크
         if time.time() - start_time >= run_limit:
-            print("[INFO] 1시간 경과, 프로그램 종료")
+            print(f"[INFO] {hour}시간 경과, 프로그램 종료")
             break
+        else:
+            print(f"경과 시간(s) : {round(time.time() - start_time)}")
 
         print("[INFO] 3분 대기중...")
         time.sleep(180)
 
         # 종료 여부 체크
         if time.time() - start_time >= run_limit:
-            print("[INFO] 1시간 경과, 프로그램 종료")
+            print(f"[INFO] {hour}시간 경과, 프로그램 종료")
             break
+        else:
+            print(f"경과 시간(s) : {round(time.time() - start_time)}")
 
         print("[INFO] 마우스 랜덤 이동 시작 (10초)")
         move_random_mouse(duration=10)
