11679a175e3bd04147fa4f31ea0faf1b06063a73
dydwnsekd <dydwnsekd123@gmail.com>
2025-12-30 20:31:37 +0900

    백준 34529 Acquiring SW-IT Corn python 코드 수정

--- baekjoon/python/34529.py
+++ baekjoon/python/34529.py
@@ -1,13 +1,9 @@
 import sys
 
-value = 0
-
 x, y, z = map(int, sys.stdin.readline().strip().split())
 u, v, w = map(int, sys.stdin.readline().strip().split())
 
-value += (u / 100) * x
-value += (v / 50) * y
-value += (w / 20) * z
+value = (u * x) / 100 + (v * y) / 50 + (w * z) / 20
 
 print(int(value))
 
