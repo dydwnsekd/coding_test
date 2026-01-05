5de8472e6464c724d26d69482886d20b1b880ac8
dydwnsekd <dydwnsekd123@gmail.com>
2026-01-05 09:31:24 +0900

    백준 34946 셔틀 탈래 말래 탈래 말래 애매하긴 해 python 개선 코드 추가

--- baekjoon/python/34946.py
+++ baekjoon/python/34946.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 a, b, c, d = map(int, sys.stdin.readline().strip().split())
@@ -10,3 +11,12 @@ elif a + b <= d and c > d:
     print("Shuttle")
 elif a + b > d and c <= d:
     print("Walk")
+"""
+import sys
+
+a, b, c, d = map(int, sys.stdin.readline().strip().split())
+
+if (a + b <= d) == (c <= d):
+    print("~.~" if a + b <= d else "T.T")
+else:
+    print("Shuttle" if a + b <= d else "Walk")
