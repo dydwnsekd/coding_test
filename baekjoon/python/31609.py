cf5e526833d574748d0ae69e234307012b1b12c6
dydwnsekd <dydwnsekd123@gmail.com>
2026-01-13 09:17:57 +0900

    백준 31609 現れている数字 (Appearing Numbers) python 개선 코드 추가

--- baekjoon/python/31609.py
+++ baekjoon/python/31609.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 n = int(sys.stdin.readline())
@@ -6,4 +7,13 @@ num_list.sort()
 
 for n in num_list:
     print(n)
+"""
+
+import sys
+
+_ = int(sys.stdin.readline().strip())
+unique_numbers = sorted(set(map(int, sys.stdin.readline().strip().split())))
+
+for value in unique_numbers:
+    print(value)
 
