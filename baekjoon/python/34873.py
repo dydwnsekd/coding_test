67baedde186d33770832079d9b98f5ef9d0e5101
dydwnsekd <dydwnsekd123@gmail.com>
2026-01-22 00:08:57 +0900

    백준 34873 사탕 나눠주기 python 완료

--- baekjoon/python/34873.py
+++ baekjoon/python/34873.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 
 n = int(sys.stdin.readline())
 candy_list = list(map(int, sys.stdin.readline().split()))
+success_count = 0
 
 candy_count = defaultdict(int)
 
@@ -10,5 +11,13 @@ for candy in candy_list:
     candy_count[candy] += 1
 
 for candy, count in candy_count.items():
-    print(candy, count)
+    if count > 1:
+        success_count += 1
+    elif count == 1:
+        success_count += 0.5
+
+if success_count >= n:
+    print('Yes')
+else:
+    print('No')
 
