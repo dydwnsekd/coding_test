31aa5717adb29eb25ac35ac7e57b73e2dea1292f
dydwnsekd <dydwnsekd123@gmail.com>
2026-01-25 18:37:32 +0900

    백준 34308 Abby's Absolutes python 완료

--- baekjoon/python/34308.py
+++ baekjoon/python/34308.py
@@ -4,7 +4,7 @@ n, k = map(int, sys.stdin.readline().strip().split())
 apple_list = list(map(int, sys.stdin.readline().strip().split()))
 
 for apple in apple_list:
-    if apple >= n / 2:
+    if abs(apple - 1) > abs(apple - n):
         print(n, end=" ")
     else:
         print(1, end=" ")
