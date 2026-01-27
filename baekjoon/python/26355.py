43d154bce2a1a541156794c967e826772c5ce4e9
dydwnsekd <dydwnsekd123@gmail.com>
2026-01-27 23:49:59 +0900

    백준 26355 Sorry About That, Chief! python 완료

---
+++ baekjoon/python/26355.py
@@ -0,0 +1,33 @@
+# commit 대기
+import sys
+
+def prime_list(num):
+    prime_flag = [True] * num
+
+    m = int(num ** 0.5)
+    for i in range(2, m + 1):
+        if prime_flag[i]:
+            for j in range(i * i, num, i):
+                prime_flag[j] = False
+
+    return [i for i in range(2, num) if prime_flag[i]]
+
+prime_list = prime_list(10010)
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    v = int(sys.stdin.readline())
+
+    print(f"Input value: {v}")
+
+    if v in prime_list:
+        print("Would you believe it; it is a prime!\n")
+    else:
+        min_distance = 10000
+        for i in prime_list:
+            if abs(i-v) < min_distance:
+                min_distance = abs(i-v)
+
+        print(f"Missed it by that much ({min_distance})!\n")
+
