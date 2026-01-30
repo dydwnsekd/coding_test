0e16849ce3ac3cd4de450a6811379904465216c2
dydwnsekd <dydwnsekd123@gmail.com>
2026-01-30 10:14:37 +0900

    백준 34454 Roller Coaster Ride python print 구문 수정

--- baekjoon/python/34454.py
+++ baekjoon/python/34454.py
@@ -4,8 +4,11 @@ n = int(sys.stdin.readline())
 c = int(sys.stdin.readline())
 p = int(sys.stdin.readline())
 
+"""
 if c * p < n:
     print("no")
 else:
     print("yes")
+"""
 
+print("yes" if c * p >= n else "no")
