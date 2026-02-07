7c414c5400b1ceef4469145e62bbf13464e9bc96
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-07 22:24:38 +0900

    백준 34922 사각지대 python 개선 코드 추가

--- baekjoon/python/34922.py
+++ baekjoon/python/34922.py
@@ -1,6 +1,7 @@
 import sys
+import math
 
 w, h = map(int, sys.stdin.readline().strip().split())
 r = int(sys.stdin.readline())
 
-print(round((w * h) - ((r ** 2) * 3.141592 / 4 ), 2))
+print(round((w * h) - ((r ** 2) * math.pi / 4 ), 2))
