a8672d70359bbd9abff50464661440d0ead4627f
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-08 19:22:03 +0900

    백준 34553 알파벳 점수 계산기 python init

---
+++ baekjoon/python/34553.py
@@ -0,0 +1,17 @@
+import sys
+
+score = 0
+result = 0
+s = sys.stdin.readline().strip()
+current_c = 'z'
+
+for c in s:
+    if ord(current_c) > ord(c):
+        score += 1
+        current_c = c
+        result += score
+    else:
+        current_c = c
+        result += score
+
+print(result)
