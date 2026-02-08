d8083c772d2afbca00cdecd398da50b089f3564b
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-09 07:04:24 +0900

    백준 34553 알파벳 점수 계산기 python 완료

--- baekjoon/python/34553.py
+++ baekjoon/python/34553.py
@@ -6,11 +6,12 @@ s = sys.stdin.readline().strip()
 current_c = 'z'
 
 for c in s:
-    if ord(current_c) > ord(c):
+    if ord(current_c) < ord(c):
         score += 1
         current_c = c
         result += score
     else:
+        score = 1
         current_c = c
         result += score
 
