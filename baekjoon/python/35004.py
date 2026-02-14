119c93d4c48ef6861cbc953ab52b3c648b176d48
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-15 08:19:21 +0900

    백준 35004 Fight Against Numbers python 완료

--- baekjoon/python/35004.py
+++ baekjoon/python/35004.py
@@ -5,7 +5,7 @@ count = 0
 n = int(sys.stdin.readline())
 bin_n = bin(n)
 
-while bin_n != 0:
+while int(bin_n, 2) != 0:
     reverse_n = '0b' + bin_n[2:][::-1]
     bin_n = bin(int(reverse_n, 2) - 1)
 
