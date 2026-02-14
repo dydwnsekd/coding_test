81b14460017a86bf122d0e7147faf5f42dc8db7b
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-14 17:11:37 +0900

    백준 35004 Fight Against Numbers python init

---
+++ baekjoon/python/35004.py
@@ -0,0 +1,15 @@
+import sys
+
+count = 0
+
+n = int(sys.stdin.readline())
+bin_n = bin(n)
+
+while bin_n != 0:
+    reverse_n = '0b' + bin_n[2:][::-1]
+    bin_n = bin(int(reverse_n, 2) - 1)
+
+    count += 1
+
+print(count)
+
