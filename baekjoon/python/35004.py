147ef0e0af90e248cd65978766978e8fa6e54e9c
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-16 22:21:06 +0900

    백준 35004 Fight Against Numbers python 개선 코드 추가

--- baekjoon/python/35004.py
+++ baekjoon/python/35004.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 count = 0
@@ -9,6 +10,25 @@ while int(bin_n, 2) != 0:
     reverse_n = '0b' + bin_n[2:][::-1]
     bin_n = bin(int(reverse_n, 2) - 1)
 
+    count += 1
+
+print(count)
+"""
+
+import sys
+
+def reverse_bits(x: int) -> int:
+    rev = 0
+    while x > 0:
+        rev = (rev << 1) | (x & 1)
+        x >>= 1
+    return rev
+
+n = int(sys.stdin.readline().strip())
+count = 0
+
+while n > 0:
+    n = reverse_bits(n) - 1
     count += 1
 
 print(count)
