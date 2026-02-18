c7c8690c87df3f4d6e5f267194177372f16418f5
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-18 21:35:44 +0900

    백준 33080 Party Medley python 코드 수정

--- baekjoon/python/33080.py
+++ baekjoon/python/33080.py
@@ -6,10 +6,13 @@ n, m = map(int, sys.stdin.readline().strip().split())
 score_list = sorted(list(map(int, sys.stdin.readline().strip().split())))
 
 for i in range(len(score_list) - 2):
-    if max(score_list[i:i + 3]) - min(score_list[i:i + 3]) < 150:
+    if max(score_list[i:i + 3]) - min(score_list[i:i + 3]) <= m:
         balance_count += 1
         if max_value < sum(score_list[i:i + 3]):
             max_value = sum(score_list[i:i + 3])
 
-print(balance_count, max_value)
+if balance_count == 0:
+    print(-1)
+else:
+    print(balance_count, max_value)
 
