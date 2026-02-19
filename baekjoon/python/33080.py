f82b519b8d759e14b613d06de918f0cb138e0984
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-19 09:41:46 +0900

    백준 33080 Party Medley python 완료

--- baekjoon/python/33080.py
+++ baekjoon/python/33080.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 max_value = 0
@@ -10,6 +11,26 @@ for i in range(len(score_list) - 2):
         balance_count += 1
         if max_value < sum(score_list[i:i + 3]):
             max_value = sum(score_list[i:i + 3])
+
+if balance_count == 0:
+    print(-1)
+else:
+    print(balance_count, max_value)
+"""
+
+import sys
+from itertools import combinations
+
+max_value = 0
+balance_count = 0
+n, m = map(int, sys.stdin.readline().strip().split())
+score_list = list(map(int, sys.stdin.readline().strip().split()))
+
+for i in combinations(score_list, 3):
+    if max(i) - min(i) <= m:
+        balance_count += 1
+        if max_value < sum(i):
+            max_value = sum(i)
 
 if balance_count == 0:
     print(-1)
