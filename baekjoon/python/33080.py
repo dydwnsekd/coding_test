6b7075660785334587a4449fca7d8f45dad305d2
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-20 09:53:00 +0900

    백준 33080 Party Medley python 개선 코드 추가

--- baekjoon/python/33080.py
+++ baekjoon/python/33080.py
@@ -18,6 +18,7 @@ else:
     print(balance_count, max_value)
 """
 
+"""
 import sys
 from itertools import combinations
 
@@ -36,4 +37,32 @@ if balance_count == 0:
     print(-1)
 else:
     print(balance_count, max_value)
+"""
+
+import sys
+
+n, m = map(int, sys.stdin.readline().strip().split())
+a = sorted(map(int, sys.stdin.readline().strip().split()))
+
+count = 0
+best_sum = 0
+
+for i in range(n - 2):
+    k = i + 2
+    for j in range(i + 1, n - 1):
+        if k < j + 1:
+            k = j + 1
+
+        while k < n and a[k] - a[i] <= m:
+            k += 1
+
+        if k - 1 >= j + 1:
+            cnt = (k - 1) - (j + 1) + 1
+            count += cnt
+
+            s = a[i] + a[j] + a[k - 1]
+            if s > best_sum:
+                best_sum = s
+
+print(-1 if count == 0 else f"{count} {best_sum}")
 
