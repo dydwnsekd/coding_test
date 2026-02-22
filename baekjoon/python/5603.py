224dda9a513faa14bad96263fe3abbc7a12fd86b
dydwnsekd <dydwnsekd123@gmail.com>
2026-02-23 00:01:16 +0900

    백준 5603 問題2 python 완료

--- baekjoon/python/5603.py
+++ baekjoon/python/5603.py
@@ -3,19 +3,19 @@ import sys
 n = int(sys.stdin.readline())
 s = sys.stdin.readline().strip()
 
-while n < 0:
+while n > 0:
     n -= 1
     result = ""
-    num_count = 0
+    num_count = 1
 
     for i in range(len(s)):
-        if s[i] == s[i+1]:
+        if i + 1 == len(s):
+            result += f"{num_count}{s[i]}"
+        elif s[i] == s[i+1]:
             num_count += 1
         else:
             result += f"{num_count}{s[i]}"
-            num_count = 0
-
-        print(num_count)
+            num_count = 1
 
     s = result
 
