7084f23d76f4a8b163fc83d6e6b976dd2893cf84
dydwnsekd <dydwnsekd123@gmail.com>
2026-03-01 23:52:02 +0900

    백준 34980 생수병 놓기 python 완료

---
+++ baekjoon/python/34980.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+s1 = sys.stdin.readline().strip()
+s2 = sys.stdin.readline().strip()
+
+if s1 == s2:
+    print("Good")
+elif s1.count('w') == s2.count('w'):
+    print("Its fine")
+elif s1.count('w') > s2.count('w'):
+    print("Oryang")
+elif s1.count('w') < s2.count('w'):
+    print("Manners maketh man")
+
