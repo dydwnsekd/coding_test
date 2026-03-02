f0503a70c21fe41ada8042a6cea28081248611e6
dydwnsekd <dydwnsekd123@gmail.com>
2026-03-02 17:27:55 +0900

    백준 34980 생수병 놓기 python 개선 코드 추가

--- baekjoon/python/34980.py
+++ baekjoon/python/34980.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 n = int(sys.stdin.readline())
@@ -12,4 +13,17 @@ elif s1.count('w') > s2.count('w'):
     print("Oryang")
 elif s1.count('w') < s2.count('w'):
     print("Manners maketh man")
+"""
+
+import sys
+
+_ = sys.stdin.readline()
+s1 = sys.stdin.readline().strip()
+s2 = sys.stdin.readline().strip()
+
+if s1 == s2:
+    print("Good")
+else:
+    c1, c2 = s1.count('w'), s2.count('w')
+    print("Its fine" if c1 == c2 else ("Oryang" if c1 > c2 else "Manners maketh man"))
 
