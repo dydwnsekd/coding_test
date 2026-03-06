32a9d05fa22fb5cc82efc6bc71631c15b3fb2fa4
dydwnsekd <dydwnsekd123@gmail.com>
2026-03-06 10:01:18 +0900

    백준 34998 1, 2, 3, 많다! python 완료

--- baekjoon/python/34998.py
+++ baekjoon/python/34998.py
@@ -8,21 +8,21 @@ for _ in range(n):
 
     if x == 1:
         if expression[0] == "!" or expression[4] == "!":
-            result = "!"
+            result = -1
         else:
             result = int(expression[0]) + int(expression[4])
     elif x == 2:
         if expression[0] == "!" or expression[4] == "!" or expression[8] == "!":
-            result = "!"
+            result = -1
         else:
             result = int(expression[0]) + int(expression[4]) + int(expression[8])
     else:
         if expression[0] == "!" or expression[4] == "!" or expression[8] == "!" or expression[12] == "!":
-            result = "!"
+            result = -1
         else:
             result = int(expression[0]) + int(expression[4]) + int(expression[8]) + int(expression[12])
 
-    if result >= 10 or result == "!":
+    if result >= 10 or result == -1:
         print("!")
     else:
         print(result)
