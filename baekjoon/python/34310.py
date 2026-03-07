27d99b2a385b3ac238417ac0dcfa1f02e1b39348
dydwnsekd <dydwnsekd123@gmail.com>
2026-03-08 00:15:45 +0900

    백준 34310 No Stragglers python 완료

---
+++ baekjoon/python/34310.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+result = 0
+
+for _ in range(n):
+    visitor, visitor_type, num = sys.stdin.readline().split()
+    num = int(num)
+
+    if visitor_type == 'IN':
+        result += num
+    elif visitor_type == 'OUT':
+        result -= num
+
+if result == 0:
+    print("NO STRAGGLERS")
+else:
+    print(result)
