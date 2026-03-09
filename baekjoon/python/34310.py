0f4bb7f13d60d297b158ec7d6083d5edf1abdb0a
dydwnsekd <dydwnsekd123@gmail.com>
2026-03-09 10:22:23 +0900

    백준 34310 No Stragglers python 개선 코드 추가

--- baekjoon/python/34310.py
+++ baekjoon/python/34310.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 n = int(sys.stdin.readline())
@@ -16,3 +17,23 @@ if result == 0:
     print("NO STRAGGLERS")
 else:
     print(result)
+"""
+
+import sys
+
+n = int(sys.stdin.readline())
+result = 0
+
+for _ in range(n):
+    _, visitor_type, num = sys.stdin.readline().split()
+    num = int(num)
+
+    if visitor_type == 'IN':
+        result += num
+    else:
+        result -= num
+
+if result:
+    print(result)
+else:
+    print("NO STRAGGLERS")
