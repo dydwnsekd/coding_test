10df733d9005b5e67258f5cf84660777d7ee946b
dydwnsekd <dydwnsekd123@gmail.com>
2026-03-13 09:45:21 +0900

    백준 21573 Кондиционер python 완료

--- baekjoon/python/21573.py
+++ baekjoon/python/21573.py
@@ -3,7 +3,19 @@ import sys
 troom, tcond = map(int, sys.stdin.readline().strip().split())
 mode = sys.stdin.readline().strip()
 
-if mode in ("freeze", "heat", "auto"):
+if mode == "freeze":
+    if troom < tcond:
+        print(troom)
+    else:
+        print(tcond)
+elif mode == "heat":
+    if troom > tcond:
+        print(troom)
+    else:
+        print(tcond)
+elif mode == "auto":
     print(tcond)
-else:
+elif mode == "fan":
     print(troom)
+
+
