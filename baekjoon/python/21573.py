42559e26896c759a2a7c58d6deb517c2607b7077
dydwnsekd <dydwnsekd123@gmail.com>
2026-03-14 16:06:39 +0900

    백준 21573 Кондиционер python 개선 코드 추가

--- baekjoon/python/21573.py
+++ baekjoon/python/21573.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 troom, tcond = map(int, sys.stdin.readline().strip().split())
@@ -17,5 +18,19 @@ elif mode == "auto":
     print(tcond)
 elif mode == "fan":
     print(troom)
+"""
 
+import sys
+
+troom, tcond = map(int, sys.stdin.readline().split())
+mode = sys.stdin.readline().rstrip('\n')
+
+if mode == "freeze":
+    print(min(troom, tcond))
+elif mode == "heat":
+    print(max(troom, tcond))
+elif mode == "auto":
+    print(tcond)
+else:
+    print(troom)
 
