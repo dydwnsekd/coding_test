88bd323812af431c924dec788e91bec807381409
dydwnsekd <dydwnsekd123@gmail.com>
2026-04-01 23:46:00 +0900

    백준 32316 Ready for Contest python 코드 수정

--- baekjoon/python/32316.py
+++ baekjoon/python/32316.py
@@ -5,10 +5,10 @@ problem_dict = {}
 
 for _ in range(m):
     p, l = map(int, sys.stdin.readline().strip().split())
-    if p in problem_dict and l not in problem_dict[p]:
-        problem_dict[p].append(l)
-    else:
+    if p not in problem_dict:
         problem_dict[p] = [l]
+    elif l not in problem_dict[p]:
+        problem_dict[p].append(l)
 
 for problem, language in problem_dict.items():
     if 1 in language and 2 in language:
