dd6c376fb63d7f2e7531ce6318305d2ea226a922
dydwnsekd <dydwnsekd123@gmail.com>
2026-04-26 22:34:41 +0900

    백준 21022 Three Points for a Win python 개선 코드 추가

--- baekjoon/python/21022.py
+++ baekjoon/python/21022.py
@@ -1,3 +1,4 @@
+"""
 import sys
 
 result = 0
@@ -11,6 +12,22 @@ for i in range(n):
     if a_list[i] > b_list[i]:
         result += 3
     elif a_list[i] == b_list[i]:
+        result += 1
+
+print(result)
+"""
+
+import sys
+
+n = int(sys.stdin.readline())
+a_list = list(map(int, sys.stdin.readline().split()))
+b_list = list(map(int, sys.stdin.readline().split()))
+
+result = 0
+for a, b in zip(a_list, b_list):
+    if a > b:
+        result += 3
+    elif a == b:
         result += 1
 
 print(result)
