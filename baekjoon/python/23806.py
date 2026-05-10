fde1ce68657700e002bc22eace1075a0cc3a5e67
dydwnsekd <dydwnsekd123@gmail.com>
2026-05-10 22:27:46 +0900

    백준 23806 골뱅이 찍기 - ㅁ 완료

--- baekjoon/python/23806.py
+++ baekjoon/python/23806.py
@@ -1,4 +1,5 @@
 # 골뱅이 찍기 - ㅁ
+"""
 import sys
 
 def print_head(n):
@@ -14,3 +15,21 @@ n = int(sys.stdin.readline())
 print_head(n)
 print_body(n)
 print_head(n)
+"""
+
+import sys
+
+n = int(sys.stdin.readline())
+
+head = "@" * (5 * n)
+body = "@" * n + " " * (3 * n) + "@" * n
+
+pattern = [
+    (head, n),
+    (body, 3 * n),
+    (head, n),
+]
+
+for line, count in pattern:
+    for _ in range(count):
+        print(line)
